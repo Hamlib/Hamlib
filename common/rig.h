df2a00ef50b5016954306ef337e51c283f168609
Frank Singleton, VK3FCS <javabear@users.sourceforge.net>
2000-09-04 03:50:42 +0000

    added GPL stuff and comments
    git-svn-id: https://hamlib.svn.sourceforge.net/svnroot/hamlib/trunk@82 7ae35d74-ebe9-4afe-98af-79ac388436b8

--- common/rig.h
+++ common/rig.h
@@ -1,3 +1,29 @@
+/*  hamlib - (C) Frank Singleton 2000 (vk3fcs@ix.netcom.com)
+ *
+ * rig.h - (C) Frank Singleton 2000 (vk3fcs@ix.netcom.com)
+ * This program defines some rig capabilities structures that
+ * will be used for obtaining rig capabilities,
+ *
+ *
+ * 	$Id: rig.h,v 1.2 2000-09-04 03:50:42 javabear Exp $	 *
+ *
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ */
+
 /*
  * Basic rig type, can store some useful
  * info about different radios. Each lib must
