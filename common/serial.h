9acee4b445297b85b0c2be21d0a6a1af2b69fb90
Stéphane Fillod, F8CFE <fillods@users.sourceforge.net>
2000-09-19 06:58:56 +0000

    write_block2 renamed to write_block
    git-svn-id: https://hamlib.svn.sourceforge.net/svnroot/hamlib/trunk@124 7ae35d74-ebe9-4afe-98af-79ac388436b8

--- common/serial.h
+++ common/serial.h
@@ -5,7 +5,7 @@
  * Provides useful routines for read/write serial data for communicating
  * via serial interface .
  *
- *    $Id: serial.h,v 1.9 2000-09-19 00:16:03 javabear Exp $  
+ *    $Id: serial.h,v 1.10 2000-09-19 06:58:56 f4cfe Exp $  
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -31,10 +31,10 @@
 
 
 int serial_open(struct rig_state *rs);
-int close_port(int fd);
+
 int read_sleep(int fd, unsigned char *rxbuffer, int num );
 int read_block(int fd, unsigned char *rxbuffer, size_t count, int timeout);
-int write_block2(int fd, const unsigned char *txbuffer, size_t count, int write_delay);
+int write_block(int fd, const unsigned char *txbuffer, size_t count, int write_delay);
 
 
 /*
