41cb317ec481026fd7f536942641e0508f352a3b
Stéphane Fillod, F8CFE <fillods@users.sourceforge.net>
2000-10-08 21:37:19 +0000

    * headers moved, * added rig transceive cap On, * made use of MHz,KHz macros, * added get_strength and decode_event to ic706iig
    git-svn-id: https://hamlib.svn.sourceforge.net/svnroot/hamlib/trunk@190 7ae35d74-ebe9-4afe-98af-79ac388436b8

--- icom/ic706.c
+++ icom/ic706.c
@@ -7,7 +7,7 @@
  * using the "CI-V" interface.
  *
  *
- * $Id: ic706.c,v 1.3 2000-10-01 12:33:30 f4cfe Exp $  
+ * $Id: ic706.c,v 1.4 2000-10-08 21:37:19 f4cfe Exp $  
  *
  *
  *
@@ -36,8 +36,8 @@
 #include <termios.h> /* POSIX terminal control definitions */
 #include <sys/ioctl.h>
 
-#include <rig.h>
-#include <riglist.h>
+#include <hamlib/rig.h>
+#include <hamlib/riglist.h>
 #include "icom.h"
 
 
@@ -61,7 +61,53 @@
 const struct rig_caps ic706_caps = {
   RIG_MODEL_IC706, "IC-706", "Icom", "0.2", RIG_STATUS_ALPHA,
   RIG_TYPE_MOBILE, RIG_PTT_NONE, 300, 19200, 8, 1, RIG_PARITY_NONE, 
-  RIG_HANDSHAKE_NONE, 0, 2000, 3, IC706_FUNC_ALL, 101,
+  RIG_HANDSHAKE_NONE, 0, 2000, 3, IC706_FUNC_ALL, 101, RIG_TRN_ON,
+  { {KHz(30),199999999,IC706_ALL_RX_MODES,-1,-1},{0,0,0,0,0}, }, /* rx range */
+  { {KHz(1800),1999999,IC706_OTHER_TX_MODES,5000,100000},	/* 100W class */
+    {KHz(1800),1999999,IC706_AM_TX_MODES,2000,40000},	/* 40W class */
+    {KHz(3500),3999999,IC706_OTHER_TX_MODES,5000,100000},
+    {KHz(3500),3999999,IC706_AM_TX_MODES,2000,40000},
+    {MHz(7),KHz(7300),IC706_OTHER_TX_MODES,5000,100000},
+    {MHz(7),KHz(7300),IC706_AM_TX_MODES,2000,40000},
+    {MHz(10),KHz(10150),IC706_OTHER_TX_MODES,5000,100000},
+    {MHz(10),KHz(10150),IC706_AM_TX_MODES,2000,40000},
+    {MHz(14),KHz(14350),IC706_OTHER_TX_MODES,5000,100000},
+    {MHz(14),KHz(14350),IC706_AM_TX_MODES,2000,40000},
+    {KHz(18068),KHz(18168),IC706_OTHER_TX_MODES,5000,100000},
+    {KHz(18068),KHz(18168),IC706_AM_TX_MODES,2000,40000},
+    {MHz(21),KHz(21450),IC706_OTHER_TX_MODES,5000,100000},
+    {MHz(21),KHz(21450),IC706_AM_TX_MODES,2000,40000},
+    {KHz(24890),KHz(24990),IC706_OTHER_TX_MODES,5000,100000},
+    {KHz(24890),KHz(24990),IC706_AM_TX_MODES,2000,40000},
+    {MHz(28),KHz(29700),IC706_OTHER_TX_MODES,5000,100000},
+    {MHz(28),KHz(29700),IC706_AM_TX_MODES,2000,40000},
+    {MHz(50),MHz(54),IC706_OTHER_TX_MODES,5000,100000},
+    {MHz(50),MHz(54),IC706_AM_TX_MODES,2000,40000},
+    {MHz(144),MHz(148),IC706_OTHER_TX_MODES,5000,20000}, /* not sure.. */
+    {MHz(144),MHz(148),IC706_AM_TX_MODES,2000,8000}, /* anyone? */
+	{0,0,0,0,0} },
+	{{IC706_1HZ_TS_MODES,1},
+	 {IC706_ALL_RX_MODES,10},
+	 {IC706_ALL_RX_MODES,100},
+	 {IC706_ALL_RX_MODES,KHz(1)},
+	 {IC706_ALL_RX_MODES,KHz(5)},
+	 {IC706_ALL_RX_MODES,KHz(9)},
+	 {IC706_ALL_RX_MODES,KHz(10)},
+	 {IC706_ALL_RX_MODES,12500},
+	 {IC706_ALL_RX_MODES,KHz(20)},
+	 {IC706_ALL_RX_MODES,KHz(25)},
+	 {IC706_ALL_RX_MODES,KHz(100)},
+	 {IC706_1MHZ_TS_MODES,MHz(1)},
+	 {0,0}
+	},
+  icom_init, icom_cleanup, NULL, NULL, NULL /* probe not supported yet */,
+  icom_set_freq, icom_get_freq, icom_set_mode, icom_get_mode, icom_set_vfo, NULL,
+};
+
+const struct rig_caps ic706mkii_caps = {
+  RIG_MODEL_IC706MKII, "IC-706MKII", "Icom", "0.2", RIG_STATUS_ALPHA,
+  RIG_TYPE_MOBILE, RIG_PTT_NONE, 300, 19200, 8, 1, RIG_PARITY_NONE, 
+  RIG_HANDSHAKE_NONE, 0, 2000, 3, IC706_FUNC_ALL, 101, RIG_TRN_ON,
   { {30000,199999999,IC706_ALL_RX_MODES,-1,-1}, {0,0,0,0,0}, }, /* rx range */
   { {1800000,1999999,IC706_OTHER_TX_MODES,5000,100000},	/* 100W class */
     {1800000,1999999,IC706_AM_TX_MODES,2000,40000},	/* 40W class */
@@ -101,13 +147,18 @@ const struct rig_caps ic706_caps = {
 	 {0,0}
 	},
   icom_init, icom_cleanup, NULL, NULL, NULL /* probe not supported yet */,
-  icom_set_freq, NULL,
+  icom_set_freq, icom_get_freq, icom_set_mode, icom_get_mode, icom_set_vfo, NULL,
 };
 
+
+/*
+ * Basically, the IC706MKIIG is an IC706MKII plus UHF, a DSP
+ * and 50W VHF
+ */
 const struct rig_caps ic706mkiig_caps = {
   RIG_MODEL_IC706MKIIG, "IC-706MKIIG", "Icom", "0.2", RIG_STATUS_ALPHA,
   RIG_TYPE_MOBILE, RIG_PTT_NONE, 300, 19200, 8, 1, RIG_PARITY_NONE, 
-  RIG_HANDSHAKE_NONE, 0, 2000, 3, IC706_FUNC_ALL|RIG_FUNC_NR|RIG_FUNC_ANF, 101,
+  RIG_HANDSHAKE_NONE, 0, 2000, 3, IC706_FUNC_ALL|RIG_FUNC_NR|RIG_FUNC_ANF, 101, RIG_TRN_ON,
   { {30000,199999999,IC706_ALL_RX_MODES,-1,-1},	/* this trx also has UHF */
  	{400000000,470000000,IC706_ALL_RX_MODES,-1,-1}, {0,0,0,0,0}, },
   { {1800000,1999999,IC706_OTHER_TX_MODES,5000,100000},	/* 100W class */
@@ -150,7 +201,10 @@ const struct rig_caps ic706mkiig_caps = {
 	 {0,0}
 	},
   icom_init, icom_cleanup, NULL, NULL, NULL /* probe not supported yet */,
-  icom_set_freq, icom_get_freq, NULL, 
+  icom_set_freq, icom_get_freq, icom_set_mode, icom_get_mode, icom_set_vfo,
+get_strength: icom_get_strength,
+decode_event: icom_decode_event,
+  NULL, 
 };
 
 
