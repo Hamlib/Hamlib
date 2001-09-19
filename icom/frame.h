897109bb7e6ab76b3f26ae72acf2e9d5d6a2d5df
Stéphane Fillod, F8CFE <fillods@users.sourceforge.net>
2001-09-19 21:17:35 +0000

    added missing prototype
    git-svn-id: https://hamlib.svn.sourceforge.net/svnroot/hamlib/trunk@643 7ae35d74-ebe9-4afe-98af-79ac388436b8

--- icom/frame.h
+++ icom/frame.h
@@ -2,7 +2,7 @@
  *  Hamlib CI-V backend - low level communication header
  *  Copyright (c) 2000,2001 by Stephane Fillod
  *
- *		$Id: frame.h,v 1.8 2001-07-13 19:08:15 f4cfe Exp $
+ *		$Id: frame.h,v 1.9 2001-09-19 21:17:35 f4cfe Exp $
  *
  *   This library is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU Library General Public License as
@@ -29,6 +29,7 @@
  */
 int make_cmd_frame(char frame[], char re_id, char cmd, int subcmd, const char *data, int data_len);
 
+int read_icom_block(port_t *p, unsigned char *rxbuffer, size_t count);
 int icom_transaction (RIG *rig, int cmd, int subcmd, const char *payload, int payload_len, char *data, int *data_len);
 int read_icom_frame(port_t *p, unsigned char rxbuffer[]);
 
