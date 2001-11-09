ad6e745d7bb53014c39dd5617ab74606ada5b0a9
Stéphane Fillod, F8CFE <fillods@users.sourceforge.net>
2001-11-09 15:45:38 +0000

    share code with the TH-F7
    git-svn-id: https://hamlib.svn.sourceforge.net/svnroot/hamlib/trunk@706 7ae35d74-ebe9-4afe-98af-79ac388436b8

--- kenwood/thd7.c
+++ kenwood/thd7.c
@@ -2,7 +2,7 @@
  *  Hamlib Kenwood backend - TH-D7 description
  *  Copyright (c) 2000,2001 by Stephane Fillod
  *
- *		$Id: thd7.c,v 1.1 2001-10-18 20:42:25 f4cfe Exp $
+ *		$Id: thd7.c,v 1.2 2001-11-09 15:45:38 f4cfe Exp $
  *
  *   This library is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU Library General Public License as
@@ -36,15 +36,7 @@
 #include <hamlib/rig.h>
 #include <hamlib/riglist.h>
 #include "kenwood.h"
-
-
-int thd7_set_vfo(RIG *rig, vfo_t vfo);
-int thd7_set_freq(RIG *rig, vfo_t vfo, freq_t freq);
-int thd7_get_freq(RIG *rig, vfo_t vfo, freq_t *freq);
-int thd7_set_mode(RIG *rig, vfo_t vfo, rmode_t mode, pbwidth_t width);
-int thd7_get_mode(RIG *rig, vfo_t vfo, rmode_t *mode, pbwidth_t *width);
-int thd7_set_ctcss_tone(RIG *rig, vfo_t vfo, tone_t tone);
-int thd7_get_ctcss_tone(RIG *rig, vfo_t vfo, tone_t *tone);
+#include "th.h"
 
 
 #define THD7_MODES 	  (RIG_MODE_FM|RIG_MODE_AM)
@@ -145,13 +137,13 @@ filters: {
 	},
 priv: NULL,
 
-set_freq: thd7_set_freq,
-get_freq: thd7_get_freq,
-set_mode: thd7_set_mode,
-get_mode: thd7_get_mode,
-set_vfo: thd7_set_vfo,
-set_ctcss_tone: thd7_set_ctcss_tone,
-get_ctcss_tone: thd7_get_ctcss_tone,
+set_freq: th_set_freq,
+get_freq: th_get_freq,
+set_mode: th_set_mode,
+get_mode: th_get_mode,
+set_vfo: th_set_vfo,
+set_ctcss_tone: th_set_ctcss_tone,
+get_ctcss_tone: th_get_ctcss_tone,
 set_ptt: kenwood_set_ptt,
 get_dcd: kenwood_get_dcd,
 vfo_op: kenwood_vfo_op,
@@ -159,205 +151,4 @@ set_mem: kenwood_set_mem,
 get_mem: kenwood_get_mem,
 
 };
-
-#define EOM ';'
-
-/*
- * modes in use by the "MD" command
- */
-#define MD_FM	'0'
-#define MD_AM	'1'
-
-int thd7_set_vfo(RIG *rig, vfo_t vfo)
-{
-		unsigned char ackbuf[16];
-		int ack_len, retval;
-
-			/*
-			 * FIXME: vfo==RIG_VFO_CURR
-			 */
-
-		switch (vfo) {
-		case RIG_VFO_A:	/* FIXME */
-		case RIG_VFO_B:
-		case RIG_VFO_VFO:
-			retval = kenwood_transaction (rig, "VMC0,0;", 7, ackbuf, &ack_len);
-			break;
-		case RIG_VFO_MEM:
-			retval = kenwood_transaction (rig, "VMC0,2;", 7, ackbuf, &ack_len);
-			break;
-		default: 
-			rig_debug(RIG_DEBUG_ERR,"thd7_set_vfo: unsupported VFO %d\n",
-								vfo);
-			return -RIG_EINVAL;
-		}
-
-		return retval;
-}
-
-/*
- * thd7_set_freq
- * Assumes rig!=NULL
- */
-int thd7_set_freq(RIG *rig, vfo_t vfo, freq_t freq)
-{
-		unsigned char freqbuf[16], ackbuf[16];
-		int freq_len, ack_len, retval;
-
-		freq_len = sprintf(freqbuf,"FQ%011Ld,0;", freq);
-
-		retval = kenwood_transaction (rig, freqbuf, freq_len, ackbuf, &ack_len);
-
-		return retval;
-}
-
-/*
- * thd7_get_freq
- * Assumes rig!=NULL, freq!=NULL
- */
-int thd7_get_freq(RIG *rig, vfo_t vfo, freq_t *freq)
-{
-		unsigned char freqbuf[16];
-		int freq_len, retval;
-
-		retval = kenwood_transaction (rig, "FQ;", 3, freqbuf, &freq_len);
-		if (retval != RIG_OK)
-			return retval;
-
-		sscanf(freqbuf,"%Ld", freq);
-
-		return RIG_OK;
-}
-
-/*
- * thd7_set_mode
- * Assumes rig!=NULL
- */
-int thd7_set_mode(RIG *rig, vfo_t vfo, rmode_t mode, pbwidth_t width)
-{
-		unsigned char mdbuf[16],ackbuf[16];
-		int mdbuf_len, ack_len, kmode, retval;
-
-		switch (mode) {
-			case RIG_MODE_FM:       kmode = MD_FM; break;
-			case RIG_MODE_AM:       kmode = MD_AM; break;
-			default:
-				rig_debug(RIG_DEBUG_ERR,"thd7_set_mode: "
-								"unsupported mode %d\n", mode);
-				return -RIG_EINVAL;
-		}
-
-		mdbuf_len = sprintf(mdbuf, "MD%c;", kmode);
-		retval = kenwood_transaction (rig, mdbuf, mdbuf_len, ackbuf, &ack_len);
-
-		return retval;
-}
-
-/*
- * thd7_get_mode
- * Assumes rig!=NULL, mode!=NULL
- */
-int thd7_get_mode(RIG *rig, vfo_t vfo, rmode_t *mode, pbwidth_t *width)
-{
-		unsigned char ackbuf[16];
-		int ack_len, retval;
-
-		retval = kenwood_transaction (rig, "MD;", 2, ackbuf, &ack_len);
-		if (retval != RIG_OK)
-			return retval;
-
-		if (ack_len != 4) {
-				rig_debug(RIG_DEBUG_ERR,"thd7_get_mode: ack NG, len=%d\n",
-								ack_len);
-				return -RIG_ERJCTED;
-		}
-
-		*width = RIG_PASSBAND_NORMAL;	/* FIXME */
-		switch (ackbuf[0]) {
-			case MD_FM:		*mode = RIG_MODE_FM; break;
-			case MD_AM:		*mode = RIG_MODE_AM; break;
-			default:
-				rig_debug(RIG_DEBUG_ERR,"thd7_get_mode: "
-								"unsupported mode %d\n", ackbuf[0]);
-				return -RIG_EINVAL;
-		}
-
-		return RIG_OK;
-}
-
-/*
- * thd7_set_ctcss_tone
- * Assumes rig!=NULL, rig->caps->ctcss_list != NULL
- *
- * Warning! This is untested stuff!
- * 	Please owners report to me <f4cfe@users.sourceforge.net>, thanks. --SF
- */
-int thd7_set_ctcss_tone(RIG *rig, vfo_t vfo, tone_t tone)
-{
-	const struct rig_caps *caps;
-	unsigned char tonebuf[16], ackbuf[16];
-	int tone_len, ack_len;
-	int i;
-								 
-	caps = rig->caps;
-
-/* TODO: replace 200 by something like RIGTONEMAX */
-	for (i = 0; caps->ctcss_list[i] != 0 && i<200; i++) {
-			if (caps->ctcss_list[i] == tone)
-					break;
-	}
-	if (caps->ctcss_list[i] != tone)
-			return -RIG_EINVAL;
-
-/* TODO: replace menu no 57 by a define */
-	tone_len = sprintf(tonebuf,"TN%02d;", i+1);
-	
-	return kenwood_transaction (rig, tonebuf, tone_len, ackbuf, &ack_len);
-}
-
-/*
- * thd7_get_ctcss_tone
- * Assumes rig!=NULL, rig->state.priv!=NULL
- */
-int thd7_get_ctcss_tone(RIG *rig, vfo_t vfo, tone_t *tone)
-{
-	const struct rig_caps *caps;
-	unsigned char tonebuf[16];
-	int tone_len, i, retval;
-	unsigned int tone_idx;
-								 
-	caps = rig->caps;
-
-	/* TODO: replace menu no 57 by a define */
-	
-	retval = kenwood_transaction (rig, "TN;", 3, tonebuf, &tone_len);
-	if (retval != RIG_OK)
-		return retval;
-
-	if (tone_len != 10) {
-			rig_debug(RIG_DEBUG_ERR,"thd7_get_ctcss_tone: unexpected reply "
-							"'%s', len=%d\n", tonebuf, tone_len);
-			return -RIG_ERJCTED;
-	}
-
-	sscanf(tonebuf+3, "%u", (int*)&tone_idx);
-
-	if (tone_idx == 0) {
-			rig_debug(RIG_DEBUG_ERR,"thd7_get_ctcss_tone: Unexpected CTCSS "
-							"no (%04d)\n", tone_idx);
-			return -RIG_EPROTO;
-	}
-		
-	/* check this tone exists. That's better than nothing. */
-	for (i = 0; i<tone_idx; i++) {
-		if (caps->ctcss_list[i] == 0) {
-			rig_debug(RIG_DEBUG_ERR,"thd7_get_ctcss_tone: CTCSS NG "
-					"(%04d)\n", tone_idx);
-			return -RIG_EPROTO;
-		}
-	}
-	*tone = caps->ctcss_list[tone_idx-1];
-
-	return RIG_OK;
-}
 
