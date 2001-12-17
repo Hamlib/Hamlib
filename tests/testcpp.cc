71898caca02f50f577ee83225858d4fd3ccad547
Stéphane Fillod, F8CFE <fillods@users.sourceforge.net>
2001-12-17 22:58:54 +0000

    misc test added
    git-svn-id: https://hamlib.svn.sourceforge.net/svnroot/hamlib/trunk@759 7ae35d74-ebe9-4afe-98af-79ac388436b8

--- tests/testcpp.cc
+++ tests/testcpp.cc
@@ -14,6 +14,7 @@ int main(int argc, char* argv[])
 		myRig.open();
 		myRig.setFreq(MHz(144));
 		cout << myRig.getLevelI(RIG_LEVEL_STRENGTH) << "dB" << endl;
+		cout << "Modes for freq 14.332: " << myRig.RngRxModes(MHz(14.332)) << endl;
 		myRig.close();
 	}
 	catch (const RigException &Ex) {
