ae5f45acb0fe8743b4077c567110eaa312ed3995
Stéphane Fillod, F8CFE <fillods@users.sourceforge.net>
2002-02-09 21:59:33 +0000

    Initial release
    git-svn-id: https://hamlib.svn.sourceforge.net/svnroot/hamlib/trunk@951 7ae35d74-ebe9-4afe-98af-79ac388436b8

---
+++ perl/Hamlib/test.pl
@@ -0,0 +1,24 @@
+# Before `make install' is performed this script should be runnable with
+# `make test'. After `make install' it should work as `perl test.pl'
+
+#########################
+
+# change 'tests => 1' to 'tests => last_test_to_print';
+
+use Test;
+BEGIN { plan tests => 3 };
+use Hamlib;
+ok(1); # If we made it this far, we're ok.
+
+#########################
+
+# Insert your test code below, the Test module is use()ed here so read
+# its man page ( perldoc Test ) for help writing this test script.
+
+$my_rig = Hamlib::init(RIG_MODEL_DUMMY);
+ok(1);
+
+$my_rig->open();
+ok(1);
+
+
