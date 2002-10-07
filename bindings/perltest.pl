03160aa72f69463b5989bb020c4565f046f6d445
Stéphane Fillod, F8CFE <fillods@users.sourceforge.net>
2002-10-07 21:44:51 +0000

    more work on swig bindings, nearly every calls supported, some chan wrapping missing, and also lists
    git-svn-id: https://hamlib.svn.sourceforge.net/svnroot/hamlib/trunk@1200 7ae35d74-ebe9-4afe-98af-79ac388436b8

--- bindings/perltest.pl
+++ bindings/perltest.pl
@@ -6,14 +6,60 @@ use hamlib;
 print "Version: $hamlib::hamlib_version\n";
 print "FM: $hamlib::RIG_MODE_FM\n";
 
-hamlib::rig_set_debug(5);
+hamlib::rig_set_debug($hamlib::RIG_DEBUG_TRACE);
 
 $rig = new hamlib::Rig($hamlib::RIG_MODEL_DUMMY);
 $rig->open();
 
+$rpath = $rig->get_conf("rig_pathname");
+$region = $rig->get_conf(1073741944);
+print "get_conf: path=\"$rpath\", ITU region=$region\n";
+
+
 $rig->set_freq(12000000, $hamlib::RIG_VFO_A);
+
+$f = $rig->get_freq();
+print "freq: $f\n";
+
+($mode, $width) = $rig->get_mode();
+
+print "mode: $mode, width: $width\n";
 
 print "ITU region: $rig->{state}->{itu_region}\n";
 print "Copyright: $rig->{caps}->{copyright}\n";
+$inf = $rig->get_info();
+
+print "get_info: $inf\n";
+
+$rig->set_level("VOX", 1);
+$lvl = $rig->get_level_i("VOX");
+print "level: $lvl\n";
+$rig->set_level($hamlib::RIG_LEVEL_VOX, 5);
+$lvl = $rig->get_level_i($hamlib::RIG_LEVEL_VOX);
+print "level: $lvl\n";
+
+
+
+$chan = new hamlib::Chan($hamlib::RIG_VFO_A);
+
+$rig->get_channel(\$chan);
+
+print "VFO: $chan->{fields}->{vfo}, $chan->{fields}->{freq}\n";
 
 $rig->close();
+
+($long1, $lat1) = locator2longlat("IN98EC");
+($long2, $lat2) = locator2longlat("DM33DX");
+$loc1 = longlat2locator($long1, $lat1);
+$loc2 = longlat2locator($long2, $lat2);
+print "Loc1: $loc1\n";
+print "Loc2: $loc2\n";
+
+($dist, $az) = qrb($long1, $lat1, $long2, $lat2);
+$longpath = distance_long_path($dist);
+print "Distance: $dist km, long path: $longpath\n";
+($deg, $min, $sec) = dec2dms($az);
+$az2 = dms2dec($deg, $min, $sec);
+print "Bearing: $az, $deg° $min' $sec\", recoded: $az2\n"
+
+
--- bindings/tcltest.tcl
+++ bindings/tcltest.tcl
@@ -6,16 +6,42 @@ load ".libs/hamlibtcl.so" Hamlib
 
 puts $hamlib_version\n
 
+rig_set_debug $RIG_DEBUG_TRACE
+
 # Init RIG_MODEL_DUMMY
 Rig my_rig $RIG_MODEL_DUMMY
 
 my_rig open
 my_rig set_freq 145550000
 
-# note: get_level may not be implemented yet!
-puts [my_rig get_level $RIG_LEVEL_STRENGTH]
+puts status:[my_rig cget -error_status]
+
+set mode [my_rig get_mode]
+puts "mode:[lindex $mode 0], bandwidth:[lindex $mode 1]Hz (0=normal)"
+
+set state [my_rig cget -state]
+puts ITU_region:[$state cget -itu_region]
+
+# The following works well also
+# puts ITU_region:[[my_rig cget -state] cget -itu_region]
+
+puts getinfo:[my_rig get_info]
+
+my_rig set_level "VOX"  1
+puts status:[my_rig cget -error_status]
+puts level:[my_rig get_level_i "VOX"]
+puts status:[my_rig cget -error_status]
+my_rig set_level $RIG_LEVEL_VOX 5
+puts status:[my_rig cget -error_status]
+puts level:[my_rig get_level_i $RIG_LEVEL_VOX]
+puts status:[my_rig cget -error_status]
+
+puts str:[my_rig get_level_i $RIG_LEVEL_STRENGTH]
+puts status:[my_rig cget -error_status]
+puts status(str):[rigerror [my_rig cget -error_status]]
+
 my_rig close
-my_rig cleanup
+#my_rig cleanup
 
 
 exit 0
