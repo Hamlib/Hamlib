b63fb958316aa83aa9b9d21b988b68abebae17fd
Stéphane Fillod, F8CFE <fillods@users.sourceforge.net>
2003-04-06 18:45:57 +0000

    hamlib -> Hamlib
    git-svn-id: https://hamlib.svn.sourceforge.net/svnroot/hamlib/trunk@1428 7ae35d74-ebe9-4afe-98af-79ac388436b8

--- bindings/perltest.pl
+++ bindings/perltest.pl
@@ -1,14 +1,13 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -Iblib/arch -Iblib/lib
 
-# make sure the module is there, hint: "ln -s .libs/hamlibperl.so hamlib.so"
-use hamlib;
+use Hamlib;
 
-print "Version: $hamlib::hamlib_version\n";
-print "FM: $hamlib::RIG_MODE_FM\n";
+print "Version: $Hamlib::hamlib_version\n";
+print "FM: $Hamlib::RIG_MODE_FM\n";
 
-hamlib::rig_set_debug($hamlib::RIG_DEBUG_TRACE);
+Hamlib::rig_set_debug($Hamlib::RIG_DEBUG_TRACE);
 
-$rig = new hamlib::Rig($hamlib::RIG_MODEL_DUMMY);
+$rig = new Hamlib::Rig($Hamlib::RIG_MODEL_DUMMY);
 $rig->open();
 
 # 1073741944 is token value for "itu_region"
@@ -17,13 +16,12 @@ $region = $rig->get_conf(1073741944);
 print "get_conf: path=\"$rpath\", ITU region=$region\n";
 
 
-$rig->set_freq(12000000, $hamlib::RIG_VFO_A);
+$rig->set_freq(12000000, $Hamlib::RIG_VFO_A);
 
 $f = $rig->get_freq();
 print "freq: $f\n";
 
 ($mode, $width) = $rig->get_mode();
-
 print "mode: $mode, width: $width\n";
 
 print "ITU region: $rig->{state}->{itu_region}\n";
@@ -35,13 +33,13 @@ print "get_info: $inf\n";
 $rig->set_level("VOX", 1);
 $lvl = $rig->get_level_i("VOX");
 print "level: $lvl\n";
-$rig->set_level($hamlib::RIG_LEVEL_VOX, 5);
-$lvl = $rig->get_level_i($hamlib::RIG_LEVEL_VOX);
+$rig->set_level($Hamlib::RIG_LEVEL_VOX, 5);
+$lvl = $rig->get_level_i($Hamlib::RIG_LEVEL_VOX);
 print "level: $lvl\n";
 
 
 
-$chan = new hamlib::Chan($hamlib::RIG_VFO_A);
+$chan = new Hamlib::Chan($Hamlib::RIG_VFO_A);
 
 $rig->get_channel($chan);
 print "get_channel status: $rig->{error_status}\n";
@@ -51,18 +49,18 @@ print "VFO: $chan->{vfo}, $chan->{freq}\n";
 $rig->close();
 
 # TODO:
-($long1, $lat1) = hamlib::locator2longlat("IN98EC");
-($long2, $lat2) = hamlib::locator2longlat("DM33DX");
-$loc1 = hamlib::longlat2locator($long1, $lat1);
-$loc2 = hamlib::longlat2locator($long2, $lat2);
+($long1, $lat1) = Hamlib::locator2longlat("IN98EC");
+($long2, $lat2) = Hamlib::locator2longlat("DM33DX");
+$loc1 = Hamlib::longlat2locator($long1, $lat1);
+$loc2 = Hamlib::longlat2locator($long2, $lat2);
 print "Loc1: $loc1\n";
 print "Loc2: $loc2\n";
 
-($dist, $az) = hamlib::qrb($long1, $lat1, $long2, $lat2);
-$longpath = hamlib::distance_long_path($dist);
+($dist, $az) = Hamlib::qrb($long1, $lat1, $long2, $lat2);
+$longpath = Hamlib::distance_long_path($dist);
 print "Distance: $dist km, long path: $longpath\n";
-($deg, $min, $sec) = hamlib::dec2dms($az);
-$az2 = hamlib::dms2dec($deg, $min, $sec);
+($deg, $min, $sec) = Hamlib::dec2dms($az);
+$az2 = Hamlib::dms2dec($deg, $min, $sec);
 print "Bearing: $az, $deg° $min' $sec\", recoded: $az2\n"
 
 
