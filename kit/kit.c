8cc6179e2611b73d2afa9bdd3e89752429d0f8ba
Stéphane Fillod, F8CFE <fillods@users.sourceforge.net>
2006-01-09 21:40:17 +0000

    usb.h to needed for the DWT
    git-svn-id: https://hamlib.svn.sourceforge.net/svnroot/hamlib/trunk@2057 7ae35d74-ebe9-4afe-98af-79ac388436b8

--- kit/kit.c
+++ kit/kit.c
@@ -2,7 +2,7 @@
  *  Hamlib KIT backend - main file
  *  Copyright (c) 2004-2005 by Stephane Fillod
  *
- *	$Id: kit.c,v 1.3 2005-11-01 23:14:41 fillods Exp $
+ *	$Id: kit.c,v 1.4 2006-01-09 21:40:17 fillods Exp $
  *
  *   This library is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU Library General Public License as
@@ -44,7 +44,7 @@ DECLARE_INITRIG_BACKEND(kit)
 	rig_register(&elektor304_caps);
 	rig_register(&drt1_caps);
 
-#if defined(HAVE_LIBUSB) || defined(_WIN32)
+#if (defined(HAVE_LIBUSB) && defined(HAVE_USB_H)) || defined(_WIN32)
 	rig_register(&dwt_caps);
 #endif
 
