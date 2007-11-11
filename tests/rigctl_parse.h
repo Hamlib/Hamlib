aa495a59a3ffba8c124ecfc9b5474eab9f1ec9cd
Stéphane Fillod, F8CFE <fillods@users.sourceforge.net>
2007-11-11 23:03:52 +0000

    initial revision
    git-svn-id: https://hamlib.svn.sourceforge.net/svnroot/hamlib/trunk@2242 7ae35d74-ebe9-4afe-98af-79ac388436b8

---
+++ tests/rigctl_parse.h
@@ -0,0 +1,51 @@
+/*
+ * rigctl_parse.h - (C) Stephane Fillod 2000-2007
+ *
+ * This program test/control a radio using Hamlib.
+ * It takes commands in interactive mode as well as 
+ * from command line options.
+ *
+ * $Id: rigctl_parse.h,v 1.1 2007-11-11 23:03:52 fillods Exp $  
+ *
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ */
+
+#ifndef RIGCTL_PARSE_H
+#define RIGCTL_PARSE_H
+
+#include <stdio.h>
+#include <hamlib/rig.h>
+
+/* 
+ * external prototype
+ */
+
+int dumpcaps (RIG *);
+
+/* 
+ * Prototypes
+ */
+void usage_rig();
+void version();
+void list_models();
+void dump_chan(FILE *, RIG*, channel_t*);
+int print_conf_list(const struct confparams *cfp, rig_ptr_t data);
+int set_conf(RIG *my_rig, char *conf_parms);
+
+int rigctl_parse(RIG *my_rig, FILE *fin, FILE *fout, char *argv[], int argc);
+
+#endif	/* RIGCTL_PARSE_H */
