2d8deab072f7ecab33e894e2475e4b38ab9d2e85
Nate Bargmann <n0nb@n0nb.us>
2011-08-20 17:55:00 -0500

    Update LGPL header in Alinco source files.
    TNX to Lucian Laga, YO6PLB, for the notification.

--- alinco/dx77.c
+++ alinco/dx77.c
@@ -2,21 +2,20 @@
  *  Hamlib Alinco backend - DX77 description
  *  Copyright (c) 2001-2005 by Stephane Fillod
  *
- *	$Id: dx77.c,v 1.16 2006-10-07 16:55:04 csete Exp $
  *
- *   This library is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU Library General Public License as
- *   published by the Free Software Foundation; either version 2 of
- *   the License, or (at your option) any later version.
+ *   This library is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU Lesser General Public
+ *   License as published by the Free Software Foundation; either
+ *   version 2.1 of the License, or (at your option) any later version.
  *
- *   This program is distributed in the hope that it will be useful,
+ *   This library is distributed in the hope that it will be useful,
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU Library General Public License for more details.
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *   Lesser General Public License for more details.
  *
- *   You should have received a copy of the GNU Library General Public
+ *   You should have received a copy of the GNU Lesser General Public
  *   License along with this library; if not, write to the Free Software
- *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  *
  */
 
@@ -63,7 +62,7 @@
 /*
  * dx77 rig capabilities.
  *
- * protocol is documented at 
+ * protocol is documented at
  * 		http://www.alinco.com/pdf.files/DX77-77_SOFTWARE_MANUAL.pdf
  *
  * This backend was a pleasure to develop. Documentation is clear,
