c7e64dfa09d21e73ebfcc13a19b0a9ef27b5dc13
Nate Bargmann <n0nb@n0nb.us>
2011-08-20 21:50:02 -0500

    Update LGPL/GPL header in lib source files.
    TNX to Lucian Laga, YO6PLB, for the notification.

--- lib/win32termios.h
+++ lib/win32termios.h
@@ -3,21 +3,21 @@
 |   Copyright 1997-2002 by Trent Jarvi taj@www.linux.org.uk.
 |
 |   This library is free software; you can redistribute it and/or
-|   modify it under the terms of the GNU Library General Public
+|   modify it under the terms of the GNU Lesser General Public
 |   License as published by the Free Software Foundation; either
-|   version 2 of the License, or (at your option) any later version.
+|   version 2.1 of the License, or (at your option) any later version.
 |
-|   If you compile this program with cyg tools this package falls
+|   If you compile this program with cygwin32 tools this package falls
 |   under the GPL.  See COPYING.CYGNUS for details.
 |
 |   This library is distributed in the hope that it will be useful,
 |   but WITHOUT ANY WARRANTY; without even the implied warranty of
 |   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-|   Library General Public License for more details.
+|   Lesser General Public License for more details.
 |
-|   You should have received a copy of the GNU Library General Public
-|   License along with this library; if not, write to the Free
-|   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+|   You should have received a copy of the GNU Lesser General Public
+|   License along with this library; if not, write to the Free Software
+|   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 |
 | This file was taken from rxtx-2.1-7pre16 and adaptated for Hamlib.
 --------------------------------------------------------------------------*/
