12f96c8564d77bdfa9547833cce6f00cba80ec9a
Nate Bargmann <n0nb@n0nb.us>
2011-08-21 18:10:52 -0500

    Update LGPL header in PRM80 source files.
    TNX to Lucian Laga, YO6PLB, for the notification.

--- prm80/prm80.c
+++ prm80/prm80.c
@@ -3,19 +3,19 @@
  *  Copyright (c) 2010 by Stephane Fillod
  *
  *
- *   This library is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU Library General Public License as
- *   published by the Free Software Foundation; either version 2 of
- *   the License, or (at your option) any later version.
+ *   This library is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU Lesser General Public
+ *   License as published by the Free Software Foundation; either
+ *   version 2.1 of the License, or (at your option) any later version.
  *
- *   This program is distributed in the hope that it will be useful,
+ *   This library is distributed in the hope that it will be useful,
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU Library General Public License for more details.
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *   Lesser General Public License for more details.
  *
- *   You should have received a copy of the GNU Library General Public
+ *   You should have received a copy of the GNU Lesser General Public
  *   License along with this library; if not, write to the Free Software
- *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  *
  */
 
@@ -96,7 +96,7 @@ static int prm80_transaction(RIG *rig, const char *cmd, int cmd_len, char *data,
 		/*
 	 	 * Does transceiver sends back ">" ?
 	 	 */
-		if (strstr(retbuf, PROMPT)) 
+		if (strstr(retbuf, PROMPT))
 			return RIG_OK;
 		else
 			return -RIG_ERJCTED;
@@ -158,7 +158,7 @@ int prm80_set_freq(RIG *rig, vfo_t vfo, freq_t freq)
 	freq_len = sprintf(freqbuf, "R%04X%04X",
             (unsigned)(freq / 12500.),
             (unsigned)(freq / 12500.));
- 
+
 	return prm80_transaction (rig, freqbuf, freq_len, NULL, NULL);
 }
 
