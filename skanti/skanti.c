f88e10188db4bead9d52222c18d1178d6e63a63b
Nate Bargmann <n0nb@n0nb.us>
2011-08-21 18:42:44 -0500

    Update LGPL header in Skanti source files.
    TNX to Lucian Laga, YO6PLB, for the notification.

--- skanti/skanti.c
+++ skanti/skanti.c
@@ -2,21 +2,20 @@
  *  Hamlib Skanti backend - main file
  *  Copyright (c) 2004-2005 by Stephane Fillod
  *
- *	$Id: skanti.c,v 1.2 2005-04-10 21:47:14 fillods Exp $
  *
- *   This library is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU Library General Public License as
- *   published by the Free Software Foundation; either version 2 of
- *   the License, or (at your option) any later version.
+ *   This library is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU Lesser General Public
+ *   License as published by the Free Software Foundation; either
+ *   version 2.1 of the License, or (at your option) any later version.
  *
- *   This program is distributed in the hope that it will be useful,
+ *   This library is distributed in the hope that it will be useful,
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU Library General Public License for more details.
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *   Lesser General Public License for more details.
  *
- *   You should have received a copy of the GNU Library General Public
+ *   You should have received a copy of the GNU Lesser General Public
  *   License along with this library; if not, write to the Free Software
- *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  *
  */
 
@@ -90,7 +89,7 @@ static int skanti_transaction(RIG *rig, const char *cmd, int cmd_len, char *data
 
 		retbuf[retval] = '\0';
 
-		if (strstr(retbuf, PROMPT)) 
+		if (strstr(retbuf, PROMPT))
 			return RIG_OK;
 		else
 			return -RIG_ERJCTED;
@@ -180,7 +179,7 @@ int skanti_set_mode(RIG *rig, vfo_t vfo, rmode_t mode, pbwidth_t width)
 	 * 			which passband is default!
 	 */
 	passband_normal = rig_passband_normal(rig, mode);
-	if (width == RIG_PASSBAND_NORMAL || 
+	if (width == RIG_PASSBAND_NORMAL ||
 				width == passband_normal)
 		sk_filter = "I" EOM;
 	else if (width < passband_normal)
