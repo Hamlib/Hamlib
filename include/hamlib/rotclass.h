2fd02a9adee91fedb7fb0c51cfc16c962695a833
Nate Bargmann <n0nb@n0nb.us>
2011-08-20 20:54:17 -0500

    Update LGPL header in include header files.
    TNX to Lucian Laga, YO6PLB, for the notification.

--- include/hamlib/rotclass.h
+++ include/hamlib/rotclass.h
@@ -2,21 +2,20 @@
  *  Hamlib C++ bindings - rotator API header
  *  Copyright (c) 2002 by Stephane Fillod
  *
- *		$Id: rotclass.h,v 1.1 2002-01-16 22:43:29 fillods Exp $
  *
- *   This library is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU Library General Public License as
- *   published by the Free Software Foundation; either version 2 of
- *   the License, or (at your option) any later version.
+ *   This library is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU Lesser General Public
+ *   License as published by the Free Software Foundation; either
+ *   version 2.1 of the License, or (at your option) any later version.
  *
- *   This program is distributed in the hope that it will be useful,
+ *   This library is distributed in the hope that it will be useful,
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU Library General Public License for more details.
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *   Lesser General Public License for more details.
  *
- *   You should have received a copy of the GNU Library General Public
+ *   You should have received a copy of the GNU Lesser General Public
  *   License along with this library; if not, write to the Free Software
- *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  *
  */
 
@@ -59,7 +58,7 @@ public:
 
   void move(int direction, int speed);
 };
- 
+
 
 
 #endif	// _ROTCLASS_H
--- include/num_stdio.h
+++ include/num_stdio.h
@@ -2,21 +2,20 @@
  *  Hamlib Interface - numeric locale wrapping helpers
  *  Copyright (c) 2009 by Stephane Fillod
  *
- *	$Id: bandplan.h,v 1.1 2002-11-16 14:05:16 fillods Exp $
  *
- *   This library is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU Library General Public License as
- *   published by the Free Software Foundation; either version 2 of
- *   the License, or (at your option) any later version.
+ *   This library is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU Lesser General Public
+ *   License as published by the Free Software Foundation; either
+ *   version 2.1 of the License, or (at your option) any later version.
  *
- *   This program is distributed in the hope that it will be useful,
+ *   This library is distributed in the hope that it will be useful,
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU Library General Public License for more details.
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *   Lesser General Public License for more details.
  *
- *   You should have received a copy of the GNU Library General Public
+ *   You should have received a copy of the GNU Lesser General Public
  *   License along with this library; if not, write to the Free Software
- *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  *
  */
 
@@ -25,8 +24,8 @@
 
 #include <locale.h>
 
-/* 
- * This header file is internal to Hamlib and its backends, 
+/*
+ * This header file is internal to Hamlib and its backends,
  * thus not part of the API.
  */
 
