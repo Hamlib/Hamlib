f3e732ff94975536897a92add21932167d3ed959
Nate Bargmann <n0nb@n0nb.us>
2012-02-03 21:45:24 -0600

    Use autoreconf to generate configure script.
    Later Autotools documentation prefers the use of autoreconf to generate or
    regenerate the configure script.  The autoreconf program is responsible
    for running the various autotools in a correct order depending on the
    macros used in configure.ac.

--- autogen.sh
+++ autogen.sh
@@ -1,32 +1,17 @@
 #!/bin/sh
-# Run this to generate or regenerate all the initial makefiles, etc.
+
+# Run this to generate all the initial makefiles, etc.
+
+# Enabling maintainer mode, as done when configure is run by this script,
+# will cause the makefiles to be regenerated if any of the Makefile.am
+# or configure.ac files are changed.
+
 # Taken from glib CVS
 
 # Change the following to match the versions installed on your system
 LIBTOOLIZE=libtoolize
 AUTOCONF=autoconf
-AUTOHEADER=autoheader
 AUTOMAKE=automake
-ACLOCAL=aclocal
-#
-# FreeBSD 6.2 uses this
-#
-#AUTOCONF=autoconf259
-#AUTOHEAD=autoheader259
-#AUTOMAKE=automake19
-#ACLOCAL=aclocal19
-#
-# Debian etch uses this
-#
-#AUTOCONF=autoconf
-#AUTOHEADER=autoheader
-#AUTOMAKE=automake-1.9
-#ACLOCAL=aclocal-1.9
-
-# Needed on Gentoo
-export WANT_AUTOCONF_2_5	#  2.54 or higher, not 2.53a or 2.13
-
-ACLOCAL_FLAGS="$ACLOCAL_FLAGS -I macros"
 
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
@@ -43,15 +28,13 @@ DIE=0
         echo
         echo "You must have autoconf installed to compile $PROJECT."
         echo "Download the appropriate package for your distribution,"
-        echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
         DIE=1
 }
 
 ($AUTOMAKE --version) < /dev/null > /dev/null 2>&1 || {
         echo
         echo "You must have automake installed to compile $PROJECT."
-        echo "Get ftp://sourceware.cygnus.com/pub/automake/automake-1.5.tar.gz"
-        echo "(or a newer version if it is available)"
+        echo "Download the appropriate package for your distribution,"
         DIE=1
 }
 
@@ -71,24 +54,23 @@ test $TEST_TYPE $FILE || {
         exit 1
 }
 
+###################################################################
+### autoreconf is now the preferred way to process configure.ac ###
+### which should handle compiler variations and ensures that    ###
+### subtools are processed in the correct order.                ###
+###################################################################
+
+echo "Running 'autoreconf -is' to process configure.ac"
+echo "and generate the configure script."
+
+# Tell autoreconf to install missing files as symbolic links
+autoreconf -is
+
+cd $ORIGDIR
+
 if test -z "$*"; then
         echo "I am going to run ./configure with no arguments - if you wish "
         echo "to pass any to it, please specify them on the $0 command line."
 fi
-
-# Are we looking for the compiler on a foreign system?
-case $CC in
-*xlc | *xlc\ * | *lcc | *lcc\ *) am_opt=--include-deps;;
-esac
-
-$ACLOCAL $ACLOCAL_FLAGS
-
-# optionally feature autoheader
-($AUTOHEADER --version)  < /dev/null > /dev/null 2>&1 && $AUTOHEADER
-
-$LIBTOOLIZE $ltz_opt
-$AUTOMAKE -a $am_opt
-$AUTOCONF
-cd $ORIGDIR
 
 $srcdir/configure --enable-maintainer-mode "$@"
