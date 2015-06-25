0f35adda2e3fd21c6dd7920494aadc5a9cbbf689
Bill Somerville <bill@classdesign.com>
2015-06-25 10:25:39 +0100

    Make autogen.sh agnostic to whitespace in paths
    Note that this patch does not enable builds with whitespace in $SRCDIR
    or in `pwd` but it does at least get as far as running configure which
    will fail  with a  meaningful warning about  unsafe characters  in the
    offending paths.

--- autogen.sh
+++ autogen.sh
@@ -14,7 +14,7 @@ AUTORECONF=autoreconf
 AUTOMAKE=automake
 
 # variables below this line should not need modification
-SRCDIR=`dirname $0`
+SRCDIR=`dirname "$0"`
 test -z "$SRCDIR" && SRCDIR=.
 
 ORIGDIR=`pwd`
@@ -26,21 +26,21 @@ FILE=include/hamlib/rig.h
 
 DIE=0
 
-($AUTORECONF --version) < /dev/null > /dev/null 2>&1 || {
+("$AUTORECONF" --version) < /dev/null > /dev/null 2>&1 || {
         echo
         echo "You must have autoreconf installed to compile $PROJECT."
         echo "Download the appropriate package for your distribution,"
         DIE=1
 }
 
-($AUTOMAKE --version) < /dev/null > /dev/null 2>&1 || {
+("$AUTOMAKE" --version) < /dev/null > /dev/null 2>&1 || {
         echo
         echo "You must have automake installed to compile $PROJECT."
         echo "Download the appropriate package for your distribution,"
         DIE=1
 }
 
-($LIBTOOLIZE --version) < /dev/null > /dev/null 2>&1 || {
+("$LIBTOOLIZE" --version) < /dev/null > /dev/null 2>&1 || {
         echo
         echo "You must have libtool installed to compile $PROJECT."
         echo "Download the appropriate package for your distribution."
@@ -51,7 +51,7 @@ if test "$DIE" -eq 1; then
         exit 1
 fi
 
-cd $SRCDIR
+cd "$SRCDIR"
 
 test $TEST_TYPE $FILE || {
         echo "You must run this script in the top-level $PROJECT directory"
@@ -68,13 +68,13 @@ echo "Running '$AUTORECONF -i' to process configure.ac"
 echo "and generate the configure script."
 
 # Tell autoreconf to install needed build system files
-$AUTORECONF -i
+"$AUTORECONF" -i
 
-cd $ORIGDIR
+cd "$ORIGDIR"
 
 if test -z "$*"; then
         echo "I am going to run ./configure with no arguments - if you wish "
-        echo "to pass any to it, please specify them on the $0 command line."
+        echo "to pass any to it, please specify them on the \"$0\" command line."
 fi
 
-$SRCDIR/configure "$@"
+"$SRCDIR/configure" "$@"
