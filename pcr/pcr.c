Hamlib/Hamlib

commit 222ad74de7ab7e2894522101643265c36188e9c5

Add RIG_PASSBAND_NOCHANGE as possible set_{split_,split_freq_,}mode argument
This change  allows the set_mode  functions to leave the  rig passband
unchanged  if required.  For the  few rigs  that do  not have  explcit
passband width control either current state is read and rewritten or a
"normal" width  is chosen e.g. select  a normal width when  there is a
choice like CW and CW-NARROW.

pcr/pcr.c
@@ -757,46 +757,52 @@ pcr_set_mode(RIG * rig, vfo_t vfo, rmode_t mode, pbwidth_t width)
 		return -RIG_EINVAL;
 	}
 
-	if (width == RIG_PASSBAND_NORMAL)
-		width = rig_passband_normal(rig, mode);
+	if (width != RIG_PASSBAND_NOCHANGE) {
+		if (width == RIG_PASSBAND_NORMAL)
+			width = rig_passband_normal(rig, mode);
 
-	rig_debug(RIG_DEBUG_VERBOSE, "%s: will set to %d\n",
-		  __func__, width);
+		rig_debug(RIG_DEBUG_VERBOSE, "%s: will set to %d\n",
+							__func__, width);
 
-	switch (width) {
-		/* nop, pcrfilter already set
-		 * TODO: use rig_passband_normal instead?
-		 */
-	case s_kHz(2.8):
-		pcrfilter = FLT_2_8kHz;
-		break;
-	case s_kHz(6):
-		pcrfilter = FLT_6kHz;
-		break;
-	case s_kHz(15):
-		pcrfilter = FLT_15kHz;
-		break;
-	case s_kHz(50):
-		pcrfilter = FLT_50kHz;
-		break;
-	case s_kHz(230):
-		pcrfilter = FLT_230kHz;
-		break;
-	default:
-		rig_debug(RIG_DEBUG_ERR, "%s: unsupported width %d\n",
-			  __func__, width);
-		return -RIG_EINVAL;
+		switch (width) {
+			/* nop, pcrfilter already set
+			 * TODO: use rig_passband_normal instead?
+			 */
+		case s_kHz(2.8):
+			pcrfilter = FLT_2_8kHz;
+			break;
+		case s_kHz(6):
+			pcrfilter = FLT_6kHz;
+			break;
+		case s_kHz(15):
+			pcrfilter = FLT_15kHz;
+			break;
+		case s_kHz(50):
+			pcrfilter = FLT_50kHz;
+			break;
+		case s_kHz(230):
+			pcrfilter = FLT_230kHz;
+			break;
+		default:
+			rig_debug(RIG_DEBUG_ERR, "%s: unsupported width %d\n",
+								__func__, width);
+			return -RIG_EINVAL;
+		}
+
+		rig_debug(RIG_DEBUG_VERBOSE, "%s: filter set to %d (%c)\n",
+							__func__, width, pcrfilter);
+
+		buf_len = sprintf((char *) buf, "K%c%010" PRIll "0%c0%c00",
+											is_sub_rcvr(rig, vfo) ? '1':'0',
+											(int64_t) rcvr->last_freq, pcrmode, pcrfilter);
 	}
-
-	rig_debug(RIG_DEBUG_VERBOSE, "%s: filter set to %d (%c)\n",
-		  __func__, width, pcrfilter);
-
-	buf_len = sprintf((char *) buf, "K%c%010" PRIll "0%c0%c00",
-			is_sub_rcvr(rig, vfo) ? '1':'0',
-			(int64_t) rcvr->last_freq, pcrmode, pcrfilter);
+	else {
+		buf_len = sprintf((char *) buf, "K%c%010" PRIll "0%c0%c00",
+											is_sub_rcvr(rig, vfo) ? '1':'0',
+											(int64_t) rcvr->last_freq, pcrmode, rcvr->last_filter);
+	}
 	if (buf_len < 0)
 		return -RIG_ETRUNC;
-
 	err = pcr_transaction(rig, (char *) buf);
 	if (err != RIG_OK)
 		return err;

adat/adat.c
@@ -2962,10 +2962,12 @@ int adat_set_mode(RIG *pRig, vfo_t vfo, rmode_t mode, pbwidth_t width)
 		pPriv->nRIGMode    = mode;
 		nRC = adat_vfo_rnr2anr(vfo, &(pPriv->nCurrentVFO));
 
-		if (width == RIG_PASSBAND_NORMAL)
-			width = rig_passband_normal(pRig, mode);
+		if (width != RIG_PASSBAND_NOCHANGE) {
+			if (width == RIG_PASSBAND_NORMAL)
+				width = rig_passband_normal(pRig, mode);
 
-		pPriv->nWidth = width;
+			pPriv->nWidth = width;
+		}
 
 		nRC = adat_transaction(pRig, &adat_cmd_list_set_mode);
 	}

alinco/alinco.c
@@ -292,6 +292,8 @@ int alinco_set_mode(RIG *rig, vfo_t vfo, rmode_t mode, pbwidth_t width)
 		if (retval != RIG_OK)
 			return retval;
 
+		if (width == RIG_PASSBAND_NOCHANGE) return retval;
+
 		/*
 		 * TODO: please DX77 owners, check this, I'm not sure
 		 * 			which passband is default!

aor/aor.c
@@ -299,55 +299,57 @@ int format8k_mode(RIG *rig, char *buf, rmode_t mode, pbwidth_t width)
 
 	switch (mode) {
 	case RIG_MODE_AM:
-        if (rig->caps->rig_model == RIG_MODEL_AR8000)
-        {
-            aormode = AR8K_AM;
-        }
-        else
-        {
-		switch(width) {
-			case RIG_PASSBAND_NORMAL:
-			case s_kHz(9): aormode = AR8K_AM; break;
+		if (rig->caps->rig_model == RIG_MODEL_AR8000)
+			{
+				aormode = AR8K_AM;
+			}
+		else
+			{
+				switch(width) {
+				case RIG_PASSBAND_NORMAL:
+				case s_kHz(9): aormode = AR8K_AM; break;
 
-			case s_kHz(12): aormode = AR8K_WAM; break;
-			case s_kHz(3): aormode = AR8K_NAM; break;
-			default:
-				rig_debug(RIG_DEBUG_ERR,
-					"%s: unsupported passband %d %d\n",
-					__FUNCTION__,
-					mode, width);
-			return -RIG_EINVAL;
-		}
-        }
+				case s_kHz(12): aormode = AR8K_WAM; break;
+				case s_kHz(3): aormode = AR8K_NAM; break;
+				case RIG_PASSBAND_NOCHANGE: aormode = AR8K_AM; break;
+				default:
+					rig_debug(RIG_DEBUG_ERR,
+										"%s: unsupported passband %d %d\n",
+										__FUNCTION__,
+										mode, width);
+					return -RIG_EINVAL;
+				}
+			}
 		break;
 	case RIG_MODE_CW:       aormode = AR8K_CW; break;
 	case RIG_MODE_USB:      aormode = AR8K_USB; break;
 	case RIG_MODE_LSB:      aormode = AR8K_LSB; break;
 	case RIG_MODE_WFM:      aormode = AR8K_WFM; break;
 	case RIG_MODE_FM:
-        if (rig->caps->rig_model == RIG_MODEL_AR8000)
-        {
-            aormode = AR8K_NFM;
-        }
-        else
-        {
-		switch(width) {
-			case RIG_PASSBAND_NORMAL:
-			case s_kHz(12): aormode = AR8K_NFM; break;
+		if (rig->caps->rig_model == RIG_MODEL_AR8000)
+			{
+				aormode = AR8K_NFM;
+			}
+		else
+			{
+				switch(width) {
+				case RIG_PASSBAND_NORMAL:
+				case s_kHz(12): aormode = AR8K_NFM; break;
 
-			case s_kHz(9): aormode = AR8K_SFM; break;
-			default:
-				rig_debug(RIG_DEBUG_ERR,
-					"%s: unsupported passband %d %d\n",
-					__FUNCTION__,
-					mode, width);
-			return -RIG_EINVAL;
-            }
-		}
+				case s_kHz(9): aormode = AR8K_SFM; break;
+				case RIG_PASSBAND_NOCHANGE: aormode = AR8K_NFM; break;
+				default:
+					rig_debug(RIG_DEBUG_ERR,
+										"%s: unsupported passband %d %d\n",
+										__FUNCTION__,
+										mode, width);
+					return -RIG_EINVAL;
+				}
+			}
 		break;
 	default:
 		rig_debug(RIG_DEBUG_ERR,"%s: unsupported mode %d\n",
-				__FUNCTION__, mode);
+							__FUNCTION__, mode);
 		return -RIG_EINVAL;
 	}
 

aor/ar3030.c
@@ -379,9 +379,14 @@ int ar3030_set_mode(RIG *rig, vfo_t vfo, rmode_t mode, pbwidth_t width)
 		return -RIG_EINVAL;
 	}
 
-	mdbuf_len = sprintf(mdbuf, "%dB%c" EOM,
-				width < rig_passband_normal(rig,mode) ? 1 : 0,
-	 			aormode);
+	if (width != RIG_PASSBAND_NOCHANGE) {
+		mdbuf_len = sprintf(mdbuf, "%c" EOM, aormode);
+	}
+	else {
+		mdbuf_len = sprintf(mdbuf, "%dB%c" EOM,
+												width < rig_passband_normal(rig,mode) ? 1 : 0,
+												aormode);
+	}
 	retval = ar3030_transaction (rig, mdbuf, mdbuf_len, NULL, NULL);
 
 	return retval;

aor/ar5000.c
@@ -396,9 +396,9 @@ const struct rig_caps ar5000a_caps = {
 
 int format5k_mode(RIG *rig, char *buf, rmode_t mode, pbwidth_t width)
 {
-    int aormode, aorwidth;
+  int aormode, aorwidth;
 
-    switch (mode) {
+  switch (mode) {
 	case RIG_MODE_AM:  aormode = AR5K_AM; break;
 	case RIG_MODE_WFM:
 	case RIG_MODE_FM:  aormode = AR5K_FM; break;
@@ -409,29 +409,34 @@ int format5k_mode(RIG *rig, char *buf, rmode_t mode, pbwidth_t width)
 	case RIG_MODE_SAL: aormode = AR5K_SAL; break;
 	case RIG_MODE_SAH: aormode = AR5K_SAH; break;
 	default:
-	    rig_debug(RIG_DEBUG_ERR,"%s: unsupported mode %d\n",
-		      __FUNCTION__, mode);
-	    return -RIG_EINVAL;
-    }
+    rig_debug(RIG_DEBUG_ERR,"%s: unsupported mode %d\n",
+              __FUNCTION__, mode);
+    return -RIG_EINVAL;
+  }
 
+  if (width != RIG_PASSBAND_NOCHANGE) {
     if (width == RIG_PASSBAND_NORMAL)
-	width = rig_passband_normal(rig, mode);
+      width = rig_passband_normal(rig, mode);
 
     switch (width) {
-	case 500:	 aorwidth = '0'; break;
-	case s_kHz(3):	 aorwidth = '1'; break;
-	case s_kHz(6):	 aorwidth = '2'; break;
-	case s_kHz(15):	 aorwidth = '3'; break;
-	case s_kHz(30):	 aorwidth = '4'; break;
-	case s_kHz(110): aorwidth = '5'; break;
-	case s_kHz(220): aorwidth = '6'; break;
-	default:
-	    rig_debug(RIG_DEBUG_ERR,"%s: unsupported width %d\n",
-		      __FUNCTION__, width);
-	    return -RIG_EINVAL;
+    case 500:	 aorwidth = '0'; break;
+    case s_kHz(3):	 aorwidth = '1'; break;
+    case s_kHz(6):	 aorwidth = '2'; break;
+    case s_kHz(15):	 aorwidth = '3'; break;
+    case s_kHz(30):	 aorwidth = '4'; break;
+    case s_kHz(110): aorwidth = '5'; break;
+    case s_kHz(220): aorwidth = '6'; break;
+    default:
+      rig_debug(RIG_DEBUG_ERR,"%s: unsupported width %d\n",
+                __FUNCTION__, width);
+      return -RIG_EINVAL;
     }
 
     return sprintf(buf, "MD%c BW%c", aormode, aorwidth);
+  }
+  else {
+    return sprintf(buf, "MD%c", aormode);
+  }
 }
 
 

aor/ar7030.c
@@ -364,6 +364,8 @@ static int ar7030_set_mode(RIG *rig, vfo_t vfo, rmode_t mode, pbwidth_t width)
       return -RIG_EINVAL;
   }
 
+  if (RIG_PASSBAND_NOCHANGE == width) return RIG_OK;
+
   if (width == RIG_PASSBAND_NORMAL)
 	  width = rig_passband_normal(rig, mode);
 

aor/ar7030p.c
@@ -572,45 +572,45 @@ static int ar7030p_set_mode( RIG * rig, vfo_t vfo, rmode_t mode,
 
   rc = lockRx( rig, LOCK_1 );
   if ( RIG_OK == rc )
-  {
-    /* TODO - deal with selected VFO */
-    ar_mode = modeToNative( mode );
-
-    rc = writeByte( rig, WORKING, MODE, ar_mode );
-    if ( RIG_OK == rc )
     {
-      if ( RIG_PASSBAND_NORMAL == width )
-      {
-	width = rig_passband_normal( rig, mode );
-      }
-      else
-      {
-	/* TODO - get filter BWs at startup */
-        ar_filter = (unsigned char) 6;
-        for ( i = 1; i <= 6; i++ )
-	{
-          if ( width <= filterTab[ i ] )
-	  {
-            if ( filterTab[ i ] < filterTab[ (int) ar_filter ] )
-	    {
-	      ar_filter = (unsigned char) i;
-	    }
-	  }
+      /* TODO - deal with selected VFO */
+      ar_mode = modeToNative( mode );
 
-	  rig_debug( RIG_DEBUG_VERBOSE, "%s: width %d ar_filter %d filterTab[%d] %d\n",
-                     __func__, width, ar_filter, i, filterTab[i] );
-	}
-      }
+      rc = writeByte( rig, WORKING, MODE, ar_mode );
+      if ( RIG_OK == rc && width != RIG_PASSBAND_NOCHANGE )
+        {
+          if ( RIG_PASSBAND_NORMAL == width )
+            {
+              width = rig_passband_normal( rig, mode );
+            }
+          else
+            {
+              /* TODO - get filter BWs at startup */
+              ar_filter = (unsigned char) 6;
+              for ( i = 1; i <= 6; i++ )
+                {
+                  if ( width <= filterTab[ i ] )
+                    {
+                      if ( filterTab[ i ] < filterTab[ (int) ar_filter ] )
+                        {
+                          ar_filter = (unsigned char) i;
+                        }
+                    }
 
-      rc = writeByte( rig, WORKING, FILTER, ar_filter );
-      if ( RIG_OK == rc )
-      {
-	rc = execRoutine( rig, SET_ALL );
-      }
+                  rig_debug( RIG_DEBUG_VERBOSE, "%s: width %d ar_filter %d filterTab[%d] %d\n",
+                             __func__, width, ar_filter, i, filterTab[i] );
+                }
+            }
+
+          rc = writeByte( rig, WORKING, FILTER, ar_filter );
+          if ( RIG_OK == rc )
+            {
+              rc = execRoutine( rig, SET_ALL );
+            }
+        }
+
+      rc = lockRx( rig, LOCK_0 );
     }
-
-    rc = lockRx( rig, LOCK_0 );
-  }
 
   return( rc );
 }

commit 5c947caf5332d31c1ee6ce6858bb2dd6fde83a2f

Fix a regression introduced in 222ad74

pcr/pcr.c
@@ -795,23 +795,24 @@ pcr_set_mode(RIG * rig, vfo_t vfo, rmode_t mode, pbwidth_t width)
 		buf_len = sprintf((char *) buf, "K%c%010" PRIll "0%c0%c00",
 											is_sub_rcvr(rig, vfo) ? '1':'0',
 											(int64_t) rcvr->last_freq, pcrmode, pcrfilter);
+		if (buf_len < 0)
+			return -RIG_ETRUNC;
+		err = pcr_transaction(rig, (char *) buf);
+		if (err != RIG_OK)
+			return err;
+		rcvr->last_filter = pcrfilter;
 	}
 	else {
 		buf_len = sprintf((char *) buf, "K%c%010" PRIll "0%c0%c00",
 											is_sub_rcvr(rig, vfo) ? '1':'0',
 											(int64_t) rcvr->last_freq, pcrmode, rcvr->last_filter);
+		if (buf_len < 0)
+			return -RIG_ETRUNC;
+		err = pcr_transaction(rig, (char *) buf);
+		if (err != RIG_OK)
+			return err;
 	}
-	if (buf_len < 0)
-		return -RIG_ETRUNC;
-	err = pcr_transaction(rig, (char *) buf);
-	if (err != RIG_OK)
-		return err;
-
-	rig_debug(RIG_DEBUG_VERBOSE, "%s: saving values\n",
-		  __func__);
-
 	rcvr->last_mode = pcrmode;
-	rcvr->last_filter = pcrfilter;
 
 	return RIG_OK;
 }