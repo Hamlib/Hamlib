14a74b1275bb62884df346d2180369c949998594
Nate Bargmann <n0nb@n0nb.us>
2017-02-25 13:17:37 -0600

    Add missing dorji/Android.mk

---
+++ dorji/Android.mk
@@ -0,0 +1,12 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := dorji.c dra818.c
+LOCAL_MODULE := dorji
+
+LOCAL_CFLAGS := -DHAVE_CONFIG_H
+LOCAL_C_INCLUDES := android include src
+LOCAL_LDLIBS := -lhamlib -Lobj/local/armeabi
+
+include $(BUILD_STATIC_LIBRARY)
