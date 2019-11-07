b8da8cd4a80ae2ef9bf82f468652991be64b0573
Michael Black <mdblack98@yahoo.com>
2019-11-07 07:52:56 -0600

    astyle meade and prosistel

--- prosistel/prosistel.c
+++ prosistel/prosistel.c
@@ -44,13 +44,14 @@
 #define STX "\x02"
 
 
-struct prosistel_rot_priv_data {
-	azimuth_t az;
-	elevation_t el;
+struct prosistel_rot_priv_data
+{
+    azimuth_t az;
+    elevation_t el;
 
-	struct timeval tv;	/* time last az/el update */
-	azimuth_t target_az;
-	elevation_t target_el;
+    struct timeval tv;  /* time last az/el update */
+    azimuth_t target_az;
+    elevation_t target_el;
 };
 
 
@@ -69,8 +70,8 @@ struct prosistel_rot_priv_data {
  *   RIG_EIO  -  if an I/O error occurred while sending/receiving data.
  *   RIG_ETIMEOUT  -  if timeout expires without any characters received.
  */
-static int prosistel_transaction (ROT *rot, const char *cmdstr,
-                    char *data, size_t data_len)
+static int prosistel_transaction(ROT *rot, const char *cmdstr,
+                                 char *data, size_t data_len)
 {
     struct rot_state *rs;
     int retval;
@@ -83,37 +84,58 @@ transaction_write:
 
     serial_flush(&rs->rotport);
 
-    if (cmdstr) {
-	retval = write_block(&rs->rotport, cmdstr, strlen(cmdstr));
+    if (cmdstr)
+    {
+        retval = write_block(&rs->rotport, cmdstr, strlen(cmdstr));
+
         if (retval != RIG_OK)
+        {
             goto transaction_quit;
+        }
     }
 
     /* Always read the reply to know whether the cmd went OK */
     if (!data)
+    {
         data = replybuf;
+    }
+
     if (!data_len)
+    {
         data_len = BUFSZ;
+    }
 
 
 
-    memset(data,0,data_len);
+    memset(data, 0, data_len);
     //remember check for STXA,G,R or STXA,?,XXX,R 10 bytes
     retval = read_string(&rs->rotport, data, 20, CR, strlen(CR));
-    if (retval < 0) {
+
+    if (retval < 0)
+    {
         if (retry_read++ < rot->state.rotport.retry)
+        {
             goto transaction_write;
+        }
+
         goto transaction_quit;
     }
 
     //check if reply match issued command
-    if(data[0]==0x02 && data[3]==cmdstr[2]) {
-	rig_debug(RIG_DEBUG_VERBOSE, "%s Command %c reply received\n", __func__, data[3]);
-	retval = RIG_OK;
-	} else {
-	rig_debug(RIG_DEBUG_VERBOSE, "%s Error Command issued: %c doesn't match reply %c\n", __func__, cmdstr[2], data[3]);
-	retval = RIG_EIO;
-	}
+    if (data[0] == 0x02 && data[3] == cmdstr[2])
+    {
+        rig_debug(RIG_DEBUG_VERBOSE, "%s Command %c reply received\n", __func__,
+                  data[3]);
+        retval = RIG_OK;
+    }
+    else
+    {
+        rig_debug(RIG_DEBUG_VERBOSE,
+                  "%s Error Command issued: %c doesn't match reply %c\n", __func__, cmdstr[2],
+                  data[3]);
+        retval = RIG_EIO;
+    }
+
 transaction_quit:
     return retval;
 }
@@ -125,44 +147,47 @@ transaction_quit:
 
 static int prosistel_rot_open(ROT *rot)
 {
-	char cmdstr[64];
-	 int retval;
-  rig_debug(RIG_DEBUG_VERBOSE, "%s called\n", __func__);
-  rot_open(rot);
-  //disable CPM mode - CPM stands for Continuous Position Monitor operating mode
-  //MCU continuously sends position data when CPM enabled
-  num_sprintf(cmdstr, STX"AS"CR);
-  retval = prosistel_transaction(rot, cmdstr, NULL, 0);
-  return retval;
+    char cmdstr[64];
+    int retval;
+    rig_debug(RIG_DEBUG_VERBOSE, "%s called\n", __func__);
+    rot_open(rot);
+    //disable CPM mode - CPM stands for Continuous Position Monitor operating mode
+    //MCU continuously sends position data when CPM enabled
+    num_sprintf(cmdstr, STX"AS"CR);
+    retval = prosistel_transaction(rot, cmdstr, NULL, 0);
+    return retval;
 }
 
 
 
-static int prosistel_rot_set_position(ROT *rot, azimuth_t az, elevation_t el) {
+static int prosistel_rot_set_position(ROT *rot, azimuth_t az, elevation_t el)
+{
 
-	char cmdstr[64];
-	int retval;
+    char cmdstr[64];
+    int retval;
 
-  rig_debug(RIG_DEBUG_VERBOSE,"%s called: %.2f %.2f\n", __func__,
-			az, el);
+    rig_debug(RIG_DEBUG_VERBOSE, "%s called: %.2f %.2f\n", __func__,
+              az, el);
 
-  num_sprintf(cmdstr, STX"AG%03.0f"CR, az);
-  retval = prosistel_transaction(rot, cmdstr, NULL, 0);
-  if(retval!=RIG_OK) {
-	  return retval;
-	  }
+    num_sprintf(cmdstr, STX"AG%03.0f"CR, az);
+    retval = prosistel_transaction(rot, cmdstr, NULL, 0);
 
-  /*
-   * Elevation section: have no hardware to test
-  memset(cmdstr,0,64);
-  num_sprintf(cmdstr, STX"EG%03.0f"CR, el);
-  retval = prosistel_transaction(rot, cmdstr, NULL, 0);
-  if(retval!=RIG_OK) {
-	  return retval;
-	  }
+    if (retval != RIG_OK)
+    {
+        return retval;
+    }
 
-   */
-  return retval;
+    /*
+     * Elevation section: have no hardware to test
+    memset(cmdstr,0,64);
+    num_sprintf(cmdstr, STX"EG%03.0f"CR, el);
+    retval = prosistel_transaction(rot, cmdstr, NULL, 0);
+    if(retval!=RIG_OK) {
+      return retval;
+      }
+
+     */
+    return retval;
 }
 
 
@@ -171,45 +196,56 @@ static int prosistel_rot_set_position(ROT *rot, azimuth_t az, elevation_t el) {
  */
 static int prosistel_rot_get_position(ROT *rot, azimuth_t *az, elevation_t *el)
 {
-	char cmdstr[64];
-	char data[20];
-	float posval;
-	int retval;
+    char cmdstr[64];
+    char data[20];
+    float posval;
+    int retval;
 
-	num_sprintf(cmdstr, STX"A?"CR);
-	retval = prosistel_transaction(rot, cmdstr, data, sizeof(data));
-	if(retval!=RIG_OK) {
-	 return retval;
-	}
-  // Example response  of 100 azimuth
-  // 02 41 2c 3f 2c 31 30 30 30 2c 52 0d   .A,?,1000,R.
-  int n = sscanf(data,"%*cA,?,%f,%*c.",&posval);
-  if (n != 1) {
-	  rig_debug(RIG_DEBUG_ERR, "%s failed to parse azimuth '%s'\n", __func__,data);
-    return RIG_EPROTO;
-  }
-  posval /= 10.0;
-	rig_debug(RIG_DEBUG_VERBOSE, "%s got position from '%s' converted to %d\n", __func__,data,posval);
-	*az = (azimuth_t) posval;
+    num_sprintf(cmdstr, STX"A?"CR);
+    retval = prosistel_transaction(rot, cmdstr, data, sizeof(data));
+
+    if (retval != RIG_OK)
+    {
+        return retval;
+    }
+
+    // Example response  of 100 azimuth
+    // 02 41 2c 3f 2c 31 30 30 30 2c 52 0d   .A,?,1000,R.
+    int n = sscanf(data, "%*cA,?,%f,%*c.", &posval);
+
+    if (n != 1)
+    {
+        rig_debug(RIG_DEBUG_ERR, "%s failed to parse azimuth '%s'\n", __func__, data);
+        return RIG_EPROTO;
+    }
+
+    posval /= 10.0;
+    rig_debug(RIG_DEBUG_VERBOSE, "%s got position from '%s' converted to %d\n",
+              __func__, data, posval);
+    *az = (azimuth_t) posval;
 
 
-	/*
-	 * Elevation section
-	*/
-	num_sprintf(cmdstr, STX"B?"CR);
-	retval = prosistel_transaction(rot, cmdstr, data, sizeof(data));
-  // Example response  of 90 elevation
-  // 02 42 2c 3f 2c 30 39 30 30 2c 52 0d   .B,?,0900,R.
-  n = sscanf(data,"%*cB,?,%f,%*c.",&posval);
-  if (n != 1) {
-	  rig_debug(RIG_DEBUG_ERR, "%s failed to parse elevation '%s'\n", __func__,data);
-    return RIG_EPROTO;
-  }
-  posval /= 10.0;
-	rig_debug(RIG_DEBUG_VERBOSE, "%s got position from '%s' converted to %d\n", __func__,data,posval);
-	*el = (elevation_t) posval/10.0;
+    /*
+     * Elevation section
+    */
+    num_sprintf(cmdstr, STX"B?"CR);
+    retval = prosistel_transaction(rot, cmdstr, data, sizeof(data));
+    // Example response  of 90 elevation
+    // 02 42 2c 3f 2c 30 39 30 30 2c 52 0d   .B,?,0900,R.
+    n = sscanf(data, "%*cB,?,%f,%*c.", &posval);
 
-  return retval;
+    if (n != 1)
+    {
+        rig_debug(RIG_DEBUG_ERR, "%s failed to parse elevation '%s'\n", __func__, data);
+        return RIG_EPROTO;
+    }
+
+    posval /= 10.0;
+    rig_debug(RIG_DEBUG_VERBOSE, "%s got position from '%s' converted to %d\n",
+              __func__, data, posval);
+    *el = (elevation_t) posval / 10.0;
+
+    return retval;
 }
 
 
@@ -220,44 +256,45 @@ static int prosistel_rot_get_position(ROT *rot, azimuth_t *az, elevation_t *el)
  * Prosistel rotator capabilities.
  */
 
-const struct rot_caps prosistel_rot_caps = {
-  .rot_model =      ROT_MODEL_PROSISTEL,
-  .model_name =     "Prosistel D",
-  .mfg_name =       "Prosistel",
-  .version =        "0.4",
-  .copyright = 	    "LGPL",
-  .status =         RIG_STATUS_STABLE,
-  .rot_type =       ROT_TYPE_AZIMUTH,
-  .port_type =      RIG_PORT_SERIAL,
-  .serial_rate_min  = 9600,
-  .serial_rate_max  = 9600,
-  .serial_data_bits = 8,
-  .serial_stop_bits = 1,
-  .serial_parity    = RIG_PARITY_NONE,
-  .serial_handshake = RIG_HANDSHAKE_NONE,
-  .write_delay      = 0,
-  .post_write_delay = 0,
-  .timeout          = 3000,
-  .retry            = 3,
+const struct rot_caps prosistel_rot_caps =
+{
+    .rot_model =      ROT_MODEL_PROSISTEL,
+    .model_name =     "Prosistel D",
+    .mfg_name =       "Prosistel",
+    .version =        "0.4",
+    .copyright =      "LGPL",
+    .status =         RIG_STATUS_STABLE,
+    .rot_type =       ROT_TYPE_AZIMUTH,
+    .port_type =      RIG_PORT_SERIAL,
+    .serial_rate_min  = 9600,
+    .serial_rate_max  = 9600,
+    .serial_data_bits = 8,
+    .serial_stop_bits = 1,
+    .serial_parity    = RIG_PARITY_NONE,
+    .serial_handshake = RIG_HANDSHAKE_NONE,
+    .write_delay      = 0,
+    .post_write_delay = 0,
+    .timeout          = 3000,
+    .retry            = 3,
 
 
-  .min_az =  	0.0,
-  .max_az =  	360.0,
-  .min_el =  	0.0,
-  .max_el =  	90.0,
+    .min_az =     0.0,
+    .max_az =     360.0,
+    .min_el =     0.0,
+    .max_el =     90.0,
 
-  .rot_open =     prosistel_rot_open,
-  .set_position =     prosistel_rot_set_position,
-  .get_position =     prosistel_rot_get_position,
+    .rot_open =     prosistel_rot_open,
+    .set_position =     prosistel_rot_set_position,
+    .get_position =     prosistel_rot_get_position,
 
 };
 
 DECLARE_INITROT_BACKEND(prosistel)
 {
-	rig_debug(RIG_DEBUG_VERBOSE, "prosistel: _init called\n");
+    rig_debug(RIG_DEBUG_VERBOSE, "prosistel: _init called\n");
 
-	rot_register(&prosistel_rot_caps);
+    rot_register(&prosistel_rot_caps);
 
 
-	return RIG_OK;
+    return RIG_OK;
 }
