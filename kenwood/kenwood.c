3220c37ff2a714d4bd8166aeab2751f69e7a52fd
Michael Black <mdblack98@yahoo.com>
2019-12-10 14:50:24 -0600

    Change XOPEN to 600

--- kenwood/kenwood.c
+++ kenwood/kenwood.c
@@ -20,7 +20,7 @@
  *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  *
  */
-#define _XOPEN_SOURCE 500
+#define _XOPEN_SOURCE 600
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
--- tentec/orion.c
+++ tentec/orion.c
@@ -65,7 +65,7 @@
  * This backend supports the Ten-Tec Orion (565) and Orion II (566) transceivers.
  * \n This backend tested mostly with firmware versions 1.372 and 2.062a
  */
-#define _XOPEN_SOURCE 500
+#define _XOPEN_SOURCE 600
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
