41da2b38f8b14cc79721eb87bfd797e366fe125b
Michael Black <mdblack98@yahoo.com>
2019-12-18 22:51:37 -0600

    Fix cppcheck warnings in kenwood.c

--- kenwood/kenwood.c
+++ kenwood/kenwood.c
@@ -2156,7 +2156,7 @@ int get_kenwood_level(RIG *rig, const char *cmd, float *f)
 
     rig_debug(RIG_DEBUG_VERBOSE, "%s called\n", __func__);
 
-    if (!cmd || !f)
+    if (!f)
     {
         return -RIG_EINVAL;
     }
