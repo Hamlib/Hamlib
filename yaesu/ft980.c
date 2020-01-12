eacd00d9c5c5e48ff3550740ddbeab202e015478
Michael Black <mdblack98@yahoo.com>
2020-01-11 23:34:44 -0600

    Fix cppcheck warning in ft980.c

--- yaesu/ft980.c
+++ yaesu/ft980.c
@@ -736,13 +736,13 @@ int ft980_open(RIG *rig)
 
         do
         {
-            retval = write_block(&rig->state.rigport, (char *) cmd_ON_OFF,
+            write_block(&rig->state.rigport, (char *) cmd_ON_OFF,
                                  YAESU_CMD_LENGTH);
             retval = read_block(&rig->state.rigport, (char *) echo_back, YAESU_CMD_LENGTH);
         }
         while (retval != 5 && retry_count2++ < rig->state.rigport.retry);
 
-        retval = write_block(&rig->state.rigport, (char *) cmd_OK, YAESU_CMD_LENGTH);
+        write_block(&rig->state.rigport, (char *) cmd_OK, YAESU_CMD_LENGTH);
         retval = read_block(&rig->state.rigport, (char *) &priv->update_data, 148);
     }
     while (!priv->update_data.EXT_CTL && retry_count1++ < rig->state.rigport.retry);
@@ -765,13 +765,13 @@ int ft980_close(RIG *rig)
 
         do
         {
-            retval = write_block(&rig->state.rigport, (char *) cmd_ON_OFF,
+            write_block(&rig->state.rigport, (char *) cmd_ON_OFF,
                                  YAESU_CMD_LENGTH);
             retval = read_block(&rig->state.rigport, (char *) echo_back, YAESU_CMD_LENGTH);
         }
         while (retval != 5 && retry_count2++ < rig->state.rigport.retry);
 
-        retval = write_block(&rig->state.rigport, (char *) cmd_OK, YAESU_CMD_LENGTH);
+        write_block(&rig->state.rigport, (char *) cmd_OK, YAESU_CMD_LENGTH);
         retval = read_block(&rig->state.rigport, (char *) &priv->update_data, 148);
     }
     while (priv->update_data.EXT_CTL && retry_count1++ < rig->state.rigport.retry);
