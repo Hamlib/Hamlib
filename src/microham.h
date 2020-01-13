272803c64c89cce2268ff87132da333576e51147
Michael Black <mdblack98@yahoo.com>
2020-01-13 07:27:41 -0600

    Add uh_wkey_close to microham.h

--- src/microham.h
+++ src/microham.h
@@ -26,6 +26,7 @@
 
 extern int  uh_open_radio(int baud, int databits, int stopbits, int rtscts);
 extern int  uh_open_ptt();
+extern void uh_close_wkey();
 extern void uh_set_ptt(int ptt);
 extern int  uh_get_ptt();
 extern void uh_close_radio();
