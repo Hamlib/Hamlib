68d8644788fd9a0acf6d5dba0af96dbb9a1683a9
Michael Black <mdblack98@yahoo.com>
2020-01-27 17:05:25 -0600

    Revert "Add CM108 and GPIO DCD types to rigctld.c"
    This reverts commit b101ad15e55997986f35801305c55aafa4139d26.

--- tests/rigctld.c
+++ tests/rigctld.c
@@ -398,18 +398,6 @@ int main(int argc, char *argv[])
             {
                 dcd_type = RIG_DCD_PARALLEL;
             }
-            else if (!strcmp(optarg, "CM108"))
-            {
-                dcd_type = RIG_DCD_CM108;
-            }
-            else if (!strcmp(optarg, "GPIO"))
-            {
-                dcd_type = RIG_DCD_GPIO;
-            }
-            else if (!strcmp(optarg, "GPION"))
-            {
-                dcd_type = RIG_DCD_GPION;
-            }
             else if (!strcmp(optarg, "NONE"))
             {
                 dcd_type = RIG_DCD_NONE;
