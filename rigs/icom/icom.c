e8673161fdd0b8a50a8b5d103b8a7bb88baf5b12
Michael Black <mdblack98@yahoo.com>
2020-03-13 10:09:32 -0500

    Change icom satmode rigs to return to rx_vfo after set_split_freq

--- rigs/icom/icom.c
+++ rigs/icom/icom.c
@@ -3698,11 +3698,11 @@ int icom_set_split_freq(RIG *rig, vfo_t vfo, freq_t tx_freq)
 
     if (VFO_HAS_MAIN_SUB_A_B_ONLY)
     {
-        // Then we return the VFO to where it was
-        if (save_vfo == RIG_VFO_MAIN && priv->split_on) { save_vfo = RIG_VFO_A; }
+        // Then we return the VFO to the rx_vfo
+        save_vfo = rx_vfo; 
 
-        rig_debug(RIG_DEBUG_TRACE, "%s: SATMODE split_on rig so setting vfo to %s\n", __func__,
-                  rig_strvfo(save_vfo));
+        rig_debug(RIG_DEBUG_TRACE, "%s: SATMODE split_on=%d rig so setting vfo to %s\n", __func__,
+                  priv->split_on, rig_strvfo(save_vfo));
 
         if (RIG_OK != (rc = icom_set_vfo(rig, save_vfo)))
         {
