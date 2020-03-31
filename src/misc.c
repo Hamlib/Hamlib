9ad4f2638a2ec604ca2f76ce2328f18a8355974f
Michael Black <mdblack98@yahoo.com>
2020-03-31 16:10:43 -0500

    Remove some verbosity from rig_strscan

--- src/misc.c
+++ src/misc.c
@@ -1016,7 +1016,8 @@ const char *HAMLIB_API rig_strscan(scan_t rscan)
 {
     int i;
 
-    rig_debug(RIG_DEBUG_VERBOSE, "%s called\n", __func__);
+// too verbose
+//    rig_debug(RIG_DEBUG_VERBOSE, "%s called\n", __func__);
 
     if (rscan == RIG_SCAN_NONE)
     {
