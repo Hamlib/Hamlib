862ae4e0da48ee04869e00b57433c7cbd5e7cc13
Michael Black <mdblack98@yahoo.com>
2020-04-14 08:38:11 -0500

    Add kenwood_close where missing

--- rigs/kenwood/ts680.c
+++ rigs/kenwood/ts680.c
@@ -179,6 +179,7 @@ const struct rig_caps ts680s_caps =
 
     .rig_init = kenwood_init,
     .rig_open = kenwood_open, // we don't know the ID for this rig
+    .rig_close = kenwood_close,
     .rig_cleanup = kenwood_cleanup,
     .set_freq =  kenwood_set_freq,
     .get_freq =  kenwood_get_freq,
