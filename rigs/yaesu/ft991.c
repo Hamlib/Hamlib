608c1d0a2be5e5f0336212425eec76d3b9168848
mdblack98 <mdblack98@yahoo.com>
2020-05-05 17:30:36 -0500

    Add FM mode to ft991.c https://github.com/Hamlib/Hamlib/issues/239

--- rigs/yaesu/ft991.c
+++ rigs/yaesu/ft991.c
@@ -55,7 +55,7 @@ const struct rig_caps ft991_caps =
     RIG_MODEL(RIG_MODEL_FT991),
     .model_name =         "FT-991",
     .mfg_name =           "Yaesu",
-    .version =            NEWCAT_VER ".0",
+    .version =            NEWCAT_VER ".1",
     .copyright =          "LGPL",
     .status =             RIG_STATUS_STABLE,
     .rig_type =           RIG_TYPE_TRANSCEIVER,
