8d376121d69450d4875d780f4675ec7f4601ddda
Blaine Kubesh <bkubesh@kubesh.org>
2020-05-15 20:27:44 -0400

    added Icom803 to icmarine

--- rigs/icmarine/icmarine.h
+++ rigs/icmarine/icmarine.h
@@ -73,5 +73,6 @@ int icmarine_get_conf(RIG *rig, token_t token, char *val);
 extern const struct rig_caps icm700pro_caps;
 extern const struct rig_caps icm710_caps;
 extern const struct rig_caps icm802_caps;
+extern const struct rig_caps icm803_caps;
 
 #endif /* _ICMARINE_H */
