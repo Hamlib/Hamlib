a3fed7629bba4b04562d8cdc4e80fceaba465708
Michael Black W9MDB <mdblack98@yahoo.com>
2020-06-13 07:51:35 -0500

    Add some more rig cases for MainA/B and Sub A/B priv setting in icom_get_freq Add VFO_CURR as a valid VFO https://github.com/Hamlib/Hamlib/issues/299

--- rigs/icom/icom.c
+++ rigs/icom/icom.c
@@ -1032,9 +1032,9 @@ int icom_set_freq(RIG *rig, vfo_t vfo, freq_t freq)
     case RIG_VFO_SUB_B:
     case RIG_VFO_B: priv->vfob_freq = freq; break;
 
-    case RIG_VFO_MAIN: priv->sub_freq = freq; break;
+    case RIG_VFO_MAIN: priv->main_freq = freq; break;
 
-    case RIG_VFO_SUB: priv->main_freq = freq; break;
+    case RIG_VFO_SUB: priv->sub_freq = freq; break;
 
     case RIG_VFO_CURR: break; 
 
@@ -1229,13 +1229,19 @@ int icom_get_freq(RIG *rig, vfo_t vfo, freq_t *freq)
 
     switch (vfo)
     {
+    case RIG_VFO_MAIN_A:
+    case RIG_VFO_SUB_A:
     case RIG_VFO_A: priv->vfoa_freq = *freq; break;
 
+    case RIG_VFO_MAIN_B:
+    case RIG_VFO_SUB_B:
     case RIG_VFO_B: priv->vfob_freq = *freq; break;
 
     case RIG_VFO_MAIN: priv->main_freq = *freq; break;
 
     case RIG_VFO_SUB: priv->sub_freq = *freq; break;
+
+    case RIG_VFO_CURR: break; 
 
     default:
         rig_debug(RIG_DEBUG_ERR, "%s: unknown VFO?  VFO=%s\n", __func__,
