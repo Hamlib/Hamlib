63357389d673d031b3a37acc5bba374a93920ed2
hiteki <tbcode@yahoo.com>
2020-06-21 19:10:21 +0200

    get_dcd responds without crashing #323

--- rigs/uniden/uniden.c
+++ rigs/uniden/uniden.c
@@ -229,8 +229,8 @@ transaction_write:
 #endif
 
     /* Special case for SQuelch */
-    if (replystr && !memcmp(cmdstr, "SQ", 2) && (replystr[0] == '-'
-            || replystr[0] == '+'))
+    if (replystr && !memcmp(cmdstr, "SQ", 2) && (data[0] == '-'
+            || data[0] == '+'))
     {
         retval = RIG_OK;
         goto transaction_quit;
