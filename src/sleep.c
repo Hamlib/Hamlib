65812b337fa6de97ec646a4670f76b0a45b97637
Michael Black W9MDB <mdblack98@yahoo.com>
2020-07-04 11:33:11 -0500

    Compile sleep replacement only when HAVE_SLEEP is not defined https://github.com/Hamlib/Hamlib/issues/336

--- src/sleep.c
+++ src/sleep.c
@@ -66,6 +66,7 @@ int hl_usleep(rig_useconds_t usec)
 }
 
 #ifdef HAVE_NANOSLEEP
+#ifndef HAVE_SLEEP
 /**
  * \brief sleep
  * \param secs is seconds to sleep
@@ -83,6 +84,8 @@ unsigned int sleep(unsigned int secs)
 
     return 0;
 }
+#endif
+#endif
 
 
 #if 0
