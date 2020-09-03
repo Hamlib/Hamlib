110cfe7abda137c71026604c4afc19f34ed0f8b5
Michael Black W9MDB <mdblack98@yahoo.com>
2020-09-02 22:13:11 -0500

    Remove warning from ampctl.c

--- tests/ampctl.c
+++ tests/ampctl.c
@@ -49,7 +49,6 @@ extern char *readline();
 #else
 /* no readline */
 #endif                              /* HAVE_LIBREADLINE */
-#warn here1
 #ifdef HAVE_READLINE_HISTORY
 #  include <sys/stat.h>
 #  define HST_SHRT_OPTS "iI"
