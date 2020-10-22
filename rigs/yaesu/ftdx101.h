d340f747c1a2d0283d9512b775931b8d2a56367d
Mikael Nousiainen <mikael.nousiainen@iki.fi>
2020-10-23 00:04:43 +0300

    Add separate definitions for FTDX101 to make fixes for filters and modes. Fix many newcat commands for FTDX101 and some other newcat Yaesu rigs.
    (cherry picked from commit fea5ed875d8c201f83f4a35a4d6b55062f0c2ccd)

--- rigs/yaesu/ftdx101.h
+++ rigs/yaesu/ftdx101.h
@@ -1,11 +1,12 @@
 /*
  * hamlib - (C) Frank Singleton 2000 (javabear at users.sourceforge.net)
  *
- * ft5000.h - (C) Nate Bargmann 2007 (n0nb at arrl.net)
- *           (C) Stephane Fillod 2008-2010
+ * ftdx101.h - (C) Nate Bargmann 2007 (n0nb at arrl.net)
+ *             (C) Stephane Fillod 2008-2010
+ *             (C) Mikael Nousiainen 2020
  *
  * This shared library provides an API for communicating
- * via serial interface to an FT-DX5000 using the "CAT" interface
+ * via serial interface to an FTDX101(D/MP) using the "CAT" interface
  *
  *
  *   This library is free software; you can redistribute it and/or
@@ -25,32 +26,32 @@
  */
 
 
-#ifndef _FT5000_H
-#define _FT5000_H 1
+#ifndef _FTDX101_H
+#define _FTDX101_H 1
 
-#define FTDX5000_VFO_ALL (RIG_VFO_MAIN|RIG_VFO_SUB)
+#define FTDX101_VFO_ALL (RIG_VFO_MAIN|RIG_VFO_SUB)
 
 /* Receiver caps */
 
-#define FTDX5000_ALL_RX_MODES (RIG_MODE_AM|RIG_MODE_CW|RIG_MODE_CWR|RIG_MODE_SSB|\
-        RIG_MODE_RTTY|RIG_MODE_RTTYR|RIG_MODE_PKTLSB|RIG_MODE_PKTUSB|RIG_MODE_PKTFM|\
-        RIG_MODE_FM|RIG_MODE_FMN)
+#define FTDX101_ALL_RX_MODES (RIG_MODE_AM|RIG_MODE_CW|RIG_MODE_CWR|RIG_MODE_SSB|\
+        RIG_MODE_RTTY|RIG_MODE_RTTYR|RIG_MODE_PKTLSB|RIG_MODE_PKTUSB|RIG_MODE_PKTAM|RIG_MODE_PKTFM|\
+        RIG_MODE_FM|RIG_MODE_FMN|RIG_MODE_PKTFMN)
 
-#define FTDX5000_SSB_CW_RX_MODES (RIG_MODE_CW|RIG_MODE_CWR|RIG_MODE_SSB|\
+#define FTDX101_SSB_CW_RX_MODES (RIG_MODE_CW|RIG_MODE_CWR|RIG_MODE_SSB|\
 		RIG_MODE_RTTY|RIG_MODE_RTTYR|RIG_MODE_PKTLSB|RIG_MODE_PKTUSB)
-#define FTDX5000_AM_RX_MODES (RIG_MODE_AM)
-#define FTDX5000_FM_RX_MODES (RIG_MODE_FM|RIG_MODE_PKTFM|RIG_MODE_FMN)
-#define FTDX5000_CW_RTTY_PKT_RX_MODES (RIG_MODE_RTTY|RIG_MODE_RTTYR|\
+#define FTDX101_AM_RX_MODES (RIG_MODE_AM|RIG_MODE_PKTAM)
+#define FTDX101_FM_RX_MODES (RIG_MODE_FM|RIG_MODE_PKTFM|RIG_MODE_FMN|RIG_MODE_PKTFMN)
+#define FTDX101_CW_RTTY_PKT_RX_MODES (RIG_MODE_RTTY|RIG_MODE_RTTYR|\
         RIG_MODE_PKTUSB|RIG_MODE_PKTLSB|RIG_MODE_CW|RIG_MODE_CWR)
 
 /* TRX caps */
 
-#define FTDX5000_OTHER_TX_MODES (RIG_MODE_CW|RIG_MODE_SSB|RIG_MODE_RTTY| \
-		                RIG_MODE_PKTLSB|RIG_MODE_PKTUSB|RIG_MODE_FM|RIG_MODE_PKTFM|RIG_MODE_FMN) /* 100 W class */
-#define FTDX5000_AM_TX_MODES (RIG_MODE_AM)    /* set 25W max */
+#define FTDX101_OTHER_TX_MODES (RIG_MODE_AM|RIG_MODE_CW|RIG_MODE_SSB|RIG_MODE_RTTY| \
+		                RIG_MODE_PKTLSB|RIG_MODE_PKTUSB|RIG_MODE_PKTAM|RIG_MODE_PKTFM|RIG_MODE_FM|RIG_MODE_FMN|RIG_MODE_PKTFMN) /* 100 W class */
+#define FTDX101_AM_TX_MODES (RIG_MODE_AM|RIG_MODE_PKTAM)    /* set 25W max */
 
 /* TBC */
-#define FTDX5000_LEVELS (RIG_LEVEL_ATT|RIG_LEVEL_PREAMP|\
+#define FTDX101_LEVELS (RIG_LEVEL_ATT|RIG_LEVEL_PREAMP|\
                RIG_LEVEL_ALC|RIG_LEVEL_RAWSTR|RIG_LEVEL_SWR|\
                RIG_LEVEL_RFPOWER|RIG_LEVEL_RF|RIG_LEVEL_SQL|\
                RIG_LEVEL_MICGAIN|RIG_LEVEL_IF|RIG_LEVEL_CWPITCH|\
@@ -61,19 +62,18 @@
                RIG_LEVEL_MONITOR_GAIN)
 
 /* TBC */
-#define FTDX5000_FUNCS (RIG_FUNC_TONE|RIG_FUNC_TSQL|RIG_FUNC_LOCK|\
+#define FTDX101_FUNCS (RIG_FUNC_TONE|RIG_FUNC_TSQL|RIG_FUNC_LOCK|\
                RIG_FUNC_MON|RIG_FUNC_NB|RIG_FUNC_NR|RIG_FUNC_VOX|\
                RIG_FUNC_FBKIN|RIG_FUNC_COMP|RIG_FUNC_ANF|RIG_FUNC_MN|\
-               RIG_FUNC_RIT|RIG_FUNC_XIT|\
-               RIG_FUNC_TUNER)
+               RIG_FUNC_RIT|RIG_FUNC_XIT|RIG_FUNC_TUNER)
 
 /* TBC */
-#define FTDX5000_VFO_OPS (RIG_OP_TUNE|RIG_OP_CPY|RIG_OP_XCHG|\
+#define FTDX101_VFO_OPS (RIG_OP_TUNE|RIG_OP_CPY|RIG_OP_XCHG|\
                RIG_OP_UP|RIG_OP_DOWN|RIG_OP_BAND_UP|RIG_OP_BAND_DOWN|\
                RIG_OP_TO_VFO|RIG_OP_FROM_VFO|RIG_OP_TOGGLE)
 
 /* TBC */
-#define FTDX5000_STR_CAL { 16, \
+#define FTDX101_STR_CAL { 16, \
 	       { \
 			{   0, -54 }, /*  S0 */ \
 			{  12, -48 }, /*  S1 */ \
@@ -93,34 +93,29 @@
 			{ 255,  60 }, /* +60 */ \
 		} }
 
-
 /*
  * Other features (used by rig_caps)
- *
  */
 
-#define FTDX5000_TX_ANTS  (RIG_ANT_1|RIG_ANT_2)
+#define FTDX101_TX_ANTS  (RIG_ANT_1|RIG_ANT_2)
 
-#define FTDX5000_MEM_CHNL_LENGTH           1       /* 0x10 P1 = 01 return size */
-#define FTDX5000_OP_DATA_LENGTH            19      /* 0x10 P1 = 03 return size */
-#define FTDX5000_VFO_DATA_LENGTH           18      /* 0x10 P1 = 03 return size -- A & B returned */
-#define FTDX5000_MEM_CHNL_DATA_LENGTH      19      /* 0x10 P1 = 04, P4 = 0x01-0x20 return size */
-#define FTDX5000_STATUS_FLAGS_LENGTH       5       /* 0xf7, 0xfa return size */
-#define FTDX5000_ALL_DATA_LENGTH           649     /* 0x10 P1 = 00 return size */
+#define FTDX101_MEM_CHNL_LENGTH           1       /* 0x10 P1 = 01 return size */
+#define FTDX101_OP_DATA_LENGTH            19      /* 0x10 P1 = 03 return size */
+#define FTDX101_VFO_DATA_LENGTH           18      /* 0x10 P1 = 03 return size -- A & B returned */
+#define FTDX101_MEM_CHNL_DATA_LENGTH      19      /* 0x10 P1 = 04, P4 = 0x01-0x20 return size */
+#define FTDX101_STATUS_FLAGS_LENGTH       5       /* 0xf7, 0xfa return size */
+#define FTDX101_ALL_DATA_LENGTH           649     /* 0x10 P1 = 00 return size */
 
 /* Timing values in mS */
 
-// #define FTDX5000_PACING_INTERVAL                5
-// #define FTDX5000_PACING_DEFAULT_VALUE           0
-
-/* Delay between bytes sent to FT-5000
+/* Delay between bytes sent
  * Should not exceed value set in CAT TOT menu (rig default is 10 mSec)
  */
-#define FTDX5000_WRITE_DELAY                    0
+#define FTDX101_WRITE_DELAY                    0
 
 
 /* Delay sequential fast writes */
 
-#define FTDX5000_POST_WRITE_DELAY               5
+#define FTDX101_POST_WRITE_DELAY               5
 
-#endif /* _FT5000_H */
+#endif /* _FTDX101_H */
--- rigs/yaesu/newcat.h
+++ rigs/yaesu/newcat.h
@@ -50,7 +50,7 @@
 typedef char ncboolean;
 
 /* shared function version */
-#define NEWCAT_VER "20201016"
+#define NEWCAT_VER "20201023"
 
 /* Hopefully large enough for future use, 128 chars plus '\0' */
 #define NEWCAT_DATA_LEN                 129
