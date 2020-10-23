948e9d5f9944ff37ca1a7db4ea98f187b6f50183
Michael Black W9MDB <mdblack98@yahoo.com>
2020-10-23 11:36:42 -0500

    astyle files

--- rigs/yaesu/newcat.c
+++ rigs/yaesu/newcat.c
@@ -4134,6 +4134,7 @@ int newcat_get_func(RIG *rig, vfo_t vfo, setting_t func, int *status)
     priv->ret_data[ret_data_len - 1] = '\0';
 
     rig_debug(RIG_DEBUG_TRACE, "%s: retfunc='%s'\n", __func__, retfunc);
+
     switch (func)
     {
     case RIG_FUNC_MN:
