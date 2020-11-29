39d9b016c15b61e731cf7c4d2237b0b5e5f3332f
Michael Black W9MDB <mdblack98@yahoo.com>
2020-11-28 22:34:15 -0600

    Some notes about microham router
    (cherry picked from commit d782fde5e5a8d522b2b0b96a213f2da512eab76b)

--- src/serial.c
+++ src/serial.c
@@ -195,6 +195,24 @@ int HAMLIB_API serial_open(hamlib_port_t *rp)
          *         export uh_radio_fd to iofunc.c because in the case of sockets,
          *         read() must be used also in the WIN32 case.
          *         This is why uh_radio_fd is declared globally in microham.h.
+         * Notes from Joe Subich about microham behavior
+         * Microham debug tags
+         * A-RX ; Asynchronous data received (data not responsive to any
+         *        poll from host or Router) - Set AI0;
+         * H2-RX;  Data received in response to poll from Host 2 poll (2nd CAT port).
+         * H2-TX;  Data poll/command from Host 2 (2nd CAT Port)
+         * R-RX;  Data received in response to poll by microHAM USB Device Router
+         * R-TX;  Router poll
+         * Note: R-TX; and R-RX; data is not passed to Host ports.
+         * 1) Router only polls when it has not seen a poll for FA; FB; and IF;
+         *    (or equivalent for other manufacturers) within its timeout period.
+         * 2) The results of router's polling are not passed to the Host/apps.
+         * 3) Router only polls when there is no activity from the applications.
+         * 4) Router is designed to be transparent as far as the applications   
+         *    are concerned.  The only exception is when the user chooses to
+         *    run two applications (CAT and 2nd CAT) at the same time and has
+         *    "auto-information" or CI-V enabled.  In that case asynchronous data
+         *    from the transceiver will be returned to both applications.
          */
         uh_radio_fd = fd;
         return RIG_OK;
