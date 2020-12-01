6acb08d62af9d6e2d9e363152230664e435c7a01
Michael Black W9MDB <mdblack98@yahoo.com>
2020-12-01 16:57:10 -0600

    Add 25ms post_write_delay for gs232a/b and a retry when \r\n is received https://github.com/Hamlib/Hamlib/issues/371
    (cherry picked from commit 1beecdd68573ce73eb4981067b22fd03434eacbb)

--- rotators/gs232a/gs232b.c
+++ rotators/gs232a/gs232b.c
@@ -110,6 +110,8 @@ transaction_write:
     retval = read_string(&rs->rotport, data, data_len, REPLY_EOM,
                          strlen(REPLY_EOM));
 
+    if (strncmp(data,"\r\n",2)==0) retval = -1;
+
     if (retval < 0)
     {
         if (retry_read++ < rot->state.rotport.retry)
@@ -341,7 +343,7 @@ const struct rot_caps gs232b_rot_caps =
     .serial_parity = RIG_PARITY_NONE,
     .serial_handshake = RIG_HANDSHAKE_NONE,
     .write_delay = 0,
-    .post_write_delay = 0,
+    .post_write_delay = 25,
     .timeout = 400,
     .retry = 3,
 
