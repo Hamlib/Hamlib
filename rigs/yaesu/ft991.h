51fefa3b7b31f57bf09250b3a09266dad799d343
Michael Black W9MDB <mdblack98@yahoo.com>
2020-12-11 16:38:02 -0600

    Add power level to ft991 and fix ft1200 table https://github.com/Hamlib/Hamlib/issues/462
    (cherry picked from commit 7911fd1d4bd04df0a1d9d737b4817653feca75b2)

--- rigs/yaesu/ft991.h
+++ rigs/yaesu/ft991.h
@@ -71,6 +71,20 @@
                RIG_OP_UP|RIG_OP_DOWN|RIG_OP_BAND_UP|RIG_OP_BAND_DOWN|\
                RIG_OP_TO_VFO|RIG_OP_FROM_VFO)
 
+// Borrowed from FLRig -- Thanks to Dave W1HKJ
+#define FT991_RFPOWER_METER_CAL \
+    { \
+        6, \
+        { \
+            {10, 0.8f}, \
+            {50, 8.0f}, \
+            {100, 26.0f}, \
+            {150, 54.0f}, \
+            {200, 92.0f}, \
+            {250, 140.0f}, \
+        } \
+    }
+
 /* TBC */
 #define FT991_STR_CAL { 16, \
            { \
