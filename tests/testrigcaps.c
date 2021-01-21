39e4aad778699793e7a4136552e28e11e7a2f3ec
Michael Black W9MDB <mdblack98@yahoo.com>
2021-01-21 12:04:46 -0600

    Another attempt to get testrigcaps to recognize compiler differences

--- tests/testrigcaps.c
+++ tests/testrigcaps.c
@@ -13,10 +13,10 @@ int main()
     void *p2 = &rig->state.vfo_list;
     unsigned long offset = p2 - p1;
     printf("offset vfo_list=%ld\n", offset);
-#if defined(__MINGW32__)
-    int expected = 10144; // mingw32
-#elif defined(__MINGW64__)
+#if defined(WIN64) || defined (_WIN64) || defined (__WIN64__)
     int expected = 13264; // mingw64
+#elif defined(WIN32) || defined (_WIN32) || defined(__WIN32__)
+    int expected = 10144; // mingw32
 #else
     int expected = 13280; // should be most 64-bit compilers
 #endif
@@ -32,10 +32,10 @@ int main()
     offset = p2 - p1;
     printf("offset power_max=%ld\n", offset);
 
-#if defined(__MINGW32__)
-    expected = 10448; // mingw32
-#elif defined(__MINGW64__)
+#if defined(WIN64) || defined (_WIN64) || defined (__WIN64__)
     expected = 13664; // mingw64
+#elif defined(WIN32) || defined (_WIN32) || defined(__WIN32__)
+    expected = 10448; // mingw32
 #else
     expected = 13696;
 #endif
