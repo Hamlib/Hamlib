816d9626c85cd4727edc79e98e8054bbdb3ec4b3
Michael Black W9MDB <mdblack98@yahoo.com>
2021-01-31 12:20:44 -0600

    Fix linking error for w32-build.sh script

--- include/hamlib/rig.h
+++ include/hamlib/rig.h
@@ -2857,7 +2857,7 @@ rig_need_debug HAMLIB_PARAMS((enum rig_debug_level_e debug_level));
 
 // this need to be fairly big to avoid compiler warnings
 #define DEBUGMSGSAVE_SIZE 24000
-extern char debugmsgsave[DEBUGMSGSAVE_SIZE];  // last debug msg
+extern HAMLIB_EXPORT_VAR(char) debugmsgsave[DEBUGMSGSAVE_SIZE];  // last debug msg
 #ifndef __cplusplus
 #ifdef __GNUC__
 // doing the debug macro with a dummy sprintf allows gcc to check the format string
