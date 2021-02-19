a05dc2194461364d78f27070cb0728ba1caa1a43
Michael Black W9MDB <mdblack98@yahoo.com>
2021-02-19 14:27:21 -0600

    fix type in ts480 ag_level

--- rigs/kenwood/ts480.c
+++ rigs/kenwood/ts480.c
@@ -115,7 +115,7 @@ kenwood_ts480_set_level(RIG *rig, vfo_t vfo, setting_t level, value_t val)
         return kenwood_set_level(rig, vfo, level, val);
 
     case RIG_LEVEL_AF:
-        priv->ag_format =22;
+        priv->ag_format =2;
         return kenwood_set_level(rig, vfo, level, val);
 
     case RIG_LEVEL_RF:
