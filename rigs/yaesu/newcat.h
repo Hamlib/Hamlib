1a6ffb23d19f57d1e25fedef664d0ec242964ddf
Michael Black W9MDB <mdblack98@yahoo.com>
2021-02-19 07:47:31 -0600

    Update newcat.h version

--- rigs/yaesu/newcat.h
+++ rigs/yaesu/newcat.h
@@ -50,7 +50,7 @@
 typedef char ncboolean;
 
 /* shared function version */
-#define NEWCAT_VER "20210213"
+#define NEWCAT_VER "20210219"
 
 /* Hopefully large enough for future use, 128 chars plus '\0' */
 #define NEWCAT_DATA_LEN                 129
