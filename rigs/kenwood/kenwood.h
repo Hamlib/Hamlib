af5090f38cc6fc3a69007119c0f7743cf5ecb873
Michael Black W9MDB <mdblack98@yahoo.com>
2021-02-26 23:28:38 -0600

    Fix KX3 set_vfo KX4 set_vfo may not work https://github.com/Hamlib/Hamlib/issues/563

--- rigs/kenwood/kenwood.h
+++ rigs/kenwood/kenwood.h
@@ -134,6 +134,12 @@ struct kenwood_priv_data
     int has_rit2;  /* rig has set 2 rit command */
     int ag_format; /* which AG command is being used...see LEVEL_AF in kenwood.c*/
     int micgain_min, micgain_max; /* varies by rig so we figure it out automagically */
+    int is_k2;
+    int is_k3;
+    int is_k3s;
+    int is_kx3;
+    int is_kx2;
+    int is_k4;
 };
 
 
