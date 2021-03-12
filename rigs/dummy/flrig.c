bc283b85ba6d203ae705f0d78c21dcd7e17423c0
Michael Black W9MDB <mdblack98@yahoo.com>
2021-03-11 22:31:37 -0600

    Fix debug statements in flrig.c

--- rigs/dummy/flrig.c
+++ rigs/dummy/flrig.c
@@ -1102,13 +1102,13 @@ static int flrig_set_freq(RIG *rig, vfo_t vfo, freq_t freq)
    if (vfo == RIG_VFO_A)
    {
        cmd = "rig.set_vfoA";
-       rig_debug(RIG_DEBUG_TRACE, "rig.set_vfoA %.0f", freq);
+       rig_debug(RIG_DEBUG_TRACE, "rig.set_vfoA %.0f\n", freq);
        priv->curr_freqA = freq;
    }
    else
    {
        cmd = "rig.set_vfoB";
-       rig_debug(RIG_DEBUG_TRACE, "rig.set_vfoA %.0f", freq);
+       rig_debug(RIG_DEBUG_TRACE, "rig.set_vfoA %.0f\n", freq);
        priv->curr_freqB = freq;
    }
 
