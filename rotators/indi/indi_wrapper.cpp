Hamlib/Hamlib

commit ac1cb518b0dba4d676168448010c93813bee8c40

Fix a few cppcheck warnings

rotators/indi/indi_wrapper.cpp
@@ -58,7 +58,7 @@ int RotINDIClient::setSpeed(int speedPercent)
 
     int speed = DIV_ROUND_UP(speedPercent, 10);
 
-    for (int i = 1; i <= 10; i++)
+    for (unsigned int i = 1; i <= 10; i++)
     {
         char switchName[4];
         snprintf(switchName, sizeof(switchName), "%ux", i);
@@ -519,10 +519,9 @@ void RotINDIClient::serverDisconnected(int exit_code)
 
 const char *RotINDIClient::getInfo(void)
 {
-    static char info[128];
-
     if (mTelescope && mTelescope->isConnected())
     {
+        static char info[128];
         snprintf(info, sizeof(info), "using INDI device %s",
                  mTelescope->getDeviceName());
         return info;

commit a4d14e6f920894c2f62e3c424b7cb54fb81afdb9

Fix warning caused by commit ac1cb518b0dba4d676168448010c93813bee8c40

rotators/indi/indi_wrapper.cpp
@@ -56,7 +56,7 @@ int RotINDIClient::setSpeed(int speedPercent)
         speedPercent = 100;
     }
 
-    int speed = DIV_ROUND_UP(speedPercent, 10);
+    unsigned int speed = DIV_ROUND_UP(speedPercent, 10);
 
     for (unsigned int i = 1; i <= 10; i++)
     {