402fecc43bdc594c30eea37a36054e8f5591b3e2
Mike Black W9MDB <mdblack98@yahoo.com>
2021-04-25 23:19:11 -0500

    Re-do get_rit for has_rit2 rigs If this works should be able to change get_rit in TS480 to the generic kenwood get_rit

--- rigs/kenwood/kenwood.h
+++ rigs/kenwood/kenwood.h
@@ -28,7 +28,7 @@
 #include "token.h"
 #include "misc.h"
 
-#define BACKEND_VER "20210423"
+#define BACKEND_VER "20210425"
 
 #define EOM_KEN ';'
 #define EOM_TH '\r'
@@ -154,6 +154,7 @@ struct kenwood_priv_data
     char last_if_response[KENWOOD_MAX_BUF_LEN];
     int poweron;   /* to avoid powering on more than once */
     int ag_format; /* which AG command is being used...see LEVEL_AF in kenwood.c*/
+    int has_rit2; /* rig has set 2 rit command -- can set rit 0-99999 directly */
     int micgain_min, micgain_max; /* varies by rig so we figure it out automagically */
     int is_k2;
     int is_k3;
