45d1162362c67858e2943ec71104bd12d3be0eee
Mike Black W9MDB <mdblack98@yahoo.com>
2021-08-09 08:46:05 -0500

    Improve debug message in iofunc.c on serial port error

--- src/iofunc.c
+++ src/iofunc.c
@@ -76,7 +76,7 @@ int HAMLIB_API port_open(hamlib_port_t *p)
 
         if (status < 0)
         {
-            rig_debug(RIG_DEBUG_ERR, "%s: serial_open status=%d\n", __func__, status);
+            rig_debug(RIG_DEBUG_ERR, "%s: serial_open(%s) status=%d\n", __func__, p->pathname, status);
             RETURNFUNC(status);
         }
 
