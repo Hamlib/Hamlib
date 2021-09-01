8fc83bd2fc6b36110fa9352d9078ed9ad6de3561
Mike Black W9MDB <mdblack98@yahoo.com>
2021-09-01 09:08:13 -0500

    Really fix icom_set_func so that it can also turn off DUAL_WATCH

--- rigs/icom/icom.c
+++ rigs/icom/icom.c
@@ -6579,7 +6579,7 @@ int icom_set_func(RIG *rig, vfo_t vfo, setting_t func, int status)
 
     case RIG_FUNC_DUAL_WATCH:
         fct_cn = C_SET_VFO;
-        fct_sc = S_DUAL;
+        fct_sc = status ? S_DUAL_ON : S_DUAL_OFF;
         break;
 
     case RIG_FUNC_SATMODE:
