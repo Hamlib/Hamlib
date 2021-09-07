39bb9c4a92410d79aff56891111762a7bdc8a65a
AuroraRAS <chplee@gmail.com>
2021-09-07 21:30:09 +0800

    Append the missing information to build hamlib v4.3 for Android NDK
    The following errors were fixed in this commit:
    error: undefined symbol: rig_sprintf_vfo
    error: undefined symbol: initrots2_radant
    error: undefined symbol: ts890s_caps
    
    (cherry picked from commit b5bd1f4036bb42ae8b5c05900f51cec60bf2502e)

---
+++ rotators/radant/Android.mk
@@ -0,0 +1,12 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := radant.c
+LOCAL_MODULE := radant
+
+LOCAL_CFLAGS := -DHAVE_CONFIG_H
+LOCAL_C_INCLUDES := android include src
+LOCAL_LDLIBS := -lhamlib -Lobj/local/armeabi
+
+include $(BUILD_STATIC_LIBRARY)
