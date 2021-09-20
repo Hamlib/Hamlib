e5aa78c3960c7a197ee86248aa9f18ae9b4fc6ae
Mike Black W9MDB <mdblack98@yahoo.com>
2021-09-19 23:49:06 -0500

    Fix FTDX9000 METER_TEMP Change return codes for meters to RIG_ENAVAIL https://github.com/Hamlib/Hamlib/issues/755

--- rigs/yaesu/newcat.h
+++ rigs/yaesu/newcat.h
@@ -50,7 +50,7 @@
 typedef char ncboolean;
 
 /* shared function version */
-#define NEWCAT_VER "20210805"
+#define NEWCAT_VER "20210919"
 
 /* Hopefully large enough for future use, 128 chars plus '\0' */
 #define NEWCAT_DATA_LEN                 129
