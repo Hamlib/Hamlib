05f58cd978c9741bec94118a4d3ffdf4f11502f2
AuroraRAS <chplee@gmail.com>
2021-09-25 07:12:08 +0800

    add androidsensor rotator
    The androidsensor rotator is not a real rotator, it uses the accelerometer sensor and magnetic field sensor of the cell phone or tablet to perform attitude determination for your antenna and the phone tied to it.
    Now you can wave your antenna to find radio signals.
    
    Signed-off-by: AuroraRAS <chplee@gmail.com>

---
+++ rotators/androidsensor/Android.mk
@@ -0,0 +1,12 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := androidsensor.cpp ndkimu.cpp
+LOCAL_MODULE := androidsensor
+
+LOCAL_CFLAGS := -DHAVE_CONFIG_H
+LOCAL_C_INCLUDES := android include src
+LOCAL_LDLIBS := -lhamlib -Lobj/local/$(TARGET_ARCH_ABI)
+
+include $(BUILD_STATIC_LIBRARY)
