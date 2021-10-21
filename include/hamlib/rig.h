c9d7a924c79e9b84b126f5cbc9e85c05bc9b8c46
Mike Black W9MDB <mdblack98@yahoo.com>
2021-10-21 17:36:54 -0500

    Add RIG_FUNC_SEND_VOICE_MEM with FT991 as the 1st implementation https://github.com/Hamlib/Hamlib/issues/830

--- include/hamlib/rig.h
+++ include/hamlib/rig.h
@@ -1123,7 +1123,7 @@ typedef uint64_t setting_t;
 #define RIG_FUNC_TRANSCEIVE CONSTANT_64BIT_FLAG (42)   /*!< \c TRANSCEIVE -- Send radio state changes automatically ON/OFF */
 #define RIG_FUNC_SPECTRUM   CONSTANT_64BIT_FLAG (43)   /*!< \c SPECTRUM -- Spectrum scope data output ON/OFF */
 #define RIG_FUNC_SPECTRUM_HOLD CONSTANT_64BIT_FLAG (44)   /*!< \c SPECTRUM_HOLD -- Pause spectrum scope updates ON/OFF */
-#define RIG_FUNC_BIT45      CONSTANT_64BIT_FLAG (45)   /*!< \c available for future RIG_FUNC items */
+#define RIG_FUNC_SEND_VOICE_MEM CONSTANT_64BIT_FLAG (45)   /*!< \c For sending voice memory */
 #define RIG_FUNC_BIT46      CONSTANT_64BIT_FLAG (46)   /*!< \c available for future RIG_FUNC items */
 #define RIG_FUNC_BIT47      CONSTANT_64BIT_FLAG (47)   /*!< \c available for future RIG_FUNC items */
 #define RIG_FUNC_BIT48      CONSTANT_64BIT_FLAG (48)   /*!< \c available for future RIG_FUNC items */
@@ -2060,7 +2060,6 @@ enum rig_function_e {
     RIG_FUNCTION_SEND_MORSE,
     RIG_FUNCTION_STOP_MORSE,
     RIG_FUNCTION_WAIT_MORSE,
-    RIG_FUNCTION_SEND_VOICE_MEM,
     RIG_FUNCTION_SET_BANK,
     RIG_FUNCTION_SET_MEM,
     RIG_FUNCTION_GET_MEM,
@@ -2076,7 +2075,7 @@ enum rig_function_e {
     RIG_FUNCTION_GET_CHAN_ALL_CB,
     RIG_FUNCTION_SET_MEM_ALL_CB,
     RIG_FUNCTION_GET_MEM_ALL_CB,
-    RIG_FUNCTION_SET_VFO_OPT
+    RIG_FUNCTION_SET_VFO_OPT,
 };
 
 /**
