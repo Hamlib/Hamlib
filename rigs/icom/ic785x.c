e63641ddd2c99e2dc4f9a2f87f96170111842efa
Mike Black W9MDB <mdblack98@yahoo.com>
2021-10-21 09:53:09 -0500

    Change IC-785x model string to IC-7850/7851 to better identify for users

--- rigs/icom/ic785x.c
+++ rigs/icom/ic785x.c
@@ -240,7 +240,7 @@ static struct icom_priv_caps ic785x_priv_caps =
 const struct rig_caps ic785x_caps =
 {
     RIG_MODEL(RIG_MODEL_IC785x),
-    .model_name = "IC-785x",
+    .model_name = "IC-7850/7851",
     .mfg_name =  "Icom",
     .version =  BACKEND_VER ".2",
     .copyright =  "LGPL",
