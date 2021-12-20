783ae504a75211afc6864ccab2eb00e96eba4500
Mike Black W9MDB <mdblack98@yahoo.com>
2021-12-20 09:23:01 -0600

    Fix comipile warning in icom.c

--- rigs/icom/icom.c
+++ rigs/icom/icom.c
@@ -8630,7 +8630,7 @@ static int icom_parse_spectrum_frame(RIG *rig, size_t length,
         cache->spectrum_metadata_valid = 1;
 
         rig_debug(RIG_DEBUG_TRACE,
-                  "%s: Spectrum line start: id=%d division=%d max_division=%d mode=%d center=%.0f span=%.0f low_edge=%.0f high_edge=%.0f oor=%d data_length=%ld\n",
+                  "%s: Spectrum line start: id=%d division=%d max_division=%d mode=%d center=%.0f span=%.0f low_edge=%.0f high_edge=%.0f oor=%d data_length=%zd\n",
                   __func__, spectrum_id, division, max_division, spectrum_scope_mode,
                   cache->spectrum_center_freq, cache->spectrum_span_freq,
                   cache->spectrum_low_edge_freq, cache->spectrum_high_edge_freq, out_of_range,
