e15f008c62cbcaafa4959cf7ceb65afdd9935d50
Mike Black W9MDB <mdblack98@yahoo.com>
2021-12-27 12:16:09 -0600

    Add Android.mk to codan

---
+++ rigs/codan/Android.mk
@@ -0,0 +1,12 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := codan.c codan.h
+LOCAL_MODULE := codan
+
+LOCAL_CFLAGS := -DHAVE_CONFIG_H
+LOCAL_C_INCLUDES := android include src
+LOCAL_LDLIBS := -lhamlib -Lobj/local/$(TARGET_ARCH_ABI)
+
+include $(BUILD_STATIC_LIBRARY)
