3f431314d83631ef951647d0ad8a42cee9cb5617
Mike Black W9MDB <mdblack98@yahoo.com>
2022-01-05 13:08:38 -0600

    Fix win32 build

--- src/iofunc.c
+++ src/iofunc.c
@@ -433,6 +433,7 @@ int HAMLIB_API port_close(hamlib_port_t *p, rig_port_t port_type)
 
 extern int is_uh_radio_fd(int fd);
 
+#ifdef ASYNC_BUG
 static int port_read_sync_data_error_code(hamlib_port_t *p)
 {
     ssize_t total_bytes_read = 0;
@@ -573,6 +574,7 @@ static ssize_t port_read_sync_data_pipe(hamlib_port_t *p, void *buf, size_t coun
 {
     return port_read_sync_data(p, buf, count);
 }
+#endif
 
 /* On MinGW32/MSVC/.. the appropriate accessor must be used
  * depending on the port type, sigh.
