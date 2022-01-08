5a9e59da30d800edce8d6ba353cf10b3029fd6b3
Mike Black W9MDB <mdblack98@yahoo.com>
2022-01-08 07:18:53 -0600

    Move Barrett 4050 channel to select 9999 during open

--- rigs/barrett/4050.c
+++ rigs/barrett/4050.c
@@ -87,6 +87,13 @@ static int barrett4050_open(RIG *rig)
     char *response;
     ENTERFUNC;
     barrett4050_get_info(rig);
+    retval = barrett_transaction(rig, "XC9999", 0, &response);
+
+    if (retval == RIG_OK)
+    {
+        rig_debug(RIG_DEBUG_ERR, "%s: result=%s\n", __func__, response);
+    }
+
     retval = barrett_transaction(rig, "IDC", 0, &response);
 
     if (retval == RIG_OK)
