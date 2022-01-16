416fe8f96e18afd03ca364a11f5309b5ee65229a
Mike Black W9MDB <mdblack98@yahoo.com>
2022-01-16 08:01:42 -0600

    Recommit si570avrusb.c -- change didn't take for some reason

--- rigs/kit/si570avrusb.c
+++ rigs/kit/si570avrusb.c
@@ -932,7 +932,6 @@ int si570xxxusb_set_conf(RIG *rig, token_t token, const char *val)
 int si570xxxusb_get_conf2(RIG *rig, token_t token, char *val, int val_len)
 {
     struct si570xxxusb_priv_data *priv;
-
     priv = (struct si570xxxusb_priv_data *)rig->state.priv;
 
     switch (token)
@@ -962,7 +961,7 @@ int si570xxxusb_get_conf2(RIG *rig, token_t token, char *val, int val_len)
 
 int si570xxxusb_get_conf(RIG *rig, token_t token, char *val)
 {
-    return si570xxusb_get_conf2(rig, token, val, 128);
+    return si570xxxusb_get_conf2(rig, token, val, 128);
 }
 
 
