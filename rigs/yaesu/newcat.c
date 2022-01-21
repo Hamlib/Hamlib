e68594f510af228ef7229f07d858ec59b102863a
Mike Black W9MDB <mdblack98@yahoo.com>
2022-01-21 17:02:56 -0600

    Add FTDX-9000 to Yaesu rigs that cannot deal with "VS0;VS;" and end up muting the VFO

--- rigs/yaesu/newcat.c
+++ rigs/yaesu/newcat.c
@@ -10150,7 +10150,8 @@ int newcat_set_cmd_validate(RIG *rig)
     else if ((strncmp(priv->cmd_str, "VS", 2) == 0) && (strlen(priv->cmd_str) > 3))
     {
         strcpy(valcmd, "VS;");
-        if (priv->rig_id == NC_RIGID_FTDX3000)
+        // Some models treat the 2nd VS as a mute request
+        if (is_ftdx3000 || is_ftdx9000)
         {
             strcpy(valcmd, ""); 
         }
