3bf3ed8aa6558941aa2d5ed508a230ded77f13a5
Mike Black W9MDB <mdblack98@yahoo.com>
2022-01-23 08:28:16 -0600

    Remove static mutex in misc.c

--- src/misc.c
+++ src/misc.c
@@ -1934,7 +1934,7 @@ vfo_t HAMLIB_API vfo_fixup(RIG *rig, vfo_t vfo, split_t split)
         rig_debug(RIG_DEBUG_VERBOSE, "%s(%d): split=%d, vfo==%s tx_vfo=%s\n", __func__,
                   __LINE__, split, rig_strvfo(vfo), rig_strvfo(rig->state.tx_vfo));
 
-        if (split && vfo == RIG_VFO_TX) { vfo = rig->state.tx_vfo; }
+        if (vfo == RIG_VFO_TX) { vfo = rig->state.tx_vfo; RETURNFUNC(RIG_OK); }
 
         if (VFO_HAS_MAIN_SUB_ONLY && !split && !satmode && vfo != RIG_VFO_B) { vfo = RIG_VFO_MAIN; }
 
