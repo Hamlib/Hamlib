81bb70bb84368f22d4080604999e88b03cf2107a
Mike Black W9MDB <mdblack98@yahoo.com>
2022-01-26 12:32:04 -0600

    Deprecate hamlib_port_t to hamlib_port_t_deprecated New hamlib_port_t at end of structure -- eventually need to change this to a pointer' Enable ASYNC_BUG now works with new hamlib_port_t Backwards compatible with already-compiled clients and newly-compiled clients https://github.com/Hamlib/Hamlib/issues/894

--- src/rig.c
+++ src/rig.c
@@ -463,7 +463,7 @@ RIG *HAMLIB_API rig_init(rig_model_t rig_model)
               rs->comm_state);
     rs->rigport.type.rig = caps->port_type; /* default from caps */
 #if defined(ASYNC_BUG) && defined(HAVE_PTHREAD)
-    rs->rigport.async = 0;
+    rs->rigport.asyncio = 0;
 #endif
 
     switch (caps->port_type)
@@ -677,6 +677,10 @@ RIG *HAMLIB_API rig_init(rig_model_t rig_model)
             return (NULL);
         }
     }
+    // Now we have to copy our new rig state hamlib_port structure to the deprecated one
+    // Clients built on older 4.X versions will use the old structure
+    // Clients built on newer 4.5 versions will use the new structure
+    memcpy(&rig->state.rigport_deprecated, &rig->state.rigport, sizeof(rig->state.rigport_deprecated));
 
     return (rig);
 }
@@ -721,7 +725,7 @@ int HAMLIB_API rig_open(RIG *rig)
 #if defined(ASYNC_BUG) && defined(HAVE_PTHREAD)
     // Enable async data only if it's enabled through conf settings *and* supported by the backend
     rs->async_data_enabled = rs->async_data_enabled && caps->async_data_supported;
-    rs->rigport.async = rs->async_data_enabled;
+    rs->rigport.asyncio = rs->async_data_enabled;
 #endif
 
     if (strlen(rs->rigport.pathname) > 0)
@@ -823,6 +827,7 @@ int HAMLIB_API rig_open(RIG *rig)
     }
 
     status = port_open(&rs->rigport);
+    memcpy(&rs->rigport_deprecated,&rs->rigport,sizeof(hamlib_port_t_deprecated));
 
     if (status < 0)
     {
@@ -1043,6 +1048,7 @@ int HAMLIB_API rig_open(RIG *rig)
     if (status < 0)
     {
         port_close(&rs->rigport, rs->rigport.type.rig);
+        memcpy(&rs->rigport_deprecated,&rs->rigport,sizeof(hamlib_port_t_deprecated));
         RETURNFUNC(status);
     }
 
@@ -1052,6 +1058,7 @@ int HAMLIB_API rig_open(RIG *rig)
     if (status < 0)
     {
         port_close(&rs->rigport, rs->rigport.type.rig);
+        memcpy(&rs->rigport_deprecated,&rs->rigport,sizeof(hamlib_port_t_deprecated));
         RETURNFUNC(status);
     }
 
@@ -1078,6 +1085,7 @@ int HAMLIB_API rig_open(RIG *rig)
             async_data_handler_stop(rig);
 #endif
             port_close(&rs->rigport, rs->rigport.type.rig);
+            memcpy(&rs->rigport_deprecated,&rs->rigport,sizeof(hamlib_port_t_deprecated));
             rs->comm_state = 0;
             RETURNFUNC(status);
         }
@@ -1214,6 +1222,7 @@ int HAMLIB_API rig_close(RIG *rig)
             if (rs->pttport.fd != rs->rigport.fd)
             {
                 port_close(&rs->pttport, RIG_PORT_SERIAL);
+                memcpy(&rs->rigport_deprecated,&rs->rigport,sizeof(hamlib_port_t_deprecated));
             }
         }
 
@@ -1229,6 +1238,7 @@ int HAMLIB_API rig_close(RIG *rig)
             if (rs->pttport.fd != rs->rigport.fd)
             {
                 port_close(&rs->pttport, RIG_PORT_SERIAL);
+                memcpy(&rs->rigport_deprecated,&rs->rigport,sizeof(hamlib_port_t_deprecated));
             }
         }
 
@@ -1269,6 +1279,7 @@ int HAMLIB_API rig_close(RIG *rig)
         if (rs->dcdport.fd != rs->rigport.fd)
         {
             port_close(&rs->dcdport, RIG_PORT_SERIAL);
+            memcpy(&rs->rigport_deprecated,&rs->rigport,sizeof(hamlib_port_t_deprecated));
         }
 
         break;
@@ -1292,6 +1303,7 @@ int HAMLIB_API rig_close(RIG *rig)
     rs->dcdport.fd = rs->pttport.fd = -1;
 
     port_close(&rs->rigport, rs->rigport.type.rig);
+    memcpy(&rs->rigport_deprecated,&rs->rigport,sizeof(hamlib_port_t_deprecated));
 
     remove_opened_rig(rig);
 
