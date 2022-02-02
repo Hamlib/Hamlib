d5cd99b7f5ee25f6a6885532d7a5390f584fdd79
Mikael Nousiainen <mikael.nousiainen@iki.fi>
2022-02-02 13:15:05 +0200

    Allow async I/O for Icom SDR rigs, mainly for waterfall spectrum data support. Async I/O is still turned off by default and needs to be enabled via a conf option.

--- rigs/icom/ic785x.c
+++ rigs/icom/ic785x.c
@@ -418,6 +418,11 @@ const struct rig_caps ic785x_caps =
     },
     .spectrum_attenuator = { 10, 20, 30, RIG_DBLST_END, },
 
+    .async_data_supported = 1,
+    .read_frame_direct = icom_read_frame_direct,
+    .is_async_frame = icom_is_async_frame,
+    .process_async_frame = icom_process_async_frame,
+
     .cfgparams =  icom_cfg_params,
     .set_conf =  icom_set_conf,
     .get_conf =  icom_get_conf,
