56bd64f5b0bc8f076b6b6216d0209406d2df5144
Mike Black W9MDB <mdblack98@yahoo.com>
2022-02-24 17:34:49 -0600

    Fix compilation on mingw for gmtime_r https://github.com/Hamlib/Hamlib/issues/813

--- security/security.c
+++ security/security.c
@@ -28,6 +28,28 @@
 #include "password.h"
 #include "../src/misc.h"
 
+#if defined(_WIN32)
+// gmtime_r can be defined by mingw
+#ifndef gmtime_r
+static struct tm *gmtime_r(const time_t *t, struct tm *r)
+{
+    // gmtime is threadsafe in windows because it uses TLS
+    struct tm *theTm = gmtime(t);
+
+    if (theTm)
+    {
+        *r = *theTm;
+        return r;
+    }
+    else
+    {
+        return 0;
+    }
+}
+#endif // gmtime_r
+#endif // _WIN32
+
+
 // using tv_usec with a sleep gives a fairly good random number
 static int my_rand(int max)
 {
