1c637a275669aff72a0c35334dbca346f7b8dae6
OK1CTR <OK1CTR@gmail.com>
2022-02-25 21:03:16 +0100

    Added Android.mk, passed make distcheck.

---
+++ rigs/gomspace/Android.mk
@@ -0,0 +1,12 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := gs100.c
+LOCAL_MODULE := gomspace
+
+LOCAL_CFLAGS := 
+LOCAL_C_INCLUDES := android include src
+LOCAL_LDLIBS := -lhamlib -Lobj/local/$(TARGET_ARCH_ABI)
+
+include $(BUILD_STATIC_LIBRARY)
