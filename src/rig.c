4757a23b399295f35deeaaf6326711e1ca0e97bd
Mike Black W9MDB <mdblack98@yahoo.com>
2022-02-25 07:41:10 -0600

    Allow rig_get_vfo_list to work if rig is not open

--- src/rig.c
+++ src/rig.c
@@ -156,6 +156,7 @@ const char hamlib_copyright[231] = /* hamlib 1.2 ABI specifies 231 bytes */
 #define DEFAULT_GPIO_PORT "0"
 
 #define CHECK_RIG_ARG(r) (!(r) || !(r)->caps || !(r)->state.comm_state)
+#define CHECK_RIG_CAPS(r) (!(r) || !(r)->caps)
 
 #define LOCK \
 
@@ -6673,7 +6674,7 @@ int HAMLIB_API rig_get_vfo_list(RIG *rig, char *buf, int buflen)
 {
     ENTERFUNC;
 
-    if (CHECK_RIG_ARG(rig))
+    if (CHECK_RIG_CAPS(rig))
     {
         RETURNFUNC(-RIG_EINVAL);
     }
