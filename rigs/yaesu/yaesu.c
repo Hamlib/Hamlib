e9b8c4f94473fab7456f250fb23b38918d6a399f
Mike Black W9MDB <mdblack98@yahoo.com>
2022-04-15 13:16:09 -0500

    Add FT450D entry

--- rigs/yaesu/yaesu.c
+++ rigs/yaesu/yaesu.c
@@ -73,8 +73,12 @@ DECLARE_INITRIG_BACKEND(yaesu)
 {
     rig_debug(RIG_DEBUG_VERBOSE, "yaesu: %s called\n", __func__);
 
+    ft450d_caps = ft450_caps;
+    ft450d_caps.rig_model = RIG_MODEL_FT450D;
+    ft450d_caps.model_name = "FT-450D";
     rig_register(&ft100_caps);
     rig_register(&ft450_caps);
+    rig_register(&ft450d_caps);
     rig_register(&ft736_caps);
     rig_register(&ft747_caps);
     rig_register(&ft757gx_caps);
