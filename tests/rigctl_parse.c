0c16eddd161e5e1e2a46ad0ae4c09ae9c209a6dc
Mike Black W9MDB <mdblack98@yahoo.com>
2022-05-20 09:06:17 -0500

    Fix MICDATA ptt with rigctld and the few rigs which needs this -- e.g. TS-590, TS-480, TS-890, TS-990. VX-1700. ICF-8101 https://github.com/Hamlib/Hamlib/issues/1033

--- tests/rigctl_parse.c
+++ tests/rigctl_parse.c
@@ -4384,7 +4384,7 @@ declare_proto_rig(dump_state)
     {
         fprintf(fout, "vfo_ops=0x%x\n", rig->caps->vfo_ops);
         fprintf(fout, "ptt_type=0x%x\n",
-                rig->state.pttport.type.ptt == RIG_PTT_NONE ? RIG_PTT_NONE : RIG_PTT_RIG);
+                rig->state.pttport.type.ptt);
         fprintf(fout, "targetable_vfo=0x%x\n", rig->caps->targetable_vfo);
         fprintf(fout, "has_set_vfo=%d\n", rig->caps->set_vfo != NULL);
         fprintf(fout, "has_get_vfo=%d\n", rig->caps->get_vfo != NULL);
