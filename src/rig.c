c91c98447b378a25ab344f4b001be2e8d0992dd9
Mike Black W9MDB <mdblack98@yahoo.com>
2022-06-06 22:58:55 -0500

    Forgot to export lock_mode

--- src/rig.c
+++ src/rig.c
@@ -93,7 +93,7 @@ const char *hamlib_license = "LGPL";
 const char hamlib_version[21] = "Hamlib " PACKAGE_VERSION;
 const char *hamlib_version2 = "Hamlib " PACKAGE_VERSION " " HAMLIBDATETIME;
 HAMLIB_EXPORT_VAR(int) cookie_use;
-int lock_mode; // for use by rigctld
+HAMLIB_EXPORT_VAR(int) lock_mode; // for use by rigctld
 //! @endcond
 
 struct rig_caps caps_test;
