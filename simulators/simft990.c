81f8b03630eb8f0fb913a063160265930c486258
Mike Black W9MDB <mdblack98@yahoo.com>
2022-07-31 07:10:15 -0500

    Add simft990

---
+++ simulators/simft990.c
@@ -0,0 +1,166 @@
+// can run this using rigctl/rigctld and socat pty devices
+// gcc -o simft990 simft990.c
+// emulates 1.2 ROM FT990 which can only read 1492 bytes
+#define _XOPEN_SOURCE 600
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+#include "../include/hamlib/rig.h"
+
+#define BUFSIZE 256
+
+float freqA = 14074000;
+float freqB = 14074500;
+char tx_vfo = '0';
+char rx_vfo = '0';
+char modeA = '1';
+char modeB = '1';
+int width_main = 500;
+int width_sub = 700;
+
+
+// ID 0310 == 310, Must drop leading zero
+typedef enum nc_rigid_e
+{
+    NC_RIGID_NONE            = 0,
+    NC_RIGID_FT450           = 241,
+    NC_RIGID_FT450D          = 244,
+    NC_RIGID_FT950           = 310,
+    NC_RIGID_FT891           = 135,
+    NC_RIGID_FT991           = 135,
+    NC_RIGID_FT2000          = 251,
+    NC_RIGID_FT2000D         = 252,
+    NC_RIGID_FTDX1200        = 583,
+    NC_RIGID_FTDX9000D       = 101,
+    NC_RIGID_FTDX9000Contest = 102,
+    NC_RIGID_FTDX9000MP      = 103,
+    NC_RIGID_FTDX5000        = 362,
+    NC_RIGID_FTDX3000        = 460,
+    NC_RIGID_FTDX101D        = 681,
+    NC_RIGID_FTDX101MP       = 682
+} nc_rigid_t;
+
+void load_dat(char *filename, unsigned char buf[1492])
+{
+    FILE *fp = fopen(filename, "r");
+    char line[4096];
+    int n = 0;
+
+    while (fgets(line, sizeof(line), fp))
+    {
+        char *s = strdup(line);
+        unsigned int val;
+        char *p = strtok(line, " \r\n");
+
+        do
+        {
+            sscanf(p, "%x", &val);
+            buf[n++] = val;
+        }
+        while (p = strtok(NULL, " \r\n"));
+
+        strtok(s, "\r\n");
+        //printf("n=%d, %s\n",n,s);
+        free(s);
+    }
+
+    fclose(fp);
+    printf("%d bytes read\n", n);
+}
+
+static unsigned char alldata[1492];
+
+int
+getmyline(int fd, char *buf)
+{
+    unsigned char c;
+    int i = 0;
+    int n = 0;
+    memset(buf, 0, BUFSIZE);
+
+#if 1
+
+    while (read(fd, &c, 1) > 0 && i < 5)
+    {
+        buf[i++] = c;
+        n++;
+    }
+
+#else
+    n = read(fd, buf, 5);
+#endif
+    printf("n=%d %02x %02x %02x %02x %02x\n", n, buf[0], buf[1], buf[2], buf[3],
+           buf[4]);
+    return n;
+}
+
+#if defined(WIN32) || defined(_WIN32)
+int openPort(char *comport) // doesn't matter for using pts devices
+{
+    int fd;
+    fd = open(comport, O_RDWR);
+
+    if (fd < 0)
+    {
+        perror(comport);
+    }
+
+    return fd;
+}
+
+#else
+int openPort(char *comport) // doesn't matter for using pts devices
+{
+    int fd = posix_openpt(O_RDWR);
+    char *name = ptsname(fd);
+
+    if (name == NULL)
+    {
+        perror("pstname");
+        return -1;
+    }
+
+    printf("name=%s\n", name);
+
+    if (fd == -1 || grantpt(fd) == -1 || unlockpt(fd) == -1)
+    {
+        perror("posix_openpt");
+        return -1;
+    }
+
+    return fd;
+}
+#endif
+
+
+
+int main(int argc, char *argv[])
+{
+    char buf[256];
+    char *pbuf;
+    int n;
+    int fd = openPort(argv[1]);
+
+    load_dat("simft990.dat", alldata);
+
+    while (1)
+    {
+        int bytes = getmyline(fd, buf);
+
+        if (bytes == 0) { continue; }
+
+        if (bytes != 5)
+        {
+            printf("Not 5 bytes?  bytes=%d\n", bytes);
+        }
+
+        if (buf[4] == 0x10)
+        {
+            write(fd, alldata, 1492);
+        }
+    }
+
+    return 0;
+}
