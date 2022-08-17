bc79320f6b2d0a5e4dd4ce97c1be785d91da08cc
Mike Black W9MDB <mdblack98@yahoo.com>
2022-08-17 13:03:03 -0500

    Increase write_delay in FT5000 to 1ms -- suspect it has similar problem to FT3000

--- rigs/yaesu/ft5000.c
+++ rigs/yaesu/ft5000.c
@@ -140,7 +140,7 @@ const struct rig_caps ftdx5000_caps =
     RIG_MODEL(RIG_MODEL_FTDX5000),
     .model_name =         "FTDX-5000",
     .mfg_name =           "Yaesu",
-    .version =            NEWCAT_VER ".5",
+    .version =            NEWCAT_VER ".6",
     .copyright =          "LGPL",
     .status =             RIG_STATUS_STABLE,
     .rig_type =           RIG_TYPE_TRANSCEIVER,
