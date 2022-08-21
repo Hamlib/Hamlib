f8f0678988c36abf2e042f97fc4166d6c91cb59b
Mike Black W9MDB <mdblack98@yahoo.com>
2022-08-21 11:57:53 -0500

    Take out EX echo request -- can't do that as EX command does not expect response https://github.com/Hamlib/Hamlib/issues/1100

--- rigs/yaesu/newcat.c
+++ rigs/yaesu/newcat.c
@@ -600,7 +600,7 @@ int newcat_open(RIG *rig)
     {
         int err;
         // set the CAT TIME OUT TIMER to 100ms
-        SNPRINTF(priv->cmd_str, sizeof(priv->cmd_str), "EX0331;EX033;");
+        SNPRINTF(priv->cmd_str, sizeof(priv->cmd_str), "EX0331;");
 
         if (RIG_OK != (err = newcat_set_cmd(rig)))
         {
@@ -612,7 +612,7 @@ int newcat_open(RIG *rig)
     {
         int err;
         // set the CAT TIME OUT TIMER to 100ms
-        SNPRINTF(priv->cmd_str, sizeof(priv->cmd_str), "EX0391;EX039;");
+        SNPRINTF(priv->cmd_str, sizeof(priv->cmd_str), "EX0391;");
 
         if (RIG_OK != (err = newcat_set_cmd(rig)))
         {
