0c28104876eb97f426c8c31e19933d991fe35bab
Mike Black W9MDB <mdblack98@yahoo.com>
2022-10-12 22:51:21 -0500

    Add FT-710 as a clone of FTDX10

--- rigs/yaesu/yaesu.h
+++ rigs/yaesu/yaesu.h
@@ -89,5 +89,6 @@ extern const struct rig_caps ftdx10_caps;
 extern const struct rig_caps ftdx101mp_caps;
 extern const struct rig_caps mchfqrp_caps;
 extern const struct rig_caps ft650_caps;
+extern const struct rig_caps ft710_caps;
 
 #endif /* _YAESU_H */
