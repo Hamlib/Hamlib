790bf7704c8a175aabfe8cd54ec267c0d5b7fbd3
Mike Black W9MDB <mdblack98@yahoo.com>
2022-11-01 12:27:33 -0500

    Remove debug from misc.c

--- src/misc.c
+++ src/misc.c
@@ -1051,7 +1051,7 @@ const char *HAMLIB_API rig_strlevel(setting_t level)
 {
     int i;
 
-    rig_debug(RIG_DEBUG_VERBOSE, "%s called level=%lx\n", __func__, level);
+    rig_debug(RIG_DEBUG_VERBOSE, "%s called\n", __func__);
 
     if (level == RIG_LEVEL_NONE)
     {
