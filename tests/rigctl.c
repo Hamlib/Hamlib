1f42eb429ad25df8f0fbe69a051475581659152d
Mike Black W9MDB <mdblack98@yahoo.com>
2022-11-13 09:53:19 -0600

    Fix segfault with ptt_type --- seems to be constant on LHS so can't set it

--- tests/rigctl.c
+++ tests/rigctl.c
@@ -554,7 +554,6 @@ int main(int argc, char *argv[])
 
             rig_close(my_rig);
         }
-        my_rig->caps->ptt_type = ptt_type;
 
         dumpcaps(my_rig, stdout);
         rig_cleanup(my_rig);    /* if you care about memory */
@@ -577,7 +576,6 @@ int main(int argc, char *argv[])
                my_rig->caps->rig_model,
                my_rig->caps->model_name);
     }
-    my_rig->caps->ptt_type = ptt_type;
 
     if (my_rig->caps->rig_model == RIG_MODEL_NETRIGCTL)
     {
--- tests/rigctld.c
+++ tests/rigctld.c
@@ -700,6 +700,7 @@ int main(int argc, char *argv[])
     {
         my_rig->state.pttport.type.ptt = ptt_type;
         my_rig->state.pttport_deprecated.type.ptt = ptt_type;
+        my_rig->caps->ptt_type = ptt_type;
     }
 
     if (dcd_type != RIG_DCD_NONE)
@@ -748,7 +749,6 @@ int main(int argc, char *argv[])
      */
     if (dump_caps_opt)
     {
-        my_rig->caps->ptt_type = ptt_type; 
         dumpcaps(my_rig, stdout);
         rig_cleanup(my_rig); /* if you care about memory */
         exit(0);
@@ -771,7 +771,6 @@ int main(int argc, char *argv[])
                my_rig->caps->rig_model,
                my_rig->caps->model_name);
     }
-    my_rig->caps->ptt_type = ptt_type; 
 
     rig_debug(RIG_DEBUG_VERBOSE, "Backend version: %s, Status: %s\n",
               my_rig->caps->version, rig_strstatus(my_rig->caps->status));
