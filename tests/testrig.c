1e1ed4c9e61f08e4022e2b3ce1a2e196c6958ba3
Mike Black W9MDB <mdblack98@yahoo.com>
2022-12-03 15:18:02 -0600

    Force the FT-991 to 100ms along with FT-DX2000, Ft-891, FT950, FTDX3000/D 10ms was seeing timeouts on some rigs using rigctl https://github.com/Hamlib/Hamlib/issues/1163

--- tests/testrig.c
+++ tests/testrig.c
@@ -11,7 +11,7 @@
 
 #include <hamlib/config.h>
 
-#define SERIAL_PORT "/dev/pts/2"
+#define SERIAL_PORT "/dev/pts/4"
 
 
 int main(int argc, char *argv[])
@@ -76,6 +76,10 @@ int main(int argc, char *argv[])
         printf("rig_open: error = %s\n", rigerror(retcode));
         exit(2);
     }
+
+    char val[256];
+    retcode = rig_get_conf2(my_rig, rig_token_lookup(my_rig, "write_delay"), val, sizeof(val));
+    printf("write_delay=%s\n", val);
 
 //    printf("Port %s opened ok\n", SERIAL_PORT);
 
