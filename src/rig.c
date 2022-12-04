c7f7a358bfc4dca20bdf5a50ad1ef4cb6a551bea
Mike Black W9MDB <mdblack98@yahoo.com>
2022-12-04 11:59:28 -0600

    If rig does not have TARGETABLE_MODE then we return the cached values from VFOA https://github.com/Hamlib/Hamlib/issues/1172

--- src/rig.c
+++ src/rig.c
@@ -2433,7 +2433,9 @@ int HAMLIB_API rig_get_mode(RIG *rig,
 
     if (vfo == RIG_VFO_B && !(caps->targetable_vfo & RIG_TARGETABLE_MODE))
     {
-        vfo = RIG_VFO_A; // we'll just return VFO_A mode which is probably cached
+        *mode = rig->state.cache.modeMainA;
+        *width = rig->state.cache.widthMainA;
+        return RIG_OK;
     }
 
     if ((*mode != RIG_MODE_NONE && cache_ms_mode < rig->state.cache.timeout_ms)
