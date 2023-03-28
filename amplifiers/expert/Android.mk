a957f0a62c38952bf5df6cfb4efc1d69894114b5
Mike Black W9MDB <mdblack98@yahoo.com>
2023-03-28 15:57:24 -0500

    Fix expert in rotators/Android.mk

--- amplifiers/expert/Android.mk
+++ amplifiers/expert/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := expert.c
-LOCAL_MODULE := expoert
+LOCAL_MODULE := expert
 
 LOCAL_CFLAGS := 
 LOCAL_C_INCLUDES := android include src
