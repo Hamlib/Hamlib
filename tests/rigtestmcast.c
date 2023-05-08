f55e7b8348e89f626d734bd0003df9d5e3e95aa5
Mike Black W9MDB <mdblack98@yahoo.com>
2023-05-07 23:23:01 -0500

    Rename multicast test routines and clean up things a bit Now works on mingw32 and Linux https://github.com/Hamlib/Hamlib/issues/695

--- tests/rigtestmcast.c
+++ tests/rigtestmcast.c
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
 {
     RIG *rig;
     rig_model_t myrig_model;
-    rig_set_debug_level(RIG_DEBUG_NONE);
+    rig_set_debug_level(RIG_DEBUG_WARN);
 
     if (argc > 1) { myrig_model = atoi(argv[1]); }
     else
@@ -19,13 +19,20 @@ int main(int argc, char *argv[])
 
     if (rig == NULL)
     {
+	    printf("Error in rig_init\n");
+	    return 1;
 
     }
 
+#ifdef _WIN32
+    strncpy(rig->state.rigport.pathname, "COM37", HAMLIB_FILPATHLEN - 1);
+#else
     strncpy(rig->state.rigport.pathname, "/dev/ttyUSB0", HAMLIB_FILPATHLEN - 1);
+#endif
     rig->state.rigport.parm.serial.rate = 38400;
     rig_open(rig);
     multicast_init(rig, "224.0.0.1", 4532);
+    printf("threadid=%lld\n", (long long)rig->state.multicast->threadid);
     pthread_join(rig->state.multicast->threadid, NULL);
     pthread_exit(NULL);
     return 0;
