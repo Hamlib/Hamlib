0ea160369b8acd57c52702ab4b11fbf33c4a27ab
Mike Black W9MDB <mdblack98@yahoo.com>
2023-05-17 23:56:33 -0500

    Add x25_always to ic7610.c ic785x.c

--- rigs/icom/ic7610.c
+++ rigs/icom/ic7610.c
@@ -241,6 +241,7 @@ static const struct icom_priv_caps ic7610_priv_caps =
         },
     },
     .extcmds = ic7610_extcmds,
+    .x25_always = 1,
 };
 
 
@@ -349,7 +350,7 @@ const struct rig_caps ic7610_caps =
     RIG_MODEL(RIG_MODEL_IC7610),
     .model_name = "IC-7610",
     .mfg_name =  "Icom",
-    .version =  BACKEND_VER ".12",
+    .version =  BACKEND_VER ".13",
     .copyright =  "LGPL",
     .status =  RIG_STATUS_STABLE,
     .rig_type =  RIG_TYPE_TRANSCEIVER,
