2157718c8b8a41956f7d060f3eb847f5a7b843df
Mike Black W9MDB <mdblack98@yahoo.com>
2023-06-02 10:25:05 -0500

    Change FT450 and FT991 RIG_LEVEL_NR back https://github.com/Hamlib/Hamlib/issues/1305

--- rigs/yaesu/ft450.c
+++ rigs/yaesu/ft450.c
@@ -39,7 +39,7 @@ const struct rig_caps ft450_caps =
     RIG_MODEL(RIG_MODEL_FT450),
     .model_name =         "FT-450",
     .mfg_name =           "Yaesu",
-    .version =            NEWCAT_VER ".2",
+    .version =            NEWCAT_VER ".3",
     .copyright =          "LGPL",
     .status =             RIG_STATUS_STABLE,
     .rig_type =           RIG_TYPE_TRANSCEIVER,
@@ -66,7 +66,7 @@ const struct rig_caps ft450_caps =
     {
 #include "level_gran_yaesu.h"
         // cppcheck-suppress *
-        [LVL_NR] = { .min = { .i = 1 }, .max = { .i = 11 }, .step = { .i = 1 } },
+        [LVL_NR] = { .min = { .f = 1 }, .max = { .f = 11 }, .step = { .f = 1 } },
         [LVL_RAWSTR] = { .min = { .i = 0 }, .max = { .i = 255 } },
         [LVL_CWPITCH] = { .min = { .i = 400 }, .max = { .i = 800 }, .step = { .i = 100 } },
         [LVL_KEYSPD] = { .min = { .i = 4 }, .max = { .i = 60 }, .step = { .i = 1 } },
