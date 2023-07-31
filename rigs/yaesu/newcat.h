801d34e3fba7e94bb738aed32ba7b91c06dcbcbd
Mike Black W9MDB <mdblack98@yahoo.com>
2023-07-31 15:31:06 -0500

    Fix reverse split for Yaeus VFO_SUB rigs https://github.com/Hamlib/Hamlib/issues/1103

--- rigs/yaesu/newcat.h
+++ rigs/yaesu/newcat.h
@@ -50,7 +50,7 @@
 typedef char ncboolean;
 
 /* shared function version */
-#define NEWCAT_VER "20230724"
+#define NEWCAT_VER "20230731"
 
 /* Hopefully large enough for future use, 128 chars plus '\0' */
 #define NEWCAT_DATA_LEN                 129
