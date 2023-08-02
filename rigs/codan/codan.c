503f27e20c060719077d73a6157b22d1f2a1906a
Mike Black W9MDB <mdblack98@yahoo.com>
2023-08-02 17:04:07 -0500

    Fix cppcheck warnings

--- rigs/codan/codan.c
+++ rigs/codan/codan.c
@@ -217,7 +217,7 @@ int codan_get_mode(RIG *rig, vfo_t vfo, rmode_t *mode, pbwidth_t *width)
     }
 
     rig_debug(RIG_DEBUG_VERBOSE, "%s: result=%s", __func__, result);
-    int n = sscanf(result, "MODE: %[A-Z], %[A-Z], %d, %d", modeA, modeB, &center,
+    int n = sscanf(result, "MODE: %7[A-Z], %7[A-Z], %d, %d", modeA, modeB, &center,
                    &widthA);
 
     if (n != 4)
