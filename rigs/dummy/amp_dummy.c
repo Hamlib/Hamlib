9c5fe584f4beb994677439f9265135f6dcad96c7
Mike Black W9MDB <mdblack98@yahoo.com>
2023-08-02 22:55:45 -0500

    Fix cppcheck style

--- rigs/dummy/amp_dummy.c
+++ rigs/dummy/amp_dummy.c
@@ -142,7 +142,7 @@ Also a way to display faults (there are commands)
 
 static int dummy_amp_get_freq(AMP *amp, freq_t *freq)
 {
-    struct dummy_amp_priv_data *priv = (struct dummy_amp_priv_data *)
+    const struct dummy_amp_priv_data *priv = (struct dummy_amp_priv_data *)
                                        amp->state.priv;
     rig_debug(RIG_DEBUG_VERBOSE, "%s called\n", __func__);
     *freq = priv->freq;
@@ -266,7 +266,7 @@ static int dummy_amp_set_powerstat(AMP *amp, powerstat_t status)
 
 static int dummy_amp_get_powerstat(AMP *amp, powerstat_t *status)
 {
-    struct dummy_amp_priv_data *priv = (struct dummy_amp_priv_data *)
+    const struct dummy_amp_priv_data *priv = (struct dummy_amp_priv_data *)
                                        amp->state.priv;
 
     *status = priv->powerstat;
