2243d89dded02dc37bfe9eb3cfc6ae605de17f51
Mike Black W9MDB <mdblack98@yahoo.com>
2023-08-02 22:47:36 -0500

    Fix cppcheck styles

--- rigs/aor/ar3030.c
+++ rigs/aor/ar3030.c
@@ -356,7 +356,7 @@ int ar3030_set_vfo(RIG *rig, vfo_t vfo)
 
 int ar3030_get_vfo(RIG *rig, vfo_t *vfo)
 {
-    struct ar3030_priv_data *priv = (struct ar3030_priv_data *)rig->state.priv;
+    const struct ar3030_priv_data *priv = (struct ar3030_priv_data *)rig->state.priv;
 
     *vfo = priv->curr_vfo;
 
