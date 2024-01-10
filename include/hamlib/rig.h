208efc1cf81409b66341b4d3fd4c2da5ded74f6d
George Baltz N3GB <GeoBaltz@gmail.com>
2024-01-09 20:06:48 -0500

    Add function to get rotator port address(es)
    Add ROTPORT2 definitions.
    Convert src/rotator.c to use new access.

--- include/hamlib/rig.h
+++ include/hamlib/rig.h
@@ -2481,6 +2481,7 @@ typedef hamlib_port_t port_t;
 #define CACHE(r) (&r->state.cache)
 #define AMPPORT(a) (&a->state.ampport)
 #define ROTPORT(r) (&r->state.rotport)
+#define ROTPORT2(r) (&r->state.rotport2)
 /* Then when the rigport address is stored as a pointer somewhere else(say,
  *  in the rig structure itself), the definition could be changed to
  *  #define RIGPORT(r) r->somewhereelse
@@ -2494,6 +2495,7 @@ typedef hamlib_port_t port_t;
 //#define HAMLIB_CACHE(r) (struct rig_cache *)rig_data_pointer(r, RIG_PTRX_CACHE)
 #define HAMLIB_AMPPORT(a) (hamlib_port_t *)amp_data_pointer(a, RIG_PTRX_AMPPORT)
 #define HAMLIB_ROTPORT(r) (hamlib_port_t *)rot_data_pointer(r, RIG_PTRX_ROTPORT)
+#define HAMLIB_ROTPORT2(r) (hamlib_port_t *)rot_data_pointer(r, RIG_PTRX_ROTPORT2)
 #endif
 
 typedef enum {
@@ -2504,6 +2506,7 @@ typedef enum {
     RIG_PTRX_CACHE,
     RIG_PTRX_AMPPORT,
     RIG_PTRX_ROTPORT,
+    RIG_PTRX_ROTPORT2,
 // New entries go directly above this line====================
     RIG_PTRX_MAXIMUM
 } rig_ptrx_t;
