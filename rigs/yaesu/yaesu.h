74b07528982fc58c0b4c3ca1a8b2bf401794da8a
Mike Black W9MDB <mdblack98@yahoo.com>
2024-01-22 16:34:18 -0600

    Add Guohe Q900 https://github.com/Hamlib/Hamlib/issues/1483

--- rigs/yaesu/yaesu.h
+++ rigs/yaesu/yaesu.h
@@ -93,5 +93,6 @@ extern struct rig_caps mchfqrp_caps;
 extern struct rig_caps ft650_caps;
 extern struct rig_caps ft710_caps;
 extern struct rig_caps ft9000Old_caps;
+extern struct rig_caps q900_caps;
 
 #endif /* _YAESU_H */
