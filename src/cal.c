05602a3ef3a3600f4f0ff0194df12821b165e2e3
Mike Black W9MDB <mdblack98@yahoo.com>
2024-02-15 09:25:27 -0600

    Astyle cal.c

--- src/cal.c
+++ src/cal.c
@@ -86,7 +86,10 @@ float HAMLIB_API rig_raw2val(int rawval, const cal_table_t *cal)
     }
 
     if (rawval == cal->table[i - 1].raw)
-        return cal->table[i-1].val;
+    {
+        return cal->table[i - 1].val;
+    }
+
     if (i == 0)
     {
         return cal->table[0].val;
