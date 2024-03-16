84d5f367f6b6631114e12fcf84e9b949411b1e55
Mike Black W9MDB <mdblack98@yahoo.com>
2024-03-15 22:51:19 -0500

    Fix compile warning on testrig.c

--- tests/testrig.c
+++ tests/testrig.c
@@ -80,7 +80,7 @@ int main(int argc, const char *argv[])
 
     uint64_t levels = rig_get_caps_int(my_rig->caps->rig_model,
                                        RIG_CAPS_HAS_GET_LEVEL);
-    printf("HAS_GET_LEVEL=0x%8llx, SWR=%8llx,true=%d\n", levels,
+    printf("HAS_GET_LEVEL=0x%8lx, SWR=%8llx,true=%d\n", levels,
            levels & RIG_LEVEL_SWR, (levels & RIG_LEVEL_SWR) == RIG_LEVEL_SWR);
 
     char val[256];
