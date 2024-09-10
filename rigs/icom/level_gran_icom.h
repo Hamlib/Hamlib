f3f3cb9977ef3f4ec6a047e33dddb358a1e41e96
Mike Black W9MDB <mdblack98@yahoo.com>
2024-09-10 17:41:45 -0500

    Fix compile warnings in ic7600.c for MacOS

--- rigs/icom/level_gran_icom.h
+++ rigs/icom/level_gran_icom.h
@@ -57,6 +57,7 @@ LVL_KEYSPD
         [LVL_ALC]           = { .min = { .f = .0 },    .max = { .f = 1 },    .step = { .f = 1.0f/120.0f } },
 #if !defined(NO_LVL_USB_AF) 
         [LVL_USB_AF]        = { .min = { .f = .0 },    .max = { .f = 1 },    .step = { .f = 1.0f/255.0f } },
+#endif
 #if !defined(NO_LVL_PBT_IN) 
         [LVL_PBT_IN]        = { .min = { .f = .0 },    .max = { .f = 1 },    .step = { .f = 1.0f/255.0f } },
 #endif
