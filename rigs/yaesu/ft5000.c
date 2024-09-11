276b891fd6c33568650f46c989bf8626675548c1
Mike Black W9MDB <mdblack98@yahoo.com>
2024-09-11 09:27:29 -0500

    Fix compile warnings in ft5000.c for MacOS

--- rigs/yaesu/ft5000.c
+++ rigs/yaesu/ft5000.c
@@ -163,7 +163,13 @@ struct rig_caps ftdx5000_caps =
     .has_set_parm =       RIG_PARM_BANDSELECT,
     .level_gran =
     {
+#define NO_LVL_NOTCHF
+#define NO_LVL_COMP
+#define NO_LVL_VOXGAIN
 #include "level_gran_yaesu.h"
+#undef NO_LVL_NOTCHF
+#undef NO_LVL_COMP
+#undef NO_LVL_VOXGAIN
         [LVL_NOTCHF] = { .min = { .i = 1 }, .max = { .i = 4000 }, .step = { .i = 10 } },
         [LVL_COMP] = { .min = { .f = 0 }, .max = { .f = 1.0 }, .step = { .f = 1.0f / 255.0f } },
         [LVL_VOXGAIN] = { .min = { .f = 0 }, .max = { .f = 1.0 }, .step = { .f = 1.0f / 255.0f } },
