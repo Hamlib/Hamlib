a81c7d90c41ba6911b75d1806343b318141e6e0b
Michael Black W9MDB <mdblack98@yahoo.com>
2024-12-21 13:01:05 -0600

    Fix bufferoverflow in rigswr.c

--- tests/rigswr.c
+++ tests/rigswr.c
@@ -166,7 +166,7 @@ int main(int argc, char *argv[])
                 return 1;
             }
 
-            strncat(conf_parms, optarg, MAXCONFLEN - strlen(conf_parms));
+            strncat(conf_parms, optarg, MAXCONFLEN - strlen(conf_parms) - 1);
             break;
 
         case 'p':
