9de6fe2400cff5489cd487e71af299758a4ee567
Michael Black W9MDB <mdblack98@yahoo.com>
2025-02-24 16:55:14 -0600

    Fix name of IC-R8500

--- rigs/icom/icr8500.c
+++ rigs/icom/icr8500.c
@@ -73,7 +73,7 @@ static struct icom_priv_caps icr8500_priv_caps =
 struct rig_caps icr8500_caps =
 {
     RIG_MODEL(RIG_MODEL_ICR8500),
-    .model_name = "ICR-8500",
+    .model_name = "IC-R8500",
     .mfg_name =  "Icom",
     .version =  BACKEND_VER ".0",
     .copyright =  "LGPL",
