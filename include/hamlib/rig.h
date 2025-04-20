cb2afb41d67da9a95712d27750a90b8daf035b83
Daniele Forsi IU5HKX <iu5hkx@gmail.com>
2025-04-20 19:16:56 +0200

    Fix list of parameters for get_func/set_func
    Fixes: OVF_STATUS was spelled wrong, SYNC was missing, SLICE is
    not available. Uses the list returned by \get_func ? sorted
    alphabetically to be more user friendly and to easily spot any
    missing item in future.

--- include/hamlib/rig.h
+++ include/hamlib/rig.h
@@ -1296,7 +1296,7 @@ typedef uint64_t setting_t;
 #define RIG_FUNC_SPECTRUM_HOLD CONSTANT_64BIT_FLAG (44)   /*!< \c SPECTRUM_HOLD -- Pause spectrum scope updates ON/OFF */
 #define RIG_FUNC_SEND_MORSE CONSTANT_64BIT_FLAG (45)   /*!< \c SEND_MORSE -- Send specified characters using CW */
 #define RIG_FUNC_SEND_VOICE_MEM CONSTANT_64BIT_FLAG (46)   /*!< \c SEND_VOICE_MEM -- Transmit in SSB message stored in memory */
-#define RIG_FUNC_OVF_STATUS CONSTANT_64BIT_FLAG (47)   /*!< \c OVF -- Read overflow status 0=Off, 1=On */
+#define RIG_FUNC_OVF_STATUS CONSTANT_64BIT_FLAG (47)   /*!< \c OVF_STATUS -- Read overflow status 0=Off, 1=On */
 #define RIG_FUNC_SYNC       CONSTANT_64BIT_FLAG (48)   /*!< \c Synchronize VFOS -- FTDX101D/MP for now SY command  */
 #define RIG_FUNC_BIT49      CONSTANT_64BIT_FLAG (49)   /*!< \c available for future RIG_FUNC items */
 #define RIG_FUNC_BIT50      CONSTANT_64BIT_FLAG (50)   /*!< \c available for future RIG_FUNC items */
