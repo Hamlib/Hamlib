52603c6ba110988aebfaca91480d0ecf49bc1996
Daniele Forsi IU5HKX <iu5hkx@gmail.com>
2025-05-04 21:42:10 +0200

    Add a minimal test for dummy amplifier

---
+++ bindings/python/test_amp.py
@@ -0,0 +1,20 @@
+#! /bin/env pytest
+"""Tests of the Python bindings for Hamlib
+
+Running this script directly will use the installed bindings.
+For an in-tree run use "make check", or set PYTHONPATH to point to
+the directories containing Hamlib.py and _Hamlib.so.
+"""
+import Hamlib
+
+AMP_MODEL = Hamlib.AMP_MODEL_DUMMY
+
+class TestClass:
+    """Container class for tests"""
+
+    def test_open_close(self):
+        """Smoke test"""
+        amp = Hamlib.Amp(AMP_MODEL)
+        assert amp is not None
+        assert amp.open() is None
+        assert amp.close() is None
