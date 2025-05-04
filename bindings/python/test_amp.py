b99241f11b5d490c200ef2ccdbfcc1c7eb3daa24
Daniele Forsi IU5HKX <iu5hkx@gmail.com>
2025-05-04 22:47:44 +0200

    Just call all the methods of the Amp object

--- bindings/python/test_amp.py
+++ bindings/python/test_amp.py
@@ -18,3 +18,24 @@ class TestClass:
         assert amp is not None
         assert amp.open() is None
         assert amp.close() is None
+
+
+    def test_all_methods(self):
+        """Just call all the methods"""
+        amp = Hamlib.Amp(AMP_MODEL)
+        assert amp is not None
+
+        # the tests that do not depend on open()
+        assert amp.set_conf("", "") is None
+        assert amp.get_conf("") == ""
+        assert amp.get_conf(0) == ""
+        assert amp.get_info() is None
+        assert amp.token_lookup("") is None
+
+        # the tests that depend on open()
+        assert amp.open() is None
+        assert amp.set_freq(0) is None
+        # assert amp.get_freq() is None  # FIXME:  AttributeError: 'Amp' object has no attribute 'get_freq'
+        assert amp.set_powerstat(Hamlib.RIG_POWER_OFF) is None
+        # assert amp.get_powerstat() is None  # FIXME:  AttributeError: 'Amp' object has no attribute 'get_powerstat'
+        assert amp.close() is None
