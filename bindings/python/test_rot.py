8ae5d1c9336170d7051778497e80ec3845c09d57
Daniele Forsi IU5HKX <iu5hkx@gmail.com>
2025-06-02 19:46:35 +0200

    Implement Rot.set_ext_parm()

--- bindings/python/test_rot.py
+++ bindings/python/test_rot.py
@@ -70,6 +70,7 @@ class TestClass:
 
         assert rot.set_ext_func(0, 0) is None
         assert rot.set_ext_level(0, value) is None
+        assert rot.set_ext_parm(0, value) is None
 
         status = 0
         assert rot.set_func(1, status) is None
