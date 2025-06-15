533f422c25313f473643120322e6083d1cf27246
Daniele Forsi IU5HKX <iu5hkx@gmail.com>
2025-06-15 12:28:40 +0200

    Update autogenerated tests for Amp object

--- bindings/python/test_Hamlib_Amp_class.py
+++ bindings/python/test_Hamlib_Amp_class.py
@@ -16,7 +16,7 @@ class TestClass:
 
     def test_callables(self):
         """Check that nothing was added or removed"""
-        expected_callables = ['close', 'get_conf', 'get_info', 'open', 'reset', 'set_conf', 'set_freq', 'set_powerstat', 'token_lookup']
+        expected_callables = ['close', 'get_conf', 'get_freq', 'get_info', 'get_level', 'get_powerstat', 'open', 'reset', 'set_conf', 'set_freq', 'set_powerstat', 'token_lookup']
         assert expected_callables == self.actual_callables
 
     def test_properties(self):
