Hamlib/Hamlib

commit d0ed28182ac86262becceb39e732d3789da175bf

Let kenwood serial read remove non-printable chars from responses Should help it be a bit more robust against bad serial data like this https://github.com/Hamlib/Hamlib/issues/1652

rigs/kenwood/kenwood.c
@@ -231,6 +231,23 @@ struct confparams kenwood_cfg_params[] =
     { RIG_CONF_END, NULL, }
 };
 
+int remove_nonprint(char *s)
+{
+    int i, j = 0;
+    if (s == NULL) return 0;
+
+    for (i = 0; s[i] != '\0'; ++i)
+    {
+        if (isprint((unsigned char)s[i]))
+        {
+            s[j++] = s[i]; // Copy printable character
+        }
+    }
+    s[j] = '\0'; // Null-terminate the string
+
+    return j; // Return the new length of the string
+}
+
 
 /**
  * kenwood_transaction
@@ -423,6 +440,10 @@ transaction_read:
                          cmdtrm_str, strlen(cmdtrm_str), 0, 1);
     rig_debug(RIG_DEBUG_TRACE, "%s: read_string len=%d '%s'\n", __func__,
               (int)strlen(buffer), buffer);
+
+    // this fixes the case when some corrupt data is returned
+    // let's us be a little more robust about funky serial data
+    remove_nonprint(buffer);
 
     if (retval < 0)
     {

rigs/kenwood/kenwood.h
@@ -28,7 +28,7 @@
 #include "token.h"
 #include "idx_builtin.h"
 
-#define BACKEND_VER "20241022"
+#define BACKEND_VER "20250107"
 
 #define EOM_KEN ';'
 #define EOM_TH '\r'

commit ebad92a3f11dad1eff43e041b73f5074235a2a9a

Un-break hamlib on TM-D710/TM-V71/etc
Commit d0ed281 introduced a `remove_nonprint` method that breaks hamlib on
all TM-D710/TM-V71A devices by erroneously removing the command termination
character (`\r`).

This commit adopts a solution proposed by @GeoBaltz that only runs
`remove_nonprint` if the command termination character itself is printable.

Resolves: #1767 #1698

rigs/kenwood/kenwood.c
@@ -231,6 +231,12 @@ struct confparams kenwood_cfg_params[] =
     { RIG_CONF_END, NULL, }
 };
 
+// This function removes non-printable characters from a buffer. This was
+// implemented to work around a problem reported with the uSDX transceiver
+// [1], which emulates the Kenwood TS-480 but apparently generates garbage
+// on the serial port in some situations.
+//
+// [1]: https://github.com/Hamlib/Hamlib/issues/1652
 static int remove_nonprint(char *s)
 {
     int i, j = 0;
@@ -443,9 +449,14 @@ transaction_read:
     rig_debug(RIG_DEBUG_TRACE, "%s: read_string len=%d '%s'\n", __func__,
               (int)strlen(buffer), buffer);
 
-    // this fixes the case when some corrupt data is returned
-    // let's us be a little more robust about funky serial data
-    remove_nonprint(buffer);
+    // This fixes the case when some corrupt data is returned; it lets us be a
+    // little more robust about funky serial data. If the terminator is
+    // printable(usually ';'), then there should be no nonprintables in the
+    // message; if it isn't (usually '\r') then don't touch the message.
+    if (isprint(caps->cmdtrm))
+    {
+        remove_nonprint(buffer);
+    }
 
     if (retval < 0)
     {