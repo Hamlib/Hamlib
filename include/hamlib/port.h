54ea6e0bc77c6635ed8c33b4ebdf41430aaa354b
George Baltz N3GB <GeoBaltz@gmail.com>
2025-06-30 13:48:53 -0400

    Basic infrastructure for moving internal structures out of rig.h
    Create breakout files, add them to Makefile.am manifest.

---
+++ include/hamlib/port.h
@@ -0,0 +1,30 @@
+/*
+ *  Hamlib Interface - Port structure
+ *  Copyright (c) 2025 by The Hamlib Group
+ *
+ *
+ *   This library is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU Lesser General Public
+ *   License as published by the Free Software Foundation; either
+ *   version 2.1 of the License, or (at your option) any later version.
+ *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *   Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with this library; if not, write to the Free Software
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+
+#ifndef _HL_PORT_H
+#define _HL_PORT_H 1
+
+__BEGIN_DECLS
+
+__END_DECLS
+
+#endif /* _HL_PORT_H */
