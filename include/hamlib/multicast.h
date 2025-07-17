133817bf146eca08ce8267793f31b14c47f90b08
Nate Bargmann <n0nb@n0nb.us>
2025-07-17 14:09:34 -0500

    Add license header to multicast.[ch]

--- include/hamlib/multicast.h
+++ include/hamlib/multicast.h
@@ -1,3 +1,29 @@
+/*
+ *  Hamlib Interface - Multicast API header
+ *  Copyright (c) 2023 by Mike Black, W9MDB
+ *
+ *
+ *   This library is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU Lesser General Public
+ *   License as published by the Free Software Foundation; either
+ *   version 2.1 of the License, or (at your option) any later version.
+ *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *   Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with this library; if not, write to the Free Software
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+
+
+#ifndef MULTICAST_H
+#define MULTICAST_H
+
 //include <stdio.h>
 //#include <stdlib.h>
 //#include <string.h>
@@ -16,9 +42,6 @@
 #ifdef HAVE_ARPA_INET_H
 #include <arpa/inet.h>
 #endif
-
-#ifndef MULTICAST_H
-#define MULTICAST_H
 
 struct multicast_vfo
 {
@@ -42,4 +65,5 @@ struct multicast_broadcast
 extern HAMLIB_EXPORT (int) multicast_init(RIG *rig, char *addr, int port);
 extern HAMLIB_EXPORT (int) multicast_send(RIG *rig, const char *msg, int msglen);
 extern HAMLIB_EXPORT (int) multicast_stop(RIG *rig);
-#endif //MULTICAST_H
+
+#endif  // MULTICAST_H
--- src/multicast.c
+++ src/multicast.c
@@ -1,3 +1,27 @@
+/*
+ *  Hamlib Interface - Multicast API header
+ *  Copyright (c) 2023,2024 by Mike Black, W9MDB
+ *  Copyright (c) 2024,2025 by George Baltz, N3GB
+ *
+ *
+ *   This library is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU Lesser General Public
+ *   License as published by the Free Software Foundation; either
+ *   version 2.1 of the License, or (at your option) any later version.
+ *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *   Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with this library; if not, write to the Free Software
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
