e2e2904c381238dc194d3dee8ec7884a4c69c03b
Mark J. Fine <mark.fine@fineware-swl.com>
2025-07-19 10:30:09 -0400

    Changed %llu to just %lu to quiet a warning.

--- rigs/dummy/gqrx.c
+++ rigs/dummy/gqrx.c
@@ -547,7 +547,7 @@ static int gqrx_set_mode(RIG *rig, vfo_t vfo, rmode_t mode, pbwidth_t width)
     struct gqrx_priv_data *priv = (struct gqrx_priv_data *) STATE(rig)->priv;
 
     rig_debug(RIG_DEBUG_TRACE, "%s\n", __func__);
-    rig_debug(RIG_DEBUG_TRACE, "%s: vfo=%s mode=%llu\n", __func__,
+    rig_debug(RIG_DEBUG_TRACE, "%s: vfo=%s mode=%lu\n", __func__,
               rig_strvfo(vfo), mode);
 
     if (check_vfo(vfo) == FALSE)
@@ -733,7 +733,7 @@ static int gqrx_set_level(RIG *rig, vfo_t vfo, setting_t level, value_t val)
     char value[1024];
 
     rig_debug(RIG_DEBUG_TRACE, "%s\n", __func__);
-    rig_debug(RIG_DEBUG_TRACE, "%s: vfo=%s level=%llu\n", __func__,
+    rig_debug(RIG_DEBUG_TRACE, "%s: vfo=%s level=%lu\n", __func__,
               rig_strvfo(vfo), level);
 
     if (check_vfo(vfo) == FALSE)
