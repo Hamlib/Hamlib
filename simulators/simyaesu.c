16a69722e26baa131bca2b176404d0748ea34419
Daniele Forsi IU5HKX <iu5hkx@gmail.com>
2025-08-02 16:04:02 +0200

    Use the resp buffer for all responses
    Insetad of the command buffer.

--- simulators/simyaesu.c
+++ simulators/simyaesu.c
@@ -222,8 +222,8 @@ int main(int argc, char *argv[])
         {
             hl_usleep(50 * 1000);
             int id = NC_RIGID_FT991;
-            SNPRINTF(buf, sizeof(buf), "ID%03d;", id);
-            n = write(fd, buf, strlen(buf));
+            SNPRINTF(resp, sizeof(resp), "ID%03d;", id);
+            n = write(fd, resp, strlen(resp));
             //printf("n=%d\n", n);
 
             if (n <= 0) { perror("ID"); }
@@ -231,8 +231,8 @@ int main(int argc, char *argv[])
         else if (strcmp(buf, "AI;") == 0)
         {
             hl_usleep(50 * 1000);
-            SNPRINTF(buf, sizeof(buf), "AI0;");
-            n = write(fd, buf, strlen(buf));
+            SNPRINTF(resp, sizeof(resp), "AI0;");
+            n = write(fd, resp, strlen(resp));
             //printf("n=%d\n", n);
 
             if (n <= 0) { perror("AI"); }
@@ -346,49 +346,49 @@ int main(int argc, char *argv[])
             static int ant = 0;
             ant = (ant + 1) % 3;
             hl_usleep(50 * 1000);
-            SNPRINTF(buf, sizeof(buf), "EX032%1d;", ant);
-            n = write(fd, buf, strlen(buf));
+            SNPRINTF(resp, sizeof(resp), "EX032%1d;", ant);
+            n = write(fd, resp, strlen(resp));
             //printf("n=%d\n", n);
 
             if (n < 0) { perror("EX032"); }
         }
         else if (strcmp(buf, "NA0;") == 0)
         {
-            SNPRINTF(buf, sizeof(buf), "NA00;");
+            SNPRINTF(resp, sizeof(resp), "NA00;");
             hl_usleep(50 * 1000);
-            n = write(fd, buf, strlen(buf));
-            //printf("%s n=%d\n", buf, n);
+            n = write(fd, resp, strlen(resp));
+            //printf("%s n=%d\n", resp, n);
         }
         else if (strcmp(buf, "RF0;") == 0)
         {
-            SNPRINTF(buf, sizeof(buf), "RF0%d;", roofing_filter_main);
+            SNPRINTF(resp, sizeof(resp), "RF0%d;", roofing_filter_main);
             hl_usleep(50 * 1000);
-            n = write(fd, buf, strlen(buf));
+            n = write(fd, resp, strlen(resp));
         }
         else if (strcmp(buf, "RF1;") == 0)
         {
-            SNPRINTF(buf, sizeof(buf), "RF1%d;", roofing_filter_sub);
+            SNPRINTF(resp, sizeof(resp), "RF1%d;", roofing_filter_sub);
             hl_usleep(50 * 1000);
-            n = write(fd, buf, strlen(buf));
+            n = write(fd, resp, strlen(resp));
         }
         else if (strncmp(buf, "RF", 2) == 0)
         {
-            SNPRINTF(buf, sizeof(buf), "RF%c%d;", buf[2],
+            SNPRINTF(resp, sizeof(resp), "RF%c%d;", buf[2],
                      buf[2] == 0 ? roofing_filter_main : roofing_filter_sub);
             hl_usleep(50 * 1000);
-            n = write(fd, buf, strlen(buf));
+            n = write(fd, resp, strlen(resp));
         }
         else if (strcmp(buf, "SH0;") == 0)
         {
-            SNPRINTF(buf, sizeof(buf), "SH%c%02d;", buf[2], width_main);
+            SNPRINTF(resp, sizeof(resp), "SH%c%02d;", buf[2], width_main);
             hl_usleep(50 * 1000);
-            n = write(fd, buf, strlen(buf));
+            n = write(fd, resp, strlen(resp));
         }
         else if (strcmp(buf, "SH1;") == 0)
         {
-            SNPRINTF(buf, sizeof(buf), "SH%c%02d;", buf[2], width_sub);
+            SNPRINTF(resp, sizeof(resp), "SH%c%02d;", buf[2], width_sub);
             hl_usleep(50 * 1000);
-            n = write(fd, buf, strlen(buf));
+            n = write(fd, resp, strlen(resp));
         }
         else if (strncmp(buf, "SH", 2) == 0 && strlen(buf) > 4)
         {
@@ -402,13 +402,13 @@ int main(int argc, char *argv[])
         }
         else if (strncmp(buf, "SH", 2) == 0)
         {
-            SNPRINTF(buf, sizeof(buf), "SH%c%02d;", buf[2],
+            SNPRINTF(resp, sizeof(resp), "SH%c%02d;", buf[2],
                      buf[2] == 0 ? width_main : width_sub);
         }
         else if (strcmp(buf, "EX039;") == 0)
         {
-            SNPRINTF(buf, sizeof(buf), "EX039%d;", ex039);
-            n = write(fd, buf, strlen(buf));
+            SNPRINTF(resp, sizeof(resp), "EX039%d;", ex039);
+            n = write(fd, resp, strlen(resp));
         }
         else if (strncmp(buf, "EX039", 5) == 0)
         {
@@ -416,8 +416,8 @@ int main(int argc, char *argv[])
         }
         else if (strcmp(buf, "LK;") == 0)
         {
-            SNPRINTF(buf, sizeof(buf), "LK%d;", lk);
-            n = write(fd, buf, strlen(buf));
+            SNPRINTF(resp, sizeof(resp), "LK%d;", lk);
+            n = write(fd, resp, strlen(resp));
         }
         else if (strncmp(buf, "LK", 3) == 0)
         {
@@ -425,18 +425,18 @@ int main(int argc, char *argv[])
         }
         else if (strncmp(buf, "DT0;", 4) == 0)
         {
-            SNPRINTF(buf, sizeof(buf), "DT020221022;");
-            n = write(fd, buf, strlen(buf));
+            SNPRINTF(resp, sizeof(resp), "DT020221022;");
+            n = write(fd, resp, strlen(resp));
         }
         else if (strncmp(buf, "DT1;", 4) == 0)
         {
-            SNPRINTF(buf, sizeof(buf), "DT1222100;");
-            n = write(fd, buf, strlen(buf));
+            SNPRINTF(resp, sizeof(resp), "DT1222100;");
+            n = write(fd, resp, strlen(resp));
         }
         else if (strncmp(buf, "DT2;", 4) == 0)
         {
-            SNPRINTF(buf, sizeof(buf), "DT2+0500;");
-            n = write(fd, buf, strlen(buf));
+            SNPRINTF(resp, sizeof(resp), "DT2+0500;");
+            n = write(fd, resp, strlen(resp));
         }
 
 
