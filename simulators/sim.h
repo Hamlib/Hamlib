249a99363363fab3b37eac66c9f83323988427af
Daniele Forsi IU5HKX <iu5hkx@gmail.com>
2025-08-05 22:19:46 +0200

    Avoid spamming the terminal with 0's

--- simulators/sim.h
+++ simulators/sim.h
@@ -120,21 +120,21 @@ getmyline5(int fd, unsigned char *buf)
 {
     unsigned char c;
     int i = 0;
-    int n = 0;
     memset(buf, 0, BUFSIZE);
 
     while (i < 5 && read(fd, &c, 1) > 0)
     {
         buf[i++] = c;
-        n++;
     }
 
-    printf("n=%d %02x %02x %02x %02x %02x\n", n, buf[0], buf[1], buf[2], buf[3],
-           buf[4]);
+    if (i > 0) {
+        printf("n=%d %02x %02x %02x %02x %02x\n", i,
+               buf[0], buf[1], buf[2], buf[3], buf[4]);
+    }
 
     if (i == 0) {
         hl_usleep(10 * 1000);
     }
 
-    return n;
+    return i;
 }
