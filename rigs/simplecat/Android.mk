1bea329650b60c4a838e532ae2ada41a188c6d6d
Gong BI1XJT <bi1xjt@rad1o.cn>
2026-05-17 01:32:05 +0800

    fix simplecat mk

--- rigs/simplecat/Android.mk
+++ rigs/simplecat/Android.mk
@@ -7,6 +7,8 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := simplecat
 LOCAL_SRC_FILES = simplecat.c
 
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/../..
+LOCAL_CFLAGS := -DIN_HAMLIB
+LOCAL_C_INCLUDES := android include include/hamlib lib src
+LOCAL_LDLIBS := -lhamlib -Lobj/local/$(TARGET_ARCH_ABI)
 
 include $(BUILD_STATIC_LIBRARY)
