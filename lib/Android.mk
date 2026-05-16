ca6fd466f91564a639db2606931ce001b833152e
Gong BI1XJT <bi1xjt@rad1o.cn>
2026-05-17 02:19:47 +0800

    integrate misc into libhamlib

--- lib/Android.mk
+++ lib/Android.mk
@@ -1,14 +1,12 @@
-LOCAL_PATH:= $(call my-dir)
+LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := \
-        cJSON.c cJSON.h asyncpipe.c asyncpipe.h precise_time.c
-
 LOCAL_MODULE := misc
-LOCAL_CFLAGS := -DIN_HAMLIB
-LOCAL_C_INCLUDES := android include lib
+LOCAL_SRC_FILES = cJSON.c cJSON.h asyncpipe.c asyncpipe.h precise_time.c
 
+LOCAL_CFLAGS := -DIN_HAMLIB
+LOCAL_C_INCLUDES := android lib include src
 LOCAL_LDLIBS := -llog -landroid
 
-include $(BUILD_SHARED_LIBRARY)
+include $(BUILD_STATIC_LIBRARY)
