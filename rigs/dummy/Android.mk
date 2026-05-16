873dd72304e31826b175ecaa5c2b27db72f7bd49
Gong BI1XJT <bi1xjt@rad1o.cn>
2026-05-17 02:18:01 +0800

    update src files for dummy rigs

--- rigs/dummy/Android.mk
+++ rigs/dummy/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := dummy.c rot_dummy.c netrigctl.c netrotctl.c flrig.c trxmanager.c dummy_common.c sdrsharp.c gqrx.c
+LOCAL_SRC_FILES := dummy_common.c dummy.c rot_dummy.c rot_pstrotator.c netrigctl.c netrotctl.c flrig.c trxmanager.c amp_dummy.c netampctl.c tci1x.c aclog.c sdrsharp.c quisk.c gqrx.c
 LOCAL_MODULE := dummy
 
 LOCAL_CFLAGS := -DIN_HAMLIB
