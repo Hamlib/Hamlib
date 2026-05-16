4fb19b0d6db317c8dc5c1d2e5e163e741791de2a
Gong BI1XJT <bi1xjt@rad1o.cn>
2026-05-17 01:39:57 +0800

    add missing source for winradio

--- rigs/winradio/Android.mk
+++ rigs/winradio/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := wr1000.c wr1500.c wr1550.c wr3100.c wr3150.c wr3500.c wr3700.c \
-		g303.c g313.c g305.c winradio.c linradio/wrg313api.c
+	g303.c g305.c winradio.c linradio/wrg313api.c
 LOCAL_MODULE := winradio
 
 LOCAL_CFLAGS := -DIN_HAMLIB
