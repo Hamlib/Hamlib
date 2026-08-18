Hamlib/Hamlib

commit 4053430b621b5f286227e53e6f8d777b97134621

More trading a little more data for less code
Add new item to width info struct
Predetermine the width_info before the rig selection
Consolidate SSB/CW/RTTY processing for the first 3 rigs.

rigs/yaesu/newcat.c
@@ -212,6 +212,16 @@ static const rptr_offs_cmd_entry_t rptr_offs_cmd_table[] = {
 #define RPTR_OFFS_CMD_TABLE_SIZE \
     (sizeof(rptr_offs_cmd_table) / sizeof(rptr_offs_cmd_table[0]) - 1)
 
+/* Dummy entry for rigs/modes with no width information
+ * Really should not be needed/used, but just in case...
+ */
+static const struct newcat_width_info dummy_widths =
+{
+    .narrow_max = 0,       // Everything is wide
+    .count = 1,
+    .widths = { 0 },       // Default width
+};
+
 /*
  * ANTIVOX command lookup table.
  * Maps rig model ID to the command string for ANTIVOX control.
@@ -8664,6 +8674,7 @@ int newcat_set_rx_bandwidth(RIG *rig, vfo_t vfo, rmode_t mode, pbwidth_t width)
 {
     struct newcat_priv_data *priv = (struct newcat_priv_data *)STATE(rig)->priv;
     struct newcat_priv_caps *priv_caps = (struct newcat_priv_caps *)rig->caps->priv;
+    const struct newcat_width_info *width_info = NULL;
     int err;
     int w = 0;
     char main_sub_vfo = '0';
@@ -8692,6 +8703,41 @@ int newcat_set_rx_bandwidth(RIG *rig, vfo_t vfo, rmode_t mode, pbwidth_t width)
 
     // NOTE: RIG_PASSBAND_NORMAL (0) should select the default filter width (SH00)
 
+    // Preselect set of possible widths
+    switch (mode)
+    {
+    case RIG_MODE_RTTY:
+    case RIG_MODE_RTTYR:
+        width_info = priv_caps->rtty_widths;  // Set RTTY data if different
+        HL_FALLTHROUGH                        // Fall into CW
+    case RIG_MODE_PKTUSB:
+    case RIG_MODE_PKTLSB:
+    case RIG_MODE_CW:
+    case RIG_MODE_CWR:
+        if (width_info == NULL)
+        {
+            width_info = priv_caps->cw_widths;
+        }
+        break;
+
+    case RIG_MODE_LSB:
+    case RIG_MODE_USB:
+        width_info = priv_caps->ssb_widths;
+        break;
+
+#if 0
+    case RIG_MODE_AM:
+    case RIG_MODE_FM:
+    case RIG_MODE_PKTFM:
+    case RIG_MODE_FMN:
+        //????
+        break;
+#endif
+    default:
+        width_info = &dummy_widths;  // Just in case...
+
+    }
+
     if (is_ft950)
     {
         switch (mode)
@@ -8702,30 +8748,17 @@ int newcat_set_rx_bandwidth(RIG *rig, vfo_t vfo, rmode_t mode, pbwidth_t width)
         case RIG_MODE_RTTYR:
         case RIG_MODE_CW:
         case RIG_MODE_CWR:
-            // Narrow mode must be chosen correctly before filter width
-            err = newcat_set_narrow(rig, vfo, width <= 500);
-
-            if (err != RIG_OK)
-            {
-                RETURNFUNC(err);
-            }
-
-            w = newcat_get_index_from_width(width, priv_caps->cw_widths);
-            if (w < 0) { RETURNFUNC(w); }
-
-            break;
-
         case RIG_MODE_LSB:
         case RIG_MODE_USB:
             // Narrow mode must be chosen correctly before filter width
-            err = newcat_set_narrow(rig, vfo, width <= 1800);
+            err = newcat_set_narrow(rig, vfo, width <= width_info->narrow_max);
 
             if (err != RIG_OK)
             {
                 RETURNFUNC(err);
             }
 
-            w = newcat_get_index_from_width(width, priv_caps->ssb_widths);
+            w = newcat_get_index_from_width(width, width_info);
             if (w < 0) { RETURNFUNC(w); }
 
             break;
@@ -8770,30 +8803,17 @@ int newcat_set_rx_bandwidth(RIG *rig, vfo_t vfo, rmode_t mode, pbwidth_t width)
         case RIG_MODE_RTTYR:
         case RIG_MODE_CW:
         case RIG_MODE_CWR:
-            // Narrow mode must be chosen correctly before filter width
-            err = newcat_set_narrow(rig, vfo, width <= 500);
-
-            if (err != RIG_OK)
-            {
-                RETURNFUNC(err);
-            }
-
-            w = newcat_get_index_from_width(width, priv_caps->cw_widths);
-            if (w < 0) { RETURNFUNC(w); }
-
-            break;
-
         case RIG_MODE_LSB:
         case RIG_MODE_USB:
             // Narrow mode must be chosen correctly before filter width
-            err = newcat_set_narrow(rig, vfo, width <= 1800);
+            err = newcat_set_narrow(rig, vfo, width <= width_info->narrow_max);
 
             if (err != RIG_OK)
             {
                 RETURNFUNC(err);
             }
 
-            w = newcat_get_index_from_width(width, priv_caps->ssb_widths);
+            w = newcat_get_index_from_width(width, width_info);
             if (w < 0) { RETURNFUNC(w); }
 
             break;
@@ -8823,30 +8843,17 @@ int newcat_set_rx_bandwidth(RIG *rig, vfo_t vfo, rmode_t mode, pbwidth_t width)
         case RIG_MODE_RTTYR:
         case RIG_MODE_CW:
         case RIG_MODE_CWR:
-            // Narrow mode must be chosen correctly before filter width
-            err = newcat_set_narrow(rig, vfo, width <= 500);
-
-            if (err != RIG_OK)
-            {
-                RETURNFUNC(err);
-            }
-
-            w = newcat_get_index_from_width(width, priv_caps->cw_widths);
-            if (w < 0) { RETURNFUNC(w); }
-
-            break;
-
         case RIG_MODE_LSB:
         case RIG_MODE_USB:
             // Narrow mode must be chosen correctly before filter width
-            err = newcat_set_narrow(rig, vfo, width <= 1800);
+            err = newcat_set_narrow(rig, vfo, width <= width_info->narrow_max);
 
             if (err != RIG_OK)
             {
                 RETURNFUNC(err);
             }
 
-            w = newcat_get_index_from_width(width, priv_caps->ssb_widths);
+            w = newcat_get_index_from_width(width, width_info);
             if (w < 0) { RETURNFUNC(w); }
 
             break;
@@ -9280,6 +9287,9 @@ int newcat_set_rx_bandwidth(RIG *rig, vfo_t vfo, rmode_t mode, pbwidth_t width)
 
     } /* end else */
 
+    /* At this point, we have the correct SH index.
+     * Now, all we have to do is format it appropriately
+     */
     if (is_ftdx101d || is_ftdx101mp || is_ft891)
     {
         // some rigs now require the bandwidth be turned "on"

rigs/yaesu/ft950.c
@@ -33,12 +33,14 @@
 
 static const struct newcat_width_info ft950_cw_widths =
 {
+  .narrow_max = 500,
   .count = 14,
   .widths = { 0, 0, 0, 100, 200, 300, 400, 500, 800, 1200, 1400, 1700, 2000, 2400}
 };
 
 static const struct newcat_width_info ft950_ssb_widths =
 {
+  .narrow_max = 1800,
   .count = 21,
   .widths = { 0, 200, 400, 600, 850, 1100, 1350, 1500, 1650, 1800, 1950, 2100, 2250, 2400,
 	      2450, 2500, 2600, 2700, 2800, 2900, 3000}

rigs/yaesu/ft991.c
@@ -171,11 +171,13 @@ int ft991_ext_tokens[] =
 
 // Shared with FT-891
 const struct newcat_width_info ft991_cw_widths = {
+    .narrow_max = 500,
     .count = 18,
     .widths = { 0, 50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 800, 1200, 1400, 1700,
                 2000, 2400, 3000 }
 };
 const struct newcat_width_info ft991_ssb_widths = {
+    .narrow_max = 1800,
     .count = 22,
     .widths = { 0, 200, 400, 600, 850, 1100, 1350, 1500, 1650, 1800, 1950, 2100, 2200, 2300,
                 2400, 2500, 2600, 2700, 2800, 2900, 3000, 3200 }

rigs/yaesu/newcat.h
@@ -110,8 +110,9 @@ struct newcat_roofing_filter
 struct newcat_width_info
 {
     // New entries should go here
-    short count;       // Max value + 1
-    uint16_t widths[]; // Must have values for all indices from 0 to max and must be last in struct
+    uint16_t narrow_max; // Max width for presetting narrow mode
+    short count;         // Max index value + 1
+    uint16_t widths[];   // Must have values for all indices from 0 to max and must be last in struct
 };
 
 struct newcat_priv_caps
@@ -120,6 +121,7 @@ struct newcat_priv_caps
     struct newcat_roofing_filter roofing_filters[NEWCAT_ROOFING_FILTER_COUNT];
     const struct newcat_width_info *ssb_widths;
     const struct newcat_width_info *cw_widths;
+    const struct newcat_width_info *rtty_widths;  // If NULL, use CW widths
 };
 
 /*

commit 3cbc4db83ccdfc90083688b6e4a8a25b9bd7d9dd

fix(yaesu): preserve legacy NewCAT bandwidth handling
FT-450, FT-450D, and FTDX9000 caps do not provide private
width tables. Shared width selection dereferenced the null caps before
the legacy mapping in both the mode set and get paths.

Centralize width-table selection behind a null-safe helper and cover
legacy USB and CW round trips with socket-backed tests.

Fixes: 4053430b621b ("More trading a little more data for less code")
Fixes: 6ad414dc8275 ("Start converting get_rx_bandwidth to common CW/RTTY/SSB handling")

rigs/yaesu/newcat.c
@@ -717,6 +717,41 @@ static int newcat_get_index_from_width(pbwidth_t width, const struct newcat_widt
     return info->count - 1; // Not found, use the maximum
 }
 
+static const struct newcat_width_info *newcat_width_info_for_mode(
+    const struct newcat_priv_caps *priv_caps, rmode_t mode)
+{
+    if (priv_caps == NULL)
+    {
+        return NULL;
+    }
+
+    switch (mode)
+    {
+    case RIG_MODE_RTTY:
+    case RIG_MODE_RTTYR:
+        if (priv_caps->rtty_widths != NULL)
+        {
+            return priv_caps->rtty_widths;
+        }
+
+        HL_FALLTHROUGH
+
+    case RIG_MODE_PKTUSB:
+    case RIG_MODE_PKTLSB:
+    case RIG_MODE_CW:
+    case RIG_MODE_CWR:
+    case RIG_MODE_PSK:
+        return priv_caps->cw_widths;
+
+    case RIG_MODE_LSB:
+    case RIG_MODE_USB:
+        return priv_caps->ssb_widths;
+
+    default:
+        return &dummy_widths;
+    }
+}
+
 /*
  * ************************************
  *
@@ -8700,41 +8735,7 @@ int newcat_set_rx_bandwidth(RIG *rig, vfo_t vfo, rmode_t mode, pbwidth_t width)
 
     // NOTE: RIG_PASSBAND_NORMAL (0) should select the default filter width (SH00)
 
-    // Preselect set of possible widths
-    switch (mode)
-    {
-    case RIG_MODE_RTTY:
-    case RIG_MODE_RTTYR:
-        width_info = priv_caps->rtty_widths;  // Set RTTY data if different
-        HL_FALLTHROUGH                        // Fall into CW
-    case RIG_MODE_PKTUSB:
-    case RIG_MODE_PKTLSB:
-    case RIG_MODE_CW:
-    case RIG_MODE_CWR:
-    case RIG_MODE_PSK:     // For FTX-1
-        if (width_info == NULL)
-        {
-            width_info = priv_caps->cw_widths;
-        }
-        break;
-
-    case RIG_MODE_LSB:
-    case RIG_MODE_USB:
-        width_info = priv_caps->ssb_widths;
-        break;
-
-#if 0
-    case RIG_MODE_AM:
-    case RIG_MODE_FM:
-    case RIG_MODE_PKTFM:
-    case RIG_MODE_FMN:
-        //????
-        break;
-#endif
-    default:
-        width_info = &dummy_widths;  // Just in case...
-
-    }
+    width_info = newcat_width_info_for_mode(priv_caps, mode);
 
     if (is_ft950)
     {
@@ -9437,40 +9438,7 @@ int newcat_get_rx_bandwidth(RIG *rig, vfo_t vfo, rmode_t mode, pbwidth_t *width)
         main_sub_vfo = (RIG_VFO_B == vfo || RIG_VFO_SUB == vfo) ? '1' : '0';
     }
 
-    // Preselect set of possible widths
-    switch (mode)
-    {
-    case RIG_MODE_RTTY:
-    case RIG_MODE_RTTYR:
-        width_info = priv_caps->rtty_widths;  // Set RTTY data if different
-        HL_FALLTHROUGH                        // Fall into CW
-    case RIG_MODE_PKTUSB:
-    case RIG_MODE_PKTLSB:
-    case RIG_MODE_CW:
-    case RIG_MODE_CWR:
-    case RIG_MODE_PSK:     // For FTX-1
-        if (width_info == NULL)
-        {
-            width_info = priv_caps->cw_widths;
-        }
-        break;
-
-    case RIG_MODE_LSB:
-    case RIG_MODE_USB:
-        width_info = priv_caps->ssb_widths;
-        break;
-
-#if 0
-    case RIG_MODE_AM:
-    case RIG_MODE_FM:
-    case RIG_MODE_PKTFM:
-    case RIG_MODE_FMN:
-        //????
-        break;
-#endif
-    default:
-        width_info = &dummy_widths;  // Just in case...
-    }
+    width_info = newcat_width_info_for_mode(priv_caps, mode);
 
     if (sh_command_valid)
     {

tests/Makefile.am
@@ -25,6 +25,7 @@ DIRECT_TESTS = \
 	testgeministatus \
 	testgs100 \
 	testicomts \
+	testnewcatwidth \
 	testthd7x \
 	testthd75emu
 
@@ -110,6 +111,7 @@ testdebug_CFLAGS = $(AM_CFLAGS) $(PTHREAD_CFLAGS)
 testctlparser_CFLAGS = $(AM_CFLAGS) $(PTHREAD_CFLAGS)
 testgeministatus_CFLAGS = $(AM_CFLAGS) $(PTHREAD_CFLAGS)
 testgs100_CFLAGS = $(AM_CFLAGS) $(PTHREAD_CFLAGS)
+testnewcatwidth_CFLAGS = $(AM_CFLAGS) $(PTHREAD_CFLAGS)
 testicomts_CPPFLAGS = $(AM_CPPFLAGS) -I$(top_srcdir)/rigs/icom
 testgeministatus_CPPFLAGS = $(AM_CPPFLAGS) -I$(top_srcdir)/amplifiers/gemini
 testftx1parsers_CPPFLAGS = $(AM_CPPFLAGS) -I$(top_srcdir)/rigs/yaesu/ftx1
@@ -142,6 +144,7 @@ testicomts_LDADD = $(top_builddir)/rigs/icom/libhamlib-icom.la $(LDADD)
 testgeministatus_LDADD = $(PTHREAD_LIBS) $(top_builddir)/amplifiers/gemini/libhamlib-gemini.la $(LDADD)
 testgs100_LDADD = $(PTHREAD_LIBS) $(top_builddir)/rigs/gomspace/libhamlib-gomspace.la $(LDADD)
 testftx1parsers_LDADD = $(top_builddir)/rigs/yaesu/libhamlib-yaesu.la $(LDADD)
+testnewcatwidth_LDADD = $(PTHREAD_LIBS) $(top_builddir)/rigs/yaesu/libhamlib-yaesu.la $(LDADD)
 if TESTS_HAVE_LIBUSB
     rigtestlibusb_LDADD = $(LIBUSB_LIBS)
 endif

tests/testnewcatwidth.c
@@ -1,0 +1,286 @@
+/*
+ * Hamlib Yaesu NewCAT legacy bandwidth tests
+ * Copyright (c) 2026 by Hamlib Team
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ */
+
+#ifdef _WIN32
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("testnewcatwidth: skipped, requires socketpair()\n");
+    return 77;
+}
+
+#else
+
+#include <poll.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include "hamlib/rig.h"
+#include "hamlib/port.h"
+#include "hamlib/rig_state.h"
+
+extern struct rig_caps ft450_caps;
+extern struct rig_caps ft9000_caps;
+
+enum test_operation
+{
+    TEST_SET_MODE,
+    TEST_GET_MODE
+};
+
+struct peer_case
+{
+    int fd;
+    const char *mode_command;
+    const char *mode_response;
+    const char *width_command;
+    const char *width_response;
+    int status;
+};
+
+struct width_case
+{
+    const char *name;
+    rig_model_t model;
+    enum test_operation operation;
+    rmode_t mode;
+    pbwidth_t width;
+    const char *mode_command;
+    const char *mode_response;
+    const char *width_command;
+    const char *width_response;
+};
+
+static int read_frame(int fd, char *buffer, size_t capacity)
+{
+    size_t used = 0;
+
+    while (used + 1 < capacity)
+    {
+        struct pollfd descriptor = { .fd = fd, .events = POLLIN };
+        ssize_t count;
+
+        if (poll(&descriptor, 1, 2000) != 1)
+        {
+            return -1;
+        }
+
+        count = read(fd, buffer + used, 1);
+
+        if (count != 1)
+        {
+            return -1;
+        }
+
+        if (buffer[used++] == ';')
+        {
+            buffer[used] = '\0';
+            return 0;
+        }
+    }
+
+    return -1;
+}
+
+static int write_all(int fd, const char *buffer)
+{
+    size_t length = strlen(buffer);
+    size_t written = 0;
+
+    while (written < length)
+    {
+        ssize_t count = write(fd, buffer + written, length - written);
+
+        if (count <= 0)
+        {
+            return -1;
+        }
+
+        written += (size_t)count;
+    }
+
+    return 0;
+}
+
+static int exchange(int fd, const char *expected, const char *response)
+{
+    char command[32];
+
+    if (read_frame(fd, command, sizeof(command)) != 0
+            || strcmp(command, expected) != 0)
+    {
+        return -1;
+    }
+
+    if (response != NULL && write_all(fd, response) != 0)
+    {
+        return -1;
+    }
+
+    return 0;
+}
+
+static void *run_peer(void *arg)
+{
+    struct peer_case *test = arg;
+
+    test->status = -1;
+
+    if (exchange(test->fd, test->mode_command,
+                 test->mode_response) != 0
+            || exchange(test->fd, test->width_command,
+                        test->width_response) != 0)
+    {
+        return NULL;
+    }
+
+    test->status = 0;
+    return NULL;
+}
+
+static int run_case(const struct width_case *test_case)
+{
+    int sockets[2];
+    pthread_t thread;
+    struct peer_case peer =
+    {
+        .fd = -1,
+        .mode_command = test_case->mode_command,
+        .mode_response = test_case->mode_response,
+        .width_command = test_case->width_command,
+        .width_response = test_case->width_response,
+        .status = -1
+    };
+    RIG *rig;
+    rmode_t mode = test_case->mode;
+    pbwidth_t width = test_case->width;
+    int retval;
+
+    if (socketpair(AF_UNIX, SOCK_STREAM, 0, sockets) != 0)
+    {
+        perror("socketpair");
+        return 1;
+    }
+
+    rig = rig_init(test_case->model);
+
+    if (rig == NULL)
+    {
+        close(sockets[0]);
+        close(sockets[1]);
+        return 1;
+    }
+
+    RIGPORT(rig)->fd = sockets[0];
+    RIGPORT(rig)->timeout = 100;
+    RIGPORT(rig)->retry = 0;
+    STATE(rig)->powerstat = RIG_POWER_ON;
+    peer.fd = sockets[1];
+
+    if (pthread_create(&thread, NULL, run_peer, &peer) != 0)
+    {
+        RIGPORT(rig)->fd = -1;
+        rig_cleanup(rig);
+        close(sockets[0]);
+        close(sockets[1]);
+        return 1;
+    }
+
+    if (test_case->operation == TEST_SET_MODE)
+    {
+        retval = rig->caps->set_mode(rig, RIG_VFO_A, mode, width);
+    }
+    else
+    {
+        mode = RIG_MODE_NONE;
+        width = RIG_PASSBAND_NORMAL;
+        retval = rig->caps->get_mode(rig, RIG_VFO_A, &mode, &width);
+    }
+
+    pthread_join(thread, NULL);
+    RIGPORT(rig)->fd = -1;
+    rig_cleanup(rig);
+    close(sockets[0]);
+    close(sockets[1]);
+
+    if (peer.status != 0 || retval != RIG_OK
+            || mode != test_case->mode || width != test_case->width)
+    {
+        fprintf(stderr,
+                "%s: expected %d/%s/%d, got %d/%s/%d (peer %d)\n",
+                test_case->name, RIG_OK, rig_strrmode(test_case->mode),
+                (int)test_case->width, retval, rig_strrmode(mode),
+                (int)width, peer.status);
+        return 1;
+    }
+
+    return 0;
+}
+
+int main(void)
+{
+    static const struct width_case cases[] =
+    {
+        {
+            "FT-450 set USB", RIG_MODEL_FT450, TEST_SET_MODE,
+            RIG_MODE_USB, 2400, "MD02;", NULL, "SH016;", NULL
+        },
+        {
+            "FT-450 get USB", RIG_MODEL_FT450, TEST_GET_MODE,
+            RIG_MODE_USB, 2400, "MD0;", "MD02;", "SH0;", "SH016;"
+        },
+        {
+            "FT-450 set CW", RIG_MODEL_FT450, TEST_SET_MODE,
+            RIG_MODE_CW, 500, "MD03;", NULL, "SH006;", NULL
+        },
+        {
+            "FT-450 get CW", RIG_MODEL_FT450, TEST_GET_MODE,
+            RIG_MODE_CW, 500, "MD0;", "MD03;", "SH0;", "SH006;"
+        },
+        {
+            "FTDX-9000 set USB", RIG_MODEL_FT9000, TEST_SET_MODE,
+            RIG_MODE_USB, 2400, "MD02;", NULL, "SH016;", NULL
+        },
+        {
+            "FTDX-9000 get USB", RIG_MODEL_FT9000, TEST_GET_MODE,
+            RIG_MODE_USB, 2400, "MD0;", "MD02;", "SH0;", "SH016;"
+        },
+        {
+            "FTDX-9000 set CW", RIG_MODEL_FT9000, TEST_SET_MODE,
+            RIG_MODE_CW, 500, "MD03;", NULL, "SH006;", NULL
+        },
+        {
+            "FTDX-9000 get CW", RIG_MODEL_FT9000, TEST_GET_MODE,
+            RIG_MODE_CW, 500, "MD0;", "MD03;", "SH0;", "SH006;"
+        }
+    };
+    size_t i;
+
+    rig_set_debug(RIG_DEBUG_NONE);
+    rig_register(&ft450_caps);
+    rig_register(&ft9000_caps);
+
+    for (i = 0; i < sizeof(cases) / sizeof(cases[0]); i++)
+    {
+        if (run_case(&cases[i]) != 0)
+        {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+#endif /* !_WIN32 */