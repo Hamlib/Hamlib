Hamlib/Hamlib

commit 6362407aa96774f058f6c87d663964ff555aa430

Add Yaesu FTX-1 backend driver
Comprehensive Hamlib backend for the Yaesu FTX-1 QRP transceiver
(5-100W HF, 50W 2m, 20W 70cm) using newcat CAT protocol variant.

Features implemented:
- VFO A/B control with frequency, mode, and split operation
- RIT/XIT control (±9999 Hz)
- All filter bandwidth settings (SH/SL/NA commands)
- DSP: NR, NB, ANF, APF, Notch, Contour
- Audio: AF, RF gain, Squelch, Monitor
- TX: Power (including 0.5W steps), Mic gain, VOX, Compression
- CW keyer: Speed, Break-in delay, Paddle ratio
- Memory channel read/write (1-99, PMS, 60m)
- Tuning step control and scan operations
- CTCSS/DCS encode/decode
- Preamp (IPO/AMP1/AMP2) and Attenuator (12dB)

Tested against FTX-1 firmware v1.08+ with 85 CAT commands verified.

Note: Several discrepancies between CAT manual (2508-C) and actual
firmware documented in code comments (ID returns 0763 not 0840,
8 documented commands return '?', PC uses decimal format, etc.)

Signed-off-by: Terrell Deppe KJ5HST

rigs/yaesu/ftx1_audio.c
@@ -1,0 +1,670 @@
+/*
+ * Hamlib Yaesu backend - FTX-1 Audio and Levels Group
+ * Copyright (c) 2025 by Terrell Deppe (KJ5HST)
+ *
+ * This file implements CAT commands for audio gain, RF gain, meters, and AGC.
+ *
+ * CAT Commands in this file:
+ *   AG P1 P2P3P4;    - AF Gain (P1=VFO 0/1, P2-P4=000-255)
+ *   RG P1 P2P3P4;    - RF Gain (P1=VFO 0/1, P2-P4=000-255)
+ *   MG P1P2P3;       - Mic Gain (000-100)
+ *   GT P1P2P3P4;     - AGC Time Constant (0000-6000ms or code)
+ *   ML P1P2P3;       - Monitor Level (000-100)
+ *   SM P1;           - S-Meter Read (P1=0 main, returns 0000-0255)
+ *   RM P1;           - Meter Read (1=COMP, 2=ALC, 3=PO, 4=SWR, 5=ID, 6=VDD)
+ *   PC P1 P2P2P2;    - Power Control (P1=1 field head, P1=2 SPA-1)
+ *                      Field head: 0.5-10W (can return decimal: PC10.5, PC11.5)
+ *                      SPA-1: 5-100W (PC2005 to PC2100)
+ *   VG P1P2P3;       - VOX Gain (000-100)
+ *   VD P1P2P3P4;     - VOX Delay (0030-3000ms)
+ *   SD P1P2P3P4;     - CW Break-in Delay (0030-3000ms)
+ */
+
+#include <stdlib.h>
+#include <string.h>
+#include <hamlib/rig.h>
+#include "misc.h"
+#include "yaesu.h"
+#include "newcat.h"
+
+/* Helper macro to convert VFO to P1 parameter (0=MAIN, 1=SUB) */
+#define FTX1_VFO_TO_P1(vfo) \
+    ((vfo == RIG_VFO_CURR || vfo == RIG_VFO_MAIN || vfo == RIG_VFO_A) ? 0 : 1)
+
+/* Level ranges */
+#define FTX1_AF_GAIN_MIN 0
+#define FTX1_AF_GAIN_MAX 255
+#define FTX1_RF_GAIN_MIN 0
+#define FTX1_RF_GAIN_MAX 255
+#define FTX1_MIC_GAIN_MIN 0
+#define FTX1_MIC_GAIN_MAX 100
+#define FTX1_POWER_MIN_FIELD 0.5f   /* Field head minimum */
+#define FTX1_POWER_MAX_FIELD 10.0f  /* Field head maximum */
+#define FTX1_POWER_MIN_SPA1 5       /* SPA-1 minimum */
+#define FTX1_POWER_MAX_SPA1 100     /* SPA-1 maximum */
+
+/* Set AF Gain (AG P1 P2P3P4;) */
+int ftx1_set_af_gain(RIG *rig, vfo_t vfo, float val)
+{
+    struct newcat_priv_data *priv = STATE(rig)->priv;
+    int p1 = FTX1_VFO_TO_P1(vfo);
+    int level = (int)(val * FTX1_AF_GAIN_MAX);
+
+    rig_debug(RIG_DEBUG_VERBOSE, "%s: vfo=%s p1=%d val=%f\n", __func__,
+              rig_strvfo(vfo), p1, val);
+
+    if (level < FTX1_AF_GAIN_MIN) level = FTX1_AF_GAIN_MIN;
+    if (level > FTX1_AF_GAIN_MAX) level = FTX1_AF_GAIN_MAX;
+
+    SNPRINTF(priv->cmd_str, sizeof(priv->cmd_str), "AG%d%03d;", p1, level);
+    return newcat_set_cmd(rig);
+}
+
+/* Get AF Gain */
+int ftx1_get_af_gain(RIG *rig, vfo_t vfo, float *val)
+{
+    struct newcat_priv_data *priv = STATE(rig)->priv;
+    int ret;
+    int p1 = FTX1_VFO_TO_P1(vfo);
+    int p1_resp, level;
+
+    rig_debug(RIG_DEBUG_VERBOSE, "%s: vfo=%s p1=%d\n", __func__,
+              rig_strvfo(vfo), p1);
+
+    SNPRINTF(priv->cmd_str, sizeof(priv->cmd_str), "AG%d;", p1);
+
+    ret = newcat_get_cmd(rig);
+    if (ret != RIG_OK) return ret;
+
+    if (sscanf(priv->ret_data + 2, "%1d%3d", &p1_resp, &level) != 2)
+    {
+        rig_debug(RIG_DEBUG_ERR, "%s: failed to parse '%s'\n", __func__,
+                  priv->ret_data);
+        return -RIG_EPROTO;
+    }
+
+    *val = (float)level / FTX1_AF_GAIN_MAX;
+    return RIG_OK;
+}
+
+/* Set RF Gain (RG P1 P2P3P4;) */
+int ftx1_set_rf_gain(RIG *rig, vfo_t vfo, float val)
+{
+    struct newcat_priv_data *priv = STATE(rig)->priv;
+    int p1 = FTX1_VFO_TO_P1(vfo);
+    int level = (int)(val * FTX1_RF_GAIN_MAX);
+
+    rig_debug(RIG_DEBUG_VERBOSE, "%s: vfo=%s p1=%d val=%f\n", __func__,
+              rig_strvfo(vfo), p1, val);
+
+    if (level < FTX1_RF_GAIN_MIN) level = FTX1_RF_GAIN_MIN;
+    if (level > FTX1_RF_GAIN_MAX) level = FTX1_RF_GAIN_MAX;
+
+    SNPRINTF(priv->cmd_str, sizeof(priv->cmd_str), "RG%d%03d;", p1, level);
+    return newcat_set_cmd(rig);
+}
+
+/* Get RF Gain */
+int ftx1_get_rf_gain(RIG *rig, vfo_t vfo, float *val)
+{
+    struct newcat_priv_data *priv = STATE(rig)->priv;
+    int ret;
+    int p1 = FTX1_VFO_TO_P1(vfo);
+    int p1_resp, level;
+
+    rig_debug(RIG_DEBUG_VERBOSE, "%s: vfo=%s p1=%d\n", __func__,
+              rig_strvfo(vfo), p1);
+
+    SNPRINTF(priv->cmd_str, sizeof(priv->cmd_str), "RG%d;", p1);
+
+    ret = newcat_get_cmd(rig);
+    if (ret != RIG_OK) return ret;
+
+    if (sscanf(priv->ret_data + 2, "%1d%3d", &p1_resp, &level) != 2)
+    {
+        rig_debug(RIG_DEBUG_ERR, "%s: failed to parse '%s'\n", __func__,
+                  priv->ret_data);
+        return -RIG_EPROTO;
+    }
+
+    *val = (float)level / FTX1_RF_GAIN_MAX;
+    return RIG_OK;
+}
+
+/* Set Mic Gain (MG P1P2P3;) */
+int ftx1_set_mic_gain(RIG *rig, float val)
+{
+    struct newcat_priv_data *priv = STATE(rig)->priv;
+    int level = (int)(val * FTX1_MIC_GAIN_MAX);
+
+    rig_debug(RIG_DEBUG_VERBOSE, "%s: val=%f\n", __func__, val);
+
+    if (level < FTX1_MIC_GAIN_MIN) level = FTX1_MIC_GAIN_MIN;
+    if (level > FTX1_MIC_GAIN_MAX) level = FTX1_MIC_GAIN_MAX;
+
+    SNPRINTF(priv->cmd_str, sizeof(priv->cmd_str), "MG%03d;", level);
+    return newcat_set_cmd(rig);
+}
+
+/* Get Mic Gain */
+int ftx1_get_mic_gain(RIG *rig, float *val)
+{
+    struct newcat_priv_data *priv = STATE(rig)->priv;
+    int ret;
+    int level;
+
+    rig_debug(RIG_DEBUG_VERBOSE, "%s\n", __func__);
+
+    SNPRINTF(priv->cmd_str, sizeof(priv->cmd_str), "MG;");
+
+    ret = newcat_get_cmd(rig);
+    if (ret != RIG_OK) return ret;
+
+    if (sscanf(priv->ret_data + 2, "%3d", &level) != 1)
+    {
+        rig_debug(RIG_DEBUG_ERR, "%s: failed to parse '%s'\n", __func__,
+                  priv->ret_data);
+        return -RIG_EPROTO;
+    }
+
+    *val = (float)level / FTX1_MIC_GAIN_MAX;
+    return RIG_OK;
+}
+
+/* Get S-Meter (SM P1;) */
+int ftx1_get_smeter(RIG *rig, vfo_t vfo, int *val)
+{
+    struct newcat_priv_data *priv = STATE(rig)->priv;
+    int ret;
+    int p1 = FTX1_VFO_TO_P1(vfo);
+    int p1_resp, level;
+
+    rig_debug(RIG_DEBUG_VERBOSE, "%s: vfo=%s p1=%d\n", __func__,
+              rig_strvfo(vfo), p1);
+
+    SNPRINTF(priv->cmd_str, sizeof(priv->cmd_str), "SM%d;", p1);
+
+    ret = newcat_get_cmd(rig);
+    if (ret != RIG_OK) return ret;
+
+    /* Response: SM P1 P2P3P4P5; (4 digits) */
+    if (sscanf(priv->ret_data + 2, "%1d%4d", &p1_resp, &level) != 2)
+    {
+        rig_debug(RIG_DEBUG_ERR, "%s: failed to parse '%s'\n", __func__,
+                  priv->ret_data);
+        return -RIG_EPROTO;
+    }
+
+    *val = level;
+    return RIG_OK;
+}
+
+/* Get Meter (RM P1;) - various meters */
+int ftx1_get_meter(RIG *rig, int meter_type, int *val)
+{
+    struct newcat_priv_data *priv = STATE(rig)->priv;
+    int ret;
+    int p1_resp, level;
+
+    rig_debug(RIG_DEBUG_VERBOSE, "%s: meter_type=%d\n", __func__, meter_type);
+
+    /* meter_type: 1=COMP, 2=ALC, 3=PO, 4=SWR, 5=ID, 6=VDD */
+    if (meter_type < 1 || meter_type > 6)
+    {
+        return -RIG_EINVAL;
+    }
+
+    SNPRINTF(priv->cmd_str, sizeof(priv->cmd_str), "RM%d;", meter_type);
+
+    ret = newcat_get_cmd(rig);
+    if (ret != RIG_OK) return ret;
+
+    /* Response: RM P1 P2P3P4; */
+    if (sscanf(priv->ret_data + 2, "%1d%3d", &p1_resp, &level) != 2)
+    {
+        rig_debug(RIG_DEBUG_ERR, "%s: failed to parse '%s'\n", __func__,
+                  priv->ret_data);
+        return -RIG_EPROTO;
+    }
+
+    *val = level;
+    return RIG_OK;
+}
+
+/*
+ * ftx1_set_power - Set Power Control
+ *
+ * CAT command: PC P1 P2P2P2;
+ *   P1 = 1 for field head (0.5-10W)
+ *   P1 = 2 for SPA-1 (5-100W)
+ *
+ * For field head, firmware accepts:
+ *   - Whole watts: PC1001, PC1005, PC1010 (3-digit zero-padded)
+ *   - Fractional: PC10.5, PC11.5 (decimal format NOT zero-padded)
+ *
+ * NOTE: Always use P1=1 (field head) since we don't have SPA-1 detection.
+ *       User can configure SPA-1 via extended settings if needed.
+ */
+int ftx1_set_power(RIG *rig, float val)
+{
+    struct newcat_priv_data *priv = STATE(rig)->priv;
+    float watts;
+
+    rig_debug(RIG_DEBUG_VERBOSE, "%s: val=%f\n", __func__, val);
+
+    /* Convert 0.0-1.0 to watts (field head: 0.5-10W) */
+    watts = val * FTX1_POWER_MAX_FIELD;
+
+    if (watts < FTX1_POWER_MIN_FIELD) watts = FTX1_POWER_MIN_FIELD;
+    if (watts > FTX1_POWER_MAX_FIELD) watts = FTX1_POWER_MAX_FIELD;
+
+    /* For field head (P1=1), format depends on whether it's fractional */
+    if (watts == (float)(int)watts)
+    {
+        /* Whole watts: use 3-digit zero-padded format */
+        SNPRINTF(priv->cmd_str, sizeof(priv->cmd_str), "PC1%03d;", (int)watts);
+    }
+    else
+    {
+        /* Fractional watts: use decimal format */
+        SNPRINTF(priv->cmd_str, sizeof(priv->cmd_str), "PC1%.1f;", watts);
+    }
+
+    return newcat_set_cmd(rig);
+}
+
+/*
+ * ftx1_get_power - Get Power Control
+ *
+ * Response formats for field head (P1=1):
+ *   - Whole watts: PC1001, PC1005, PC1010 (3-digit zero-padded)
+ *   - Fractional: PC10.5, PC11.5, PC15.1 (decimal format)
+ */
+int ftx1_get_power(RIG *rig, float *val)
+{
+    struct newcat_priv_data *priv = STATE(rig)->priv;
+    int ret;
+    int p1;
+    float watts;
+
+    rig_debug(RIG_DEBUG_VERBOSE, "%s\n", __func__);
+
+    SNPRINTF(priv->cmd_str, sizeof(priv->cmd_str), "PC;");
+
+    ret = newcat_get_cmd(rig);
+    if (ret != RIG_OK) return ret;
+
+    /* Response: PC P1 P2... where P2 can be 3-digit or decimal */
+    /* Try to parse P1 first (always 1 digit) */
+    if (sscanf(priv->ret_data + 2, "%1d", &p1) != 1)
+    {
+        rig_debug(RIG_DEBUG_ERR, "%s: failed to parse P1 from '%s'\n", __func__,
+                  priv->ret_data);
+        return -RIG_EPROTO;
+    }
+
+    /* Now parse the power value - could be decimal (0.5, 1.5, 5.1) or integer (001, 005, 010) */
+    if (sscanf(priv->ret_data + 3, "%f", &watts) != 1)
+    {
+        rig_debug(RIG_DEBUG_ERR, "%s: failed to parse watts from '%s'\n", __func__,
+                  priv->ret_data);
+        return -RIG_EPROTO;
+    }
+
+    rig_debug(RIG_DEBUG_VERBOSE, "%s: p1=%d watts=%.1f\n", __func__, p1, watts);
+
+    /* Convert watts to 0.0-1.0 range based on head type */
+    if (p1 == 1)
+    {
+        /* Field head: 0.5-10W maps to 0.05-1.0 */
+        *val = watts / FTX1_POWER_MAX_FIELD;
+    }
+    else if (p1 == 2)
+    {
+        /* SPA-1: 5-100W maps to 0.05-1.0 */
+        *val = watts / (float)FTX1_POWER_MAX_SPA1;
+    }
+    else
+    {
+        rig_debug(RIG_DEBUG_ERR, "%s: unexpected P1=%d\n", __func__, p1);
+        *val = watts / FTX1_POWER_MAX_FIELD;  /* Default to field head */
+    }
+
+    return RIG_OK;
+}
+
+/* Set VOX Gain (VG P1P2P3;) */
+int ftx1_set_vox_gain(RIG *rig, float val)
+{
+    struct newcat_priv_data *priv = STATE(rig)->priv;
+    int level = (int)(val * 100);
+
+    rig_debug(RIG_DEBUG_VERBOSE, "%s: val=%f\n", __func__, val);
+
+    if (level < 0) level = 0;
+    if (level > 100) level = 100;
+
+    SNPRINTF(priv->cmd_str, sizeof(priv->cmd_str), "VG%03d;", level);
+    return newcat_set_cmd(rig);
+}
+
+/* Get VOX Gain */
+int ftx1_get_vox_gain(RIG *rig, float *val)
+{
+    struct newcat_priv_data *priv = STATE(rig)->priv;
+    int ret;
+    int level;
+
+    rig_debug(RIG_DEBUG_VERBOSE, "%s\n", __func__);
+
+    SNPRINTF(priv->cmd_str, sizeof(priv->cmd_str), "VG;");
+
+    ret = newcat_get_cmd(rig);
+    if (ret != RIG_OK) return ret;
+
+    if (sscanf(priv->ret_data + 2, "%3d", &level) != 1)
+    {
+        rig_debug(RIG_DEBUG_ERR, "%s: failed to parse '%s'\n", __func__,
+                  priv->ret_data);
+        return -RIG_EPROTO;
+    }
+
+    *val = (float)level / 100.0f;
+    return RIG_OK;
+}
+
+/*
+ * ftx1_set_vox_delay - Set VOX Delay
+ * CAT command: VD P1P2; (2 digits, 00-30)
+ *
+ * FTX-1 uses 00-30 range (100ms units: 0=0ms to 30=3000ms)
+ * Hamlib VOXDELAY is in tenths of seconds (0-30 maps to 0-3.0s)
+ */
+int ftx1_set_vox_delay(RIG *rig, int tenths)
+{
+    struct newcat_priv_data *priv = STATE(rig)->priv;
+
+    rig_debug(RIG_DEBUG_VERBOSE, "%s: tenths=%d\n", __func__, tenths);
+
+    if (tenths < 0) tenths = 0;
+    if (tenths > 30) tenths = 30;
+
+    SNPRINTF(priv->cmd_str, sizeof(priv->cmd_str), "VD%02d;", tenths);
+    return newcat_set_cmd(rig);
+}
+
+/* Get VOX Delay */
+int ftx1_get_vox_delay(RIG *rig, int *tenths)
+{
+    struct newcat_priv_data *priv = STATE(rig)->priv;
+    int ret;
+    int delay;
+
+    rig_debug(RIG_DEBUG_VERBOSE, "%s\n", __func__);
+
+    SNPRINTF(priv->cmd_str, sizeof(priv->cmd_str), "VD;");
+
+    ret = newcat_get_cmd(rig);
+    if (ret != RIG_OK) return ret;
+
+    /* Response: VD00 to VD30 (2 digits) */
+    if (sscanf(priv->ret_data + 2, "%2d", &delay) != 1)
+    {
+        rig_debug(RIG_DEBUG_ERR, "%s: failed to parse '%s'\n", __func__,
+                  priv->ret_data);
+        return -RIG_EPROTO;
+    }
+
+    *tenths = delay;
+    return RIG_OK;
+}
+
+/*
+ * ftx1_set_monitor_level - Set Monitor Level (ML P1 P2P3P4;)
+ * CAT command: ML P1 P2P3P4; (P1=VFO 0/1, P2-P4=000-100)
+ *
+ * FTX-1 uses P1=0 for MAIN VFO
+ */
+int ftx1_set_monitor_level(RIG *rig, float val)
+{
+    struct newcat_priv_data *priv = STATE(rig)->priv;
+    int level = (int)(val * 100);
+
+    rig_debug(RIG_DEBUG_VERBOSE, "%s: val=%f\n", __func__, val);
+
+    if (level < 0) level = 0;
+    if (level > 100) level = 100;
+
+    /* FTX-1 format: ML P1 P2P3P4; where P1=VFO (0=MAIN) */
+    SNPRINTF(priv->cmd_str, sizeof(priv->cmd_str), "ML0%03d;", level);
+    return newcat_set_cmd(rig);
+}
+
+/*
+ * ftx1_get_monitor_level - Get Monitor Level
+ * Response: ML P1 P2P3P4; (P1=VFO, P2-P4=level)
+ */
+int ftx1_get_monitor_level(RIG *rig, float *val)
+{
+    struct newcat_priv_data *priv = STATE(rig)->priv;
+    int ret;
+    int p1, level;
+
+    rig_debug(RIG_DEBUG_VERBOSE, "%s\n", __func__);
+
+    /* Query MAIN VFO: ML0; */
+    SNPRINTF(priv->cmd_str, sizeof(priv->cmd_str), "ML0;");
+
+    ret = newcat_get_cmd(rig);
+    if (ret != RIG_OK) return ret;
+
+    /* Response: ML0100 (P1=0, level=100) */
+    if (sscanf(priv->ret_data + 2, "%1d%3d", &p1, &level) != 2)
+    {
+        rig_debug(RIG_DEBUG_ERR, "%s: failed to parse '%s'\n", __func__,
+                  priv->ret_data);
+        return -RIG_EPROTO;
+    }
+
+    *val = (float)level / 100.0f;
+    return RIG_OK;
+}
+
+/*
+ * ftx1_set_agc - Set AGC Time Constant
+ * CAT command: GT P1 P2; (P1=VFO 0/1, P2=mode 0-4)
+ *
+ * FTX-1 AGC values (P2):
+ *   0 = OFF
+ *   1 = FAST
+ *   2 = MID
+ *   3 = SLOW
+ *   4 = AUTO
+ *
+ * Note: Always uses P1=0 (MAIN VFO) for now
+ */
+int ftx1_set_agc(RIG *rig, int val)
+{
+    struct newcat_priv_data *priv = STATE(rig)->priv;
+
+    rig_debug(RIG_DEBUG_VERBOSE, "%s: val=%d\n", __func__, val);
+
+    /* val is AGC setting code (0-4 for FTX-1) */
+    /* Format: GT P1 P2; where P1=VFO (0=MAIN), P2=mode */
+    SNPRINTF(priv->cmd_str, sizeof(priv->cmd_str), "GT0%d;", val);
+    return newcat_set_cmd(rig);
+}
+
+/* Get AGC */
+int ftx1_get_agc(RIG *rig, int *val)
+{
+    struct newcat_priv_data *priv = STATE(rig)->priv;
+    int ret;
+    int p1, agc;
+
+    rig_debug(RIG_DEBUG_VERBOSE, "%s\n", __func__);
+
+    /* Query MAIN VFO AGC: GT0; */
+    SNPRINTF(priv->cmd_str, sizeof(priv->cmd_str), "GT0;");
+
+    ret = newcat_get_cmd(rig);
+    if (ret != RIG_OK) return ret;
+
+    /* Response: GT0X where X is AGC mode (0-6, 4-6 are AUTO variants) */
+    if (sscanf(priv->ret_data + 2, "%1d%1d", &p1, &agc) != 2)
+    {
+        rig_debug(RIG_DEBUG_ERR, "%s: failed to parse '%s'\n", __func__,
+                  priv->ret_data);
+        return -RIG_EPROTO;
+    }
+
+    *val = agc;
+    return RIG_OK;
+}
+
+/*
+ * ftx1_set_amc_output - Set AMC Output Level (AO command)
+ * CAT command: AO P1P2P3; (000-100)
+ */
+int ftx1_set_amc_output(RIG *rig, float val)
+{
+    struct newcat_priv_data *priv = STATE(rig)->priv;
+    int level = (int)(val * 100);
+
+    rig_debug(RIG_DEBUG_VERBOSE, "%s: val=%f\n", __func__, val);
+
+    if (level < 0) level = 0;
+    if (level > 100) level = 100;
+
+    SNPRINTF(priv->cmd_str, sizeof(priv->cmd_str), "AO%03d;", level);
+    return newcat_set_cmd(rig);
+}
+
+/* Get AMC Output Level */
+int ftx1_get_amc_output(RIG *rig, float *val)
+{
+    struct newcat_priv_data *priv = STATE(rig)->priv;
+    int ret;
+    int level;
+
+    rig_debug(RIG_DEBUG_VERBOSE, "%s\n", __func__);
+
+    SNPRINTF(priv->cmd_str, sizeof(priv->cmd_str), "AO;");
+
+    ret = newcat_get_cmd(rig);
+    if (ret != RIG_OK) return ret;
+
+    /* Response: AO P1P2P3; (3 digits 000-100) */
+    if (sscanf(priv->ret_data + 2, "%3d", &level) != 1)
+    {
+        rig_debug(RIG_DEBUG_ERR, "%s: failed to parse '%s'\n", __func__,
+                  priv->ret_data);
+        return -RIG_EPROTO;
+    }
+
+    *val = (float)level / 100.0f;
+    return RIG_OK;
+}
+
+/*
+ * ftx1_set_width - Set Filter Width (SH command)
+ * CAT command: SH P1 P2 P3P4; (P1=VFO, P2=0, P3P4=00-23)
+ *
+ * FTX-1 width codes (00-23):
+ *   00=200, 01=250, 02=300, 03=350, 04=400, 05=450, 06=500
+ *   07=600, 08=700, 09=800, 10=900, 11=1000, 12=1200, 13=1400
+ *   14=1600, 15=1800, 16=2000, 17=2200, 18=2400, 19=2600
+ *   20=2800, 21=3000, 22=3200, 23=3400
+ */
+int ftx1_set_width(RIG *rig, vfo_t vfo, int width_code)
+{
+    struct newcat_priv_data *priv = STATE(rig)->priv;
+    int p1 = FTX1_VFO_TO_P1(vfo);
+
+    rig_debug(RIG_DEBUG_VERBOSE, "%s: vfo=%s width_code=%d\n", __func__,
+              rig_strvfo(vfo), width_code);
+
+    if (width_code < 0) width_code = 0;
+    if (width_code > 23) width_code = 23;
+
+    SNPRINTF(priv->cmd_str, sizeof(priv->cmd_str), "SH%d0%02d;", p1, width_code);
+    return newcat_set_cmd(rig);
+}
+
+/* Get Filter Width */
+int ftx1_get_width(RIG *rig, vfo_t vfo, int *width_code)
+{
+    struct newcat_priv_data *priv = STATE(rig)->priv;
+    int ret;
+    int p1 = FTX1_VFO_TO_P1(vfo);
+    int p1_resp, p2, width;
+
+    rig_debug(RIG_DEBUG_VERBOSE, "%s: vfo=%s\n", __func__, rig_strvfo(vfo));
+
+    SNPRINTF(priv->cmd_str, sizeof(priv->cmd_str), "SH%d;", p1);
+
+    ret = newcat_get_cmd(rig);
+    if (ret != RIG_OK) return ret;
+
+    /* Response: SH P1 P2 P3P4; (P1=VFO, P2=0, P3P4=width code) */
+    if (sscanf(priv->ret_data + 2, "%1d%1d%2d", &p1_resp, &p2, &width) != 3)
+    {
+        rig_debug(RIG_DEBUG_ERR, "%s: failed to parse '%s'\n", __func__,
+                  priv->ret_data);
+        return -RIG_EPROTO;
+    }
+
+    *width_code = width;
+    return RIG_OK;
+}
+
+/*
+ * ftx1_set_meter_switch - Set Meter Switch (MS command)
+ * CAT command: MS P1P2; (P1=VFO 0/1, P2=meter type 0-5)
+ *
+ * Meter types:
+ *   0 = S-meter
+ *   1 = COMP
+ *   2 = ALC
+ *   3 = PO (Power Output)
+ *   4 = SWR
+ *   5 = ID
+ */
+int ftx1_set_meter_switch(RIG *rig, int meter_type)
+{
+    struct newcat_priv_data *priv = STATE(rig)->priv;
+
+    rig_debug(RIG_DEBUG_VERBOSE, "%s: meter_type=%d\n", __func__, meter_type);
+
+    if (meter_type < 0) meter_type = 0;
+    if (meter_type > 5) meter_type = 5;
+
+    SNPRINTF(priv->cmd_str, sizeof(priv->cmd_str), "MS0%d;", meter_type);
+    return newcat_set_cmd(rig);
+}
+
+/* Get Meter Switch */
+int ftx1_get_meter_switch(RIG *rig, int *meter_type)
+{
+    struct newcat_priv_data *priv = STATE(rig)->priv;
+    int ret;
+    int p1, mt;
+
+    rig_debug(RIG_DEBUG_VERBOSE, "%s\n", __func__);
+
+    SNPRINTF(priv->cmd_str, sizeof(priv->cmd_str), "MS;");
+
+    ret = newcat_get_cmd(rig);
+    if (ret != RIG_OK) return ret;
+
+    /* Response: MS P1P2; */
+    if (sscanf(priv->ret_data + 2, "%1d%1d", &p1, &mt) != 2)
+    {
+        rig_debug(RIG_DEBUG_ERR, "%s: failed to parse '%s'\n", __func__,
+                  priv->ret_data);
+        return -RIG_EPROTO;
+    }
+
+    *meter_type = mt;
+    return RIG_OK;
+}

commit 170a4c42e629d93a8d016db9ef7271a1e4280336

fix(ftx1): prevent monitor control from keying transmitter
Map Hamlib monitor control to the FTX-1 ML on/off selector.

Keep ML1 monitor gain separate and validate both response formats.

Fixes: 6362407aa967 ("Add Yaesu FTX-1 backend driver")

rigs/yaesu/ftx1/ftx1.h
@@ -274,6 +274,8 @@ extern int ftx1_parse_clar_offset(const char *response, char vfo,
                                   shortfreq_t *offset);
 extern int ftx1_parse_ex_menu_response(const char *response, int group,
                                        int section, int item, int *value);
+extern int ftx1_parse_monitor_response(const char *response, int selector,
+                                       int *level);
 extern int ftx1_parse_smeter_response(const char *response, int p1, int *value);
 
 #endif /* _FTX1_H */

rigs/yaesu/ftx1/ftx1_audio.c
@@ -9,7 +9,7 @@
  *   RG P1 P2P3P4;    - RF Gain (P1=VFO 0/1, P2-P4=000-255)
  *   MG P1P2P3;       - Mic Gain (000-100)
  *   GT P1P2P3P4;     - AGC Time Constant (0000-6000ms or code)
- *   ML P1P2P3;       - Monitor Level (000-100)
+ *   ML P1P2P3;       - Monitor on/off or level (selector-dependent)
  *   SM P1;           - S-Meter Read (P1=0 main, returns 0000-0255)
  *   RM P1;           - Meter Read (1=Main S, 2=Sub S, 3=COMP, 4=ALC, 5=PO, 6=SWR, 7=ID, 8=VDD)
  *   PC P1 P2P2P2;    - Power Control (P1=1 field head, P1=2 SPA-1)
@@ -174,6 +174,33 @@ int ftx1_get_mic_gain(RIG *rig, float *val)
     }
 
     *val = (float)level / FTX1_MIC_GAIN_MAX;
+    return RIG_OK;
+}
+
+int ftx1_parse_monitor_response(const char *response, int selector, int *level)
+{
+    int parsed_level;
+
+    if (selector < 0 || selector > 1 || strlen(response) != 7
+            || response[0] != 'M' || response[1] != 'L'
+            || response[2] != '0' + selector
+            || response[3] < '0' || response[3] > '9'
+            || response[4] < '0' || response[4] > '9'
+            || response[5] < '0' || response[5] > '9'
+            || response[6] != ';')
+    {
+        return -RIG_EPROTO;
+    }
+
+    parsed_level = (response[3] - '0') * 100 + (response[4] - '0') * 10
+                   + response[5] - '0';
+
+    if ((selector == 0 && parsed_level > 1) || parsed_level > 100)
+    {
+        return -RIG_EPROTO;
+    }
+
+    *level = parsed_level;
     return RIG_OK;
 }
 
@@ -584,46 +611,46 @@ int ftx1_get_vox_delay(RIG *rig, int *tenths)
 }
 
 /*
- * ftx1_set_monitor_level - Set Monitor Level (ML P1 P2P3P4;)
- * CAT command: ML P1 P2P3P4; (P1=VFO 0/1, P2-P4=000-100)
+ * ftx1_set_monitor_level - Set Monitor Level (ML 1 P2P3P4;)
+ * The FTX-1's ML selector 1 controls the global monitor level; it is not a
+ * VFO selector.
  */
 int ftx1_set_monitor_level(RIG *rig, vfo_t vfo, float val)
 {
     struct newcat_priv_data *priv = STATE(rig)->priv;
-    int p1 = ftx1_vfo_to_p1(rig, vfo);
     int level = (int)(val * 100);
 
-    rig_debug(RIG_DEBUG_VERBOSE, "%s: vfo=%s p1=%d val=%f\n", __func__,
-              rig_strvfo(vfo), p1, val);
+    (void)vfo;
+
+    rig_debug(RIG_DEBUG_VERBOSE, "%s: val=%f\n", __func__, val);
 
     if (level < 0) level = 0;
     if (level > 100) level = 100;
 
-    SNPRINTF(priv->cmd_str, sizeof(priv->cmd_str), "ML%d%03d;", p1, level);
+    SNPRINTF(priv->cmd_str, sizeof(priv->cmd_str), "ML1%03d;", level);
     return newcat_set_cmd(rig);
 }
 
 /*
  * ftx1_get_monitor_level - Get Monitor Level
- * Response: ML P1 P2P3P4; (P1=VFO, P2-P4=level)
+ * Response: ML P1 P2P3P4; (P1=1, P2-P4=level)
  */
 int ftx1_get_monitor_level(RIG *rig, vfo_t vfo, float *val)
 {
     struct newcat_priv_data *priv = STATE(rig)->priv;
     int ret;
-    int p1 = ftx1_vfo_to_p1(rig, vfo);
-    int p1_resp, level;
+    int level;
 
-    rig_debug(RIG_DEBUG_VERBOSE, "%s: vfo=%s p1=%d\n", __func__,
-              rig_strvfo(vfo), p1);
+    (void)vfo;
 
-    SNPRINTF(priv->cmd_str, sizeof(priv->cmd_str), "ML%d;", p1);
+    rig_debug(RIG_DEBUG_VERBOSE, "%s\n", __func__);
+
+    SNPRINTF(priv->cmd_str, sizeof(priv->cmd_str), "ML1;");
 
     ret = newcat_get_cmd(rig);
     if (ret != RIG_OK) return ret;
 
-    /* Response: ML0100 (P1=0, level=100) */
-    if (sscanf(priv->ret_data + 2, "%1d%3d", &p1_resp, &level) != 2)
+    if (ftx1_parse_monitor_response(priv->ret_data, 1, &level) != RIG_OK)
     {
         rig_debug(RIG_DEBUG_ERR, "%s: failed to parse '%s'\n", __func__,
                   priv->ret_data);

rigs/yaesu/ftx1/ftx1_tx.c
@@ -7,7 +7,7 @@
  * CAT Commands in this file:
  *   TX P1;           - Transmit (P1: 0=RX, 1=TX CAT, 2=TX Data)
  *   PC P1P2P3;       - Power Control (005-100 watts) [also in ftx1_audio.c]
- *   MX P1;           - Monitor TX Audio (0=off, 1=on)
+ *   MX P1;           - MOX transmit control
  *   VX P1;           - VOX on/off (0=off, 1=on)
  *   AC P1P2P3;       - Antenna Tuner Control (P1: 0=off, 1=on, 2=tune)
  *   BI P1;           - Break-In on/off (0=off, 1=on) - type via EX020115
@@ -142,39 +142,39 @@ int ftx1_get_vox(RIG *rig, int *status)
     return RIG_OK;
 }
 
-/* Set TX Monitor (MX P1;) */
+/* Set TX Monitor using ML selector 0 (000=off, 001=on). */
 int ftx1_set_monitor(RIG *rig, int status)
 {
     struct newcat_priv_data *priv = STATE(rig)->priv;
-    int p1 = status ? 1 : 0;
+    int level = status ? 1 : 0;
 
     rig_debug(RIG_DEBUG_VERBOSE, "%s: status=%d\n", __func__, status);
 
-    SNPRINTF(priv->cmd_str, sizeof(priv->cmd_str), "MX%d;", p1);
+    SNPRINTF(priv->cmd_str, sizeof(priv->cmd_str), "ML0%03d;", level);
     return newcat_set_cmd(rig);
 }
 
-/* Get TX Monitor status (MX P1;) */
+/* Get TX Monitor using ML selector 0 (000=off, 001=on). */
 int ftx1_get_monitor(RIG *rig, int *status)
 {
     struct newcat_priv_data *priv = STATE(rig)->priv;
-    int ret, p1;
+    int ret, level;
 
     rig_debug(RIG_DEBUG_VERBOSE, "%s\n", __func__);
 
-    SNPRINTF(priv->cmd_str, sizeof(priv->cmd_str), "MX;");
+    SNPRINTF(priv->cmd_str, sizeof(priv->cmd_str), "ML0;");
 
     ret = newcat_get_cmd(rig);
     if (ret != RIG_OK) return ret;
 
-    if (sscanf(priv->ret_data + 2, "%1d", &p1) != 1)
+    if (ftx1_parse_monitor_response(priv->ret_data, 0, &level) != RIG_OK)
     {
         rig_debug(RIG_DEBUG_ERR, "%s: failed to parse '%s'\n", __func__,
                   priv->ret_data);
         return -RIG_EPROTO;
     }
 
-    *status = p1;
+    *status = level;
 
     rig_debug(RIG_DEBUG_VERBOSE, "%s: status=%d\n", __func__, *status);
 

tests/testftx1parsers.c
@@ -130,6 +130,36 @@ static int expect_smeter(const char *response, int p1, int expected,
     return 0;
 }
 
+static int expect_monitor(const char *response, int selector, int expected,
+                          int expected_level)
+{
+    int level = 42;
+    int ret = ftx1_parse_monitor_response(response, selector, &level);
+
+    if (ret != expected)
+    {
+        fprintf(stderr, "ML response '%s': expected %d, got %d\n",
+                response, expected, ret);
+        return 1;
+    }
+
+    if (ret == RIG_OK && level != expected_level)
+    {
+        fprintf(stderr, "ML response '%s': expected %d, got %d\n",
+                response, expected_level, level);
+        return 1;
+    }
+
+    if (ret != RIG_OK && level != 42)
+    {
+        fprintf(stderr, "ML response '%s' changed output on failure\n",
+                response);
+        return 1;
+    }
+
+    return 0;
+}
+
 int main(void)
 {
     static const char *invalid_states[] =
@@ -144,6 +174,10 @@ int main(void)
     {
         "EX0301051;", "EX030104;", "EX0301041junk;",
         "EX0301042147483648;"
+    };
+    static const char *invalid_monitor[] =
+    {
+        "ML0002;", "ML2000;", "ML0000"
     };
 
     if (expect_clar_state("CF00000000;", '0', RIG_OK, '0', '0') != 0
@@ -182,6 +216,24 @@ int main(void)
     for (size_t i = 0; i < sizeof(invalid_ex) / sizeof(invalid_ex[0]); i++)
     {
         if (expect_ex(invalid_ex[i], -RIG_EPROTO, 0) != 0)
+        {
+            return 1;
+        }
+    }
+
+    if (expect_monitor("ML0000;", 0, RIG_OK, 0) != 0
+            || expect_monitor("ML0001;", 0, RIG_OK, 1) != 0
+            || expect_monitor("ML1000;", 1, RIG_OK, 0) != 0
+            || expect_monitor("ML1100;", 1, RIG_OK, 100) != 0
+            || expect_monitor("ML1101;", 1, -RIG_EPROTO, 0) != 0)
+    {
+        return 1;
+    }
+
+    for (size_t i = 0; i < sizeof(invalid_monitor)
+            / sizeof(invalid_monitor[0]); i++)
+    {
+        if (expect_monitor(invalid_monitor[i], 0, -RIG_EPROTO, 0) != 0)
         {
             return 1;
         }